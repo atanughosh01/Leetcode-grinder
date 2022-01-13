9e2e38772b7433ce13f8bb2397183948f6500cf2
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-01-13 20:37:06 +0530

    Time: 774 ms (14.59%), Space: 102.3 MB (11.99%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,6 +1,8 @@
+/*
 class Solution {
 public:
-
+    
+    // sort based on the end of the ballon
     int findMinArrowShots(vector<vector<int>>& points) {
         if(points.size() == 0) return 0;
         
@@ -15,6 +17,32 @@ public:
             if(startX > endX){
                 cnt++;
                 endX = point[1];
+            }
+        }
+        return cnt;
+    }
+};
+*/
+
+class Solution {
+public:
+    
+    // sort based on the start of the ballon
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if(points.size() == 0) return 0;
+    
+        // for sorting on basis of 1st column
+        sort(points.begin(), points.end());
+        int startX = points[0][0], endX = points[0][1];
+        int cnt = 1;
+        for(auto point : points) {
+            if(point[0] > endX) {
+                cnt++;
+                startX = point[0];
+                endX = point[1];
+            } else {
+                startX = max(point[0], startX);
+                endX = min(point[1], endX);
             }
         }
         return cnt;
