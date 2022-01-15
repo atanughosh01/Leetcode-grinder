3281bf44a49983c64d3b78a7d19b79c3173a1e31
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-01-15 23:46:12 +0530

    Time: 24 ms (63.43%), Space: 19.2 MB (56.86%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
@@ -10,5 +11,24 @@ public:
             }
         }
         while(cnt--) nums.push_back(0);
+    }
+};
+*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        if(n<=1)return;
+        int z = 0;
+        int j = 1;
+        while(j < n){
+            if(nums[z] == 0 && nums[j]!=0){
+                swap(nums[z],nums[j]);
+                z++;               
+            }
+            if(nums[z]!=0)z++;
+             j++;
+        }
     }
 };
