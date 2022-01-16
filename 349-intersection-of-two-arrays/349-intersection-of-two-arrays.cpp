627daf026e008ff00b70190906efe1dba555b21e
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-01-16 23:59:33 +0530

    Time: 0 ms (100.00%), Space: 10.5 MB (28.70%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+    /*
     // Time Complexity = O(N*M), Space Complexity = O(N)
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         set<int> st;        
@@ -18,5 +18,19 @@ public:
             vt.push_back(*itr);
         }
         return vt;
+    }
+    */
+    
+    // Time Complexity = O(N), Space Complexity = O(N)
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> st(nums1.begin(), nums1.end());
+        vector<int> res;
+        for(int itr = 0; itr < nums2.size(); itr++){
+            if(st.find(nums2[itr]) != st.end()){
+                res.emplace_back(nums2[itr]);
+                st.erase(nums2[itr]);
+            }            
+        }
+    return res;
     }
 };
