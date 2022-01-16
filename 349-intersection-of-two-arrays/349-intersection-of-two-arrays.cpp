05e09b4c09bfd1197cd6646ff323bf377e5f143b
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-01-16 23:57:23 +0530

    Time: 54 ms (5.21%), Space: 10.3 MB (63.91%) - LeetHub

---
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    // Time Complexity = O(N*M), Space Complexity = O(N)
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> st;        
+        for(int itr = 0; itr < nums1.size(); itr++){
+            for(int jtr = 0; jtr < nums2.size(); jtr++){
+                if(nums1[itr] == nums2[jtr]){
+                    st.insert(nums1[itr]);
+                    break;
+                }
+            }
+        }
+
+        vector<int> vt;
+        for(auto itr = st.begin(); itr != st.end(); itr++){
+            vt.push_back(*itr);
+        }
+        return vt;
+    }
+};
