17a47f3ecf2ef2e18e7d5943f43e72053f4a1966
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-01-17 14:01:40 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (50.50%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     bool wordPattern(string pattern, string str) {
@@ -11,5 +12,41 @@ public:
             pat_map[pattern[i]-'a'] = str_map[token] = i+1;
         }
         return i == n;
+    }
+};
+*/
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, string> StoP; 
+        unordered_map<string, char> PtoS;
+        
+        stringstream sss(s); // Used for breaking words
+        string word; // To store individual words
+        
+        // if length of both pattern and s is diff then return false
+        int count = 0;
+        while (sss >> word)
+            count++;
+        if(count != pattern.length())
+            return false;
+        
+        int i=0;
+        stringstream ss(s); 
+        string Word;
+        while(ss >> Word) {            
+            // if mapping dosen't exist in either of the maps
+            if(StoP.find(pattern[i]) == StoP.end() && PtoS.find(Word) == PtoS.end()) {
+                StoP[pattern[i]] = Word;
+                PtoS[Word] = pattern[i];
+            } 
+            // if mapping dosen't exist in one of the maps or mapping exist but dosen't match
+            else if(!(StoP[pattern[i]] == Word && PtoS[Word] == pattern[i])) {
+                return false;
+            }
+            i++;
+        }
+        return true;
     }
 };
