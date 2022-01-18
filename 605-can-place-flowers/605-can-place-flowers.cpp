ce0c65bbe2e229a7bc632192b2010f11dadb6b1b
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-01-18 21:24:07 +0530

    Time: 37 ms (9.49%), Space: 20.3 MB (20.95%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -1,3 +1,5 @@
+/*
+
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
@@ -10,5 +12,23 @@ public:
             }
         }
         return false;
+    }
+};
+
+*/
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int zeros = 1, ans = 0; // Easier handling of prefixes, just initialize zeros to 1
+        for (int i = 0; i < flowerbed.size(); i ++) {
+            if (flowerbed[i] == 0) {
+                ++ zeros;
+            } else {
+                ans += (zeros - 1) / 2;
+                zeros = 0;
+            }
+        }
+        return ans + zeros / 2 >= n; // Note that suffix zeros need not -1
     }
 };
