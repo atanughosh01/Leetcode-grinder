3517e28f073211b5ce9f3196bc9573979cc226c2
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-01-20 23:55:58 +0530

    Time: 72 ms (32.94%), Space: 28.4 MB (5.12%) - LeetHub

--- 561-array-partition-i/561-array-partition-i.cpp
+++ 561-array-partition-i/561-array-partition-i.cpp
@@ -1,3 +1,5 @@
+/*
+
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
@@ -7,5 +9,29 @@ public:
             result += nums[i];
         }
         return result;
+    }
+};
+
+*/
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        int buckets[20001] = {0};
+        for(auto n : nums) {
+            ++buckets[n + 10000];
+        }
+        int sum = 0;
+        for(int i = 0, used = 0; used < nums.size();) {
+            if(buckets[i] == 0) {
+                ++i;
+            } else {
+                if(used % 2 == 0)
+                    sum += (i - 10000);
+                --buckets[i];
+                ++used;
+            }
+        }
+        return sum;
     }
 };
