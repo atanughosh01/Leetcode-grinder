219d6b49cf6231435f5e457b3ba100216a54094c
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-01-26 18:15:21 +0530

    Time: 132 ms (81.97%), Space: 85.4 MB (44.06%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -10,7 +10,7 @@
  * };
  */
 
-
+/*
 // TC : O(NlogN), SC : O(N)
 class Solution {
 public:
@@ -28,4 +28,37 @@ public:
           sort(res.begin(), res.end());
           return res;
      }
+};
+*/
+
+// TC : O(N), SC : O(N)
+class Solution {
+public:
+    void dfs(TreeNode* root, vector<int> &v){
+        if (!root)
+               return;
+        dfs(root->left,v);
+        v.emplace_back(root->val);
+        dfs(root->right,v);
+    }
+
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> a,b;
+        dfs(root1,a);
+        dfs(root2,b);
+        
+        vector<int> ans((int)(a.size()+b.size()));
+        int i=0,j=0,k=0;
+
+        while(i<(int)a.size() and j<(int)b.size())
+            ans[k++]=a[i]<b[j] ? a[i++] : b[j++];
+
+        while(i<(int)a.size())
+            ans[k++]=a[i++];
+
+        while(j<(int)b.size())
+            ans[k++]=b[j++];
+
+        return ans;
+    }
 };
