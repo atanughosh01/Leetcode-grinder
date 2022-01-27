c4db3b64f58518f62daadd1a26dfc42391b59a66
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-01-27 10:04:36 +0530

    Time: 329 ms (62.49%), Space: 65.2 MB (83.21%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,3 +1,5 @@
+/*
+
 class Solution {
 private:
     int helper(vector<int>& nums, int ls, int le, int rs, int re, int val, int bit) {
@@ -33,5 +35,79 @@ public:
     int findMaximumXOR(vector<int>& nums) {
         int n = (int)nums.size();
         return helper(nums, 0, n-1, 0, n-1, 0, 30);
+    }
+};
+
+*/
+
+
+struct trie{
+    trie* next[2];
+    trie()
+    {
+        next[0]=NULL;
+        next[1]=NULL;
+    }
+};
+class TrieNode{
+    private:
+        trie* root;
+    public:
+        TrieNode()
+        {
+            root=new trie();
+        }
+    
+    void insert(int num)
+    {
+        trie* curr=root;
+        
+        for(int i=31;i>=0;i--)
+        {
+            int bit=(num>>i)&1;
+            if(curr->next[bit]==NULL)
+            {
+                curr->next[bit]=new trie();
+            }
+            curr=curr->next[bit];
+        }
+    }
+    
+    int max_xor(int num)
+    {
+        trie* curr=root;
+        int ans=0;
+        for(int i=31;i>=0;i--)
+        {
+            int bit=(num>>i)&1;
+            
+            if(curr->next[!bit])
+            {
+                ans+=(1<<i);
+                curr=curr->next[!bit];
+            }
+            else
+            {
+                curr=curr->next[bit];
+            }
+        }
+        return ans;
+    }   
+};
+
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        int max_ans=0;
+        int n=nums.size();
+        
+        TrieNode* t=new TrieNode();
+        
+        for(int i=0;i<n;i++)
+        {
+            t->insert(nums[i]);
+            max_ans=max(max_ans,t->max_xor(nums[i]));
+        }
+        return max_ans;
     }
 };
