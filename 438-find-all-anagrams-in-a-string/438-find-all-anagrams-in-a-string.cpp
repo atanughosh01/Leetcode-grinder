a5cc84fa8ed695212efc021c0ad11be986193489
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-02 16:03:22 +0530

    Time: 12 ms (87.76%), Space: 8.7 MB (42.26%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,53 @@
+/* 
+
+// BruteForce TLE SOLUTION
+
+class Solution {
+public:
+	vector<int> findAnagrams(string s, string p) {
+		int pn = (int)p.size();
+		int sn = (int)s.size();
+
+		sort(p.begin(), p.end());
+		vector<int> idx;
+
+		for (int i = 0; i + pn <= sn; i++) {
+            string str = s.substr(i, pn);
+			sort(str.begin(), str.end());
+			if (str == p) {
+				idx.emplace_back(i);
+			}
+		}
+		return idx;
+	}
+};
+
+*/
+
+
+// Optimal Solution
+
+class Solution {
+public:
+	vector<int> findAnagrams(string s, string p) {
+		int n = s.length(), m = p.length();
+		vector<int> pv(26, 0), sv(26, 0), res;
+		for (int l = 0; l < m; l++)
+			pv[p[l] - 'a']++;
+		int i = 0, j = 0;
+		while (j < n) {
+			sv[s[j] - 'a']++;
+			if ((j - i + 1) < m) {
+				j++;
+			}
+			else if ((j - i + 1) == m) {
+				if (pv == sv)
+					res.emplace_back(i);
+				sv[s[i] - 'a']--;
+				i++;
+				j++;
+			}
+		}
+		return res;
+	}
+};
