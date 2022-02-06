ace1c13e945e4e4679a2cded8b6d54d8ebdeb52f
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-06 13:00:24 +0530

    Added solution - LeetHub

---
+++ Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+// Problem : https://practice.geeksforgeeks.org/problems/minimum-swaps/1
+
+
+class Solution {
+public:
+	int minSwaps(vector<int>& nums) {
+		vector<pair<int, int>> v;
+		int n = nums.size();
+		for (int i = 0; i < n; i++)
+			v.emplace_back(nums[i], i);
+		sort(v.begin(), v.end());
+		int swaps = 0;
+		for (int i = 0; i < n; i++) {
+			int j = v[i].second;
+			while (i != j) {
+				swap(v[i], v[j]);
+				j = v[i].second;
+				swaps++;
+			}
+		}
+		return swaps;
+	}
+};
+
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for(int i = 0; i < n; i++)
+			cin >> nums[i];
+		Solution obj;
+		int ans = obj.minSwaps(nums);
+		cout << ans <<"\n";
+	}
+	return 0;
+}  // } Driver Code Ends
