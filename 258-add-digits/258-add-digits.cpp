aab9de474d96495b3adcef85d3f579381f8eaaa9
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-08 11:04:51 +0530

    Time: 3 ms (53.62%), Space: 5.9 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,19 +1,20 @@
+/*
 
 // Recursion
-// class Solution {
-// public:
-//     int addDigits(int num) {
-//       int sum=0;
-//         while(num) {
-//             sum+=(num%10);
-//             num/=10;
-//         }
-//         if(sum<10)
-//             return sum;
-//         else
-//             return addDigits(sum);
-//     }
-// };
+class Solution {
+public:
+    int addDigits(int num) {
+      int sum=0;
+        while(num) {
+            sum+=(num%10);
+            num/=10;
+        }
+        if(sum<10)
+            return sum;
+        else
+            return addDigits(sum);
+    }
+};
 
 
 // Iteration
@@ -33,16 +34,17 @@ public:
     }
 };
 
-
-// // O(1) Time Complexity
-// class Solution {
-// public:
-//     int addDigits(int num) {
-//       if(num==0)
-//           return 0;
-//       else if(num%9==0)
-//           return 9;
-//       else
-//           return num%9;
-//     }
-// };
+*/
+    
+// O(1) Time Complexity
+class Solution {
+public:
+    int addDigits(int num) {
+      if(num==0)
+          return 0;
+      else if(num%9==0)
+          return 9;
+      else
+          return num%9;
+    }
+};
