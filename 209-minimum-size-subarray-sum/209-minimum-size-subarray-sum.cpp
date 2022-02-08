2c8230d8350ca93755203a4988516ac28045cb92
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-09 02:14:20 +0530

    Time: 11 ms (49.72%), Space: 10.8 MB (6.26%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,3 +1,26 @@
+// O(NlogN) using Prefix Sum
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = (int)nums.size(), len = INT_MAX;
+        vector<int> sums(n+1);
+        sums[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+        for (int i = n; i >= 0; i--) {
+            if(sums[i] < target)
+                break;
+            int j = upper_bound(sums.begin(), sums.end(), sums[i]-target) - sums.begin();
+            len = min(len, i - j + 1);
+        }
+        if(len==INT_MAX) return 0;
+        else return len;
+    }
+};
+
+/*
+// O(NlogN) using Two Pointers
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
@@ -16,3 +39,4 @@ public:
         else return len;
     }
 };
+*/
