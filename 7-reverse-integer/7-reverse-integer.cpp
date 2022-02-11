e61739aed0443108b6144d8da7af51e65cf58502
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-11 23:45:37 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (76.58%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -6,6 +6,9 @@ public:
             res = res*10 + x%10;
             x /= 10;
         }
-        return (res<INT_MIN || res>INT_MAX) ? 0 : res;
+        if(res<INT_MIN or res>INT_MAX)
+            return 0;
+        else
+            return res;
     }
 };
