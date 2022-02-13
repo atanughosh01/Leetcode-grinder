7e6fff5b8a293f171cfd2b2e9b6cf413e8f92d6a
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-13 12:01:57 +0530

    Time: 8 ms (30.54%), Space: 7.7 MB (6.54%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,3 +1,6 @@
+/*
+
+// Chad Solution :)
 class Solution {
 public:
 
@@ -11,6 +14,52 @@ public:
        return shortUrl;        
     }
 };
+
+*/
+
+
+// Optimal Solution
+class Solution {
+private:
+
+    // Database
+    unordered_map<string, string> codeDB, urlDB;
+    const string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    int len = (int)chars.size(), idx = 0;
+
+    // Generates a random 6 character alphanumeric string
+    string gencode() {
+        string code = "";
+        for(int i=0; i<6; ++i) {
+            idx = rand() % len;
+            code += chars[idx];
+        }
+        return "http://tinyurl.com/" + code;
+    }
+
+public:
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        if(urlDB.find(longUrl) != urlDB.end())
+            return urlDB[longUrl];
+        
+        string code = gencode();
+        while(codeDB.find(code) != codeDB.end())
+            code = gencode();
+        
+        codeDB[code] = longUrl;
+        urlDB[longUrl] = code;
+        return code;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        string str = codeDB[shortUrl];
+        return str;
+    }
+};
+
 
 // Your Solution object will be instantiated and called as such:
 // Solution solution;
