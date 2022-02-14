c18a597724e6e2263119f1e72d5d39dfba6c3778
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-14 23:20:37 +0530

    Time: 21 ms (12.88%), Space: 19.1 MB (15.07%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -10,6 +10,10 @@
  * };
  */
 
+
+/*
+
+// DFS
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
@@ -17,5 +21,33 @@ public:
         int maxLeft = maxDepth(root->left);
         int maxRight = maxDepth(root->right);
         return max(maxLeft, maxRight)+1;        
+    }
+};
+
+*/
+
+
+// BFS
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root==NULL)
+            return 0; 
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth=0;
+        while(!q.empty()) {
+            ++depth;
+            int s=q.size();
+            for (int i=0; i<s; i++) {
+                TreeNode* front=q.front();
+                q.pop();
+                if (front->left)
+                    q.push(front->left);
+                if (front->right)
+                    q.push(front->right);
+            }
+        }
+        return depth;
     }
 };
