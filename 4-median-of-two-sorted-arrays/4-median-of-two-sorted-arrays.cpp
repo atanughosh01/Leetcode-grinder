0cf52feb2621a64a14c5167f694d4b24f8e4d0a7
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-16 23:11:07 +0530

    Time: 44 ms (62.34%), Space: 89.8 MB (31.25%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -4,7 +4,7 @@ public:
         for(int n : nums2) nums1.push_back(n);
         int n = (int)nums1.size();
         sort(nums1.begin(), nums1.end());
-        if(n%2 != 0) return (double)nums1[n/2];
+        if(n%2) return (double)nums1[n/2];
         else return (double)(nums1[n/2-1] + nums1[n/2])/2.0;
     }
 };
