81747b29f7f29906fa9efd010c65a40ed804a7fa
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-20 17:12:15 +0530

    Time: 13 ms (60.00%), Space: 13.1 MB (13.33%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -1,14 +1,37 @@
+/*
+    class Solution {
+    public:
+        int countPairs(vector<int>& nums, int k) {
+            int n = (int)nums.size();
+            int cnt=0;
+            for(int i=0; i<n; ++i) {
+                for(int j=i+1; j<n; ++j) {
+                    if((nums[i]==nums[j]) && (i*j)%k==0)
+                        cnt++;
+                }
+            }
+            return cnt;
+        }
+    };
+*/
+
+
 class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
-        int n = (int)nums.size();
-        int cnt=0;
-        for(int i=0; i<n; ++i) {
-            for(int j=i+1; j<n; ++j) {
-                if((nums[i]==nums[j]) && (i*j)%k==0)
-                    cnt++;
+        int res = 0;
+        unordered_map<int, vector<int>> m;
+        for (int i = 0;  i < nums.size(); ++i)
+            m[nums[i]].push_back(i);
+        for (auto &[n, ids] : m) {
+            unordered_map<int, int> gcds;
+            for (auto i : ids) {
+                auto gcd_i = gcd(i, k);
+                for (auto &[gcd_j, cnt] : gcds)
+                    res += gcd_i * gcd_j % k ? 0 : cnt;
+                ++gcds[gcd_i];
             }
         }
-        return cnt;
+        return res;
     }
 };
