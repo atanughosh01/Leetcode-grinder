7aa715d12fcd90f3a78d4b3e2eb942a6ea24e6dd
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-20 23:46:55 +0530

    Time: 203 ms (61.54%), Space: 40.5 MB (92.31%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
@@ -1,20 +1,60 @@
+/*
+    class Solution {
+    public:
+        vector<long long> maximumEvenSplit(long long num) {
+            vector<long long> arr = {};
+            if(num%2)
+                return arr;
+            int con = 0;
+            while(true) {
+               con += 2;
+               num -= con;
+                if(num >= con+2)
+                    arr.push_back(con);
+                else{
+                    arr.push_back(num + con);
+                    break;
+                }
+           }
+            return arr;
+        }
+    };
+
+
+    // Time: O(sqrt(N))
+    // Space: O(sqrt(N))
+    class Solution {
+    public:
+        vector<long long> maximumEvenSplit(long long s) {
+            if (s % 2) return {};
+            vector<long long> ans;
+            function<bool(long, long)>dfs = [&](long i, long target) {
+                if (target == 0) return true;
+                if (target < i) return false;
+                ans.push_back(i);
+                if (dfs(i + 2, target - i)) return true; // use `i`
+                ans.pop_back();
+                return dfs(i + 2, target); // skip `i`
+            };
+            dfs(2, s);
+            return ans;
+        }
+    };
+*/
+
+
+// Time: O(sqrt(N))
+// Space: O(1)
 class Solution {
 public:
-    vector<long long> maximumEvenSplit(long long num) {
-        vector<long long> arr = {};
-        if(num%2)
-            return arr;
-        int con = 0;
-        while(true) {
-           con += 2;
-           num -= con;
-            if(num >= con+2)
-                arr.push_back(con);
-            else{
-                arr.push_back(num + con);
-                break;
-            }
-       }
-        return arr;
+    vector<long long> maximumEvenSplit(long long s) {
+        if (s % 2) return {};
+        vector<long long> ans;
+        for (int i = 2; s - i >= i + 2; i += 2) {
+            ans.push_back(i);
+            s -= i;
+        }
+        ans.push_back(s);
+        return ans;
     }
 };
