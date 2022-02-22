6a40c03b74ff5303f2e5c16f521867d5e49bdb98
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-22 23:45:00 +0530

    Time: 144 ms (97.80%), Space: 84.5 MB (28.37%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -21,22 +21,40 @@
     };
 */
 
-// Optimised
+/*
+    // Optimised
+    class Solution {
+    public:
+        vector<int> canSeePersonsCount(vector<int>& heights) {
+            int n = (int)heights.size();
+            vector<int> ans(n), stack;
+            for (int i = 0; i < n; ++i) {
+                while (!stack.empty() and heights[stack.back()] <= heights[i]) {
+                    ans[stack.back()]++;
+                    stack.pop_back();
+                }
+                if (!stack.empty()) {
+                    ans[stack.back()]++;
+                }
+                stack.push_back(i);
+            }
+            return ans;
+        }
+    };
+*/
+
+
 class Solution {
 public:
     vector<int> canSeePersonsCount(vector<int>& heights) {
-        int n = (int)heights.size();
-        vector<int> ans(n), stack;
-        for (int i = 0; i < n; ++i) {
-            while (!stack.empty() and heights[stack.back()] <= heights[i]) {
-                ans[stack.back()]++;
-                stack.pop_back();
-            }
-            if (!stack.empty()) {
-                ans[stack.back()]++;
-            }
-            stack.push_back(i);
+        vector<int> mono;
+        for (int i = heights.size() - 1; i >= 0; --i) {
+            int h = heights[i], cnt = 0;
+            for (; !mono.empty() && mono.back() < h; ++cnt)
+                mono.pop_back();
+            heights[i] = cnt + !mono.empty();
+            mono.push_back(h);
         }
-        return ans;
+        return heights;
     }
 };
