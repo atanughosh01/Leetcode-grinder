b7d25bd4dc1d22015fce36785088136ac590bed4
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-22 18:59:46 +0530

    Time: 4 ms (47.34%), Space: 6 MB (38.78%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -1,13 +1,29 @@
+/*
+    class Solution {
+    public:
+        int titleToNumber(string col) {
+            int a[26], res = 0;
+            for (int i=0; i<26; i++) {
+                a[i] = i+1;
+            }
+            for (char x : col) {
+                res *= 26;
+                res += a[(x-'0')-17];
+            }
+            return res;
+        }
+    };
+*/
+
+
 class Solution {
 public:
     int titleToNumber(string col) {
-        int a[26], res = 0;
-        for (int i=0; i<26; i++) {
-            a[i] = i+1;
-        }
+        int res = 0;
         for (char x : col) {
+            int ch = x - 'A' + 1;
             res *= 26;
-            res += a[(x-'0')-17];
+            res += ch;
         }
         return res;
     }
