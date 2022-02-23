01fff4bb281a2ffa57ce05017062294c3d0a8bb4
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-23 21:33:24 +0530

    Time: 15 ms (14.73%), Space: 8.5 MB (93.77%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -19,38 +19,8 @@
     };
 */
 
-/*
-    // B F S
-    class Solution {
-    private:
-        unordered_map<Node*, Node*> copies;
-    public:
-        Node* cloneGraph(Node* node) {
-            if (!node) {
-                return NULL;
-            }
-            Node* copy = new Node(node -> val, {});
-            copies[node] = copy;
-            queue<Node*> todo;
-            todo.push(node);
-            while (!todo.empty()) {
-                Node* cur = todo.front();
-                todo.pop();
-                for (Node* neighbor : cur -> neighbors) {
-                    if (copies.find(neighbor) == copies.end()) {
-                        copies[neighbor] = new Node(neighbor -> val, {});
-                        todo.push(neighbor);
-                    }
-                    copies[cur] -> neighbors.push_back(copies[neighbor]);
-                }
-            }
-            return copy;
-        }
-    };
-*/
 
-
-// D F S
+// B F S
 class Solution {
 private:
     unordered_map<Node*, Node*> copies;
@@ -59,12 +29,43 @@ public:
         if (!node) {
             return NULL;
         }
-        if (copies.find(node) == copies.end()) {
-            copies[node] = new Node(node -> val, {});
-            for (Node* neighbor : node -> neighbors) {
-                copies[node] -> neighbors.push_back(cloneGraph(neighbor));
+        Node* copy = new Node(node -> val, {});
+        copies[node] = copy;
+        queue<Node*> todo;
+        todo.push(node);
+        while (!todo.empty()) {
+            Node* cur = todo.front();
+            todo.pop();
+            for (Node* neighbor : cur -> neighbors) {
+                if (copies.find(neighbor) == copies.end()) {
+                    copies[neighbor] = new Node(neighbor -> val, {});
+                    todo.push(neighbor);
+                }
+                copies[cur] -> neighbors.push_back(copies[neighbor]);
             }
         }
-        return copies[node];
+        return copy;
     }
 };
+
+
+/*
+    // D F S
+    class Solution {
+    private:
+        unordered_map<Node*, Node*> copies;
+    public:
+        Node* cloneGraph(Node* node) {
+            if (!node) {
+                return NULL;
+            }
+            if (copies.find(node) == copies.end()) {
+                copies[node] = new Node(node -> val, {});
+                for (Node* neighbor : node -> neighbors) {
+                    copies[node] -> neighbors.push_back(cloneGraph(neighbor));
+                }
+            }
+            return copies[node];
+        }
+    };
+*/
