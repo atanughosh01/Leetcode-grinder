653295ff95e5bea9e0d0c801a5a3b485f72721b4
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-26 11:30:37 +0530

    Time: 46 ms (69.14%), Space: 7.8 MB (94.55%) - LeetHub

---
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+    
+    int dis[15][15];
+    int dp[1ll<<13][13];
+    
+    void floyd(int n) {
+        for(int k=0; k<n; k++)
+            for(int i=0; i<n; i++)
+                for(int j=0; j<n; j++)
+                    dis[i][j] = min(dis[i][j], dis[i][k]+dis[k][j]);
+    }
+    
+    int DP(int n) {
+        memset(dp, 0x3f, sizeof(dp));
+        for (int i=0; i<n; i++)
+            dp[1ll<<i][i] = 0;
+        for (int group=1; group<(1ll<<n); group++)
+            for (int u=0; u<n; u++)
+                for (int v=0; v<n; v++) {
+                    int src = 1ll << u, dst = 1 << v;
+                    if ((group & src) && !(group & dst))
+                        dp[group|dst][v] = min(dp[group][u] + dis[u][v], dp[group|dst][v]);
+                }
+        int min_dis = 0x3f3f3f3f;
+        for (int i=0; i<n; i++)
+            min_dis = min(dp[(1<<n)-1][i], min_dis);
+        return min_dis;
+    }
+
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        int n = graph.size();
+        memset(dis, 0x3f, sizeof(dis));
+        for (int i=0; i<n; i++) {
+            int to_size = graph[i].size();
+            for (int j=0; j<to_size; j++) {
+                int u = i, v = graph[i][j];
+                dis[u][v] = 1;
+            }
+        }
+        floyd(n);
+        int min_dis = DP(n);
+        return min_dis;
+    }
+};
