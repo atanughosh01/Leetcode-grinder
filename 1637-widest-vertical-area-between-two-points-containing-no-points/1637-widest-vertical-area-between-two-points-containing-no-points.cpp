0bf4330ab49ab0a3f5b7ced38199fae7c8361ea7
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-26 22:44:11 +0530

    Time: 381 ms (56.04%), Space: 81.3 MB (6.46%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.cpp
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -1,16 +1,33 @@
+/*
+    class Solution {
+    public:
+        int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+            int res = INT_MIN;
+            set<int> st;
+            for(auto &x : points) {
+                st.insert(x[0]);
+            }
+            auto j = st.begin();
+            for(auto itr=st.begin(); itr != st.end();) {
+                j = itr;
+                itr++;
+                res = max(res, *itr - *j);
+            }
+            return res;
+        }
+    };
+*/
+
 class Solution {
 public:
     int maxWidthOfVerticalArea(vector<vector<int>>& points) {
-        int res = INT_MIN;
+        int res = 0;
         set<int> st;
-        for(auto x : points) {
+        for(auto &x : points) {
             st.insert(x[0]);
         }
-        auto j = st.begin();
-        for(auto itr=st.begin(); itr != st.end();) {
-            j = itr;
-            itr++;
-            res = max(res, *itr - *j);
+        for(auto itr=next(st.begin()); itr != st.end(); itr++) {
+            res = max(res, *itr - *prev(itr));
         }
         return res;
     }
