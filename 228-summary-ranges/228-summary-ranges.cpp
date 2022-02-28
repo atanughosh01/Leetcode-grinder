c83cae9320255f081b2ef0d6be1caa1cb3dd166a
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-28 17:48:09 +0530

    Time: 0 ms (100.00%), Space: 7 MB (14.46%) - LeetHub

---
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -0,0 +1,36 @@
+#define ll long long
+
+class Solution {
+public:
+    string convRangetoStr(vector<int>& v) {
+        if((ll)v.size() == 1)
+            return to_string(v[0]);
+        string ff = to_string(v[0]);
+        string ss = to_string(v[1]);
+        string str = ff + "->" + ss;
+        return str;
+    }
+
+    vector<string> summaryRanges(vector<int>& nums) {
+        int n = (ll)nums.size();
+        if(n==0) return {};
+        if(n==1) return {to_string(nums[0])};
+        if(n==2){
+            if(nums[1]-nums[0] == 1)
+                return {convRangetoStr(nums)};
+            return {to_string(nums[0]), to_string(nums[1])};
+        }
+        vector<string> list;
+        vector<int> range = {nums[0]};
+        for(int i=0; i<n; ++i) {
+            if(i==n-1) nums.push_back((ll)nums[n-1] + 2);
+            if((ll)nums[i+1]-(ll)nums[i] != 1) {
+                if(range[0] != nums[i])
+                    range.push_back(nums[i]);
+                list.push_back(convRangetoStr(range));
+                range = {nums[i+1]};
+            }
+        }
+        return list;
+    }
+};
