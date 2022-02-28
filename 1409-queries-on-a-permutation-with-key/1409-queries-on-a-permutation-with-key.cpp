1199929d77fe200cac74aa10900cd76b553364b8
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-02-28 19:53:39 +0530

    Time: 12 ms (78.29%), Space: 8.3 MB (60.75%) - LeetHub

--- 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.cpp
+++ 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.cpp
@@ -1,21 +1,47 @@
+/*
+    // Time : O(m*m*n), Space : O(m + n); BRUTEFORCE;
+    class Solution {
+    public:
+        vector<int> processQueries(vector<int>& queries, int m) {
+            int n = (int)queries.size();
+            vector<int> ans, p(m);
+            for (int i=0; i<m; i++) {
+                p[i] = i+1;
+            }
+            for (int i=0; i<n; i++) {
+                for (int j=0; j<m; j++) {
+                    if (p[j] == queries[i]) {
+                        ans.emplace_back(j);
+                        p.insert(p.begin(), p[j]);
+                        p.erase(p.begin()+j+1);
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+    };
+*/
+
+
 class Solution {
 public:
     vector<int> processQueries(vector<int>& queries, int m) {
-        int n = (int)queries.size();
-        vector<int> ans, p(m);
-        for (int i=0; i<m; i++) {
-            p[i] = i+1;
+        int n = (int)queries.size(), temp;
+        vector<int> p(m+1);
+        p[0] = 0;
+        for (int i=1; i<=m; i++) {
+            p[i] = i-1;
         }
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                if (p[j] == queries[i]) {
-                    ans.emplace_back(j);
-                    p.insert(p.begin(), p[j]);
-                    p.erase(p.begin()+j+1);
-                    break;
+        for (int j=0; j<n; j++) {
+            temp = p[queries[j]];
+            for(int i=1; i<=m; i++)
+                if(p[i] < temp) {
+                    p[i]++;
                 }
-            }
+            p[queries[j]] = 0;
+            queries[j] = temp;
         }
-        return ans;
+        return queries;
     }
 };
