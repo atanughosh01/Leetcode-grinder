529acb952b32a068810998995dacc73382b140c8
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-03-03 22:58:48 +0530

    Time: 11 ms (25.39%), Space: 7 MB (93.80%) - LeetHub

--- 980-unique-paths-iii/980-unique-paths-iii.cpp
+++ 980-unique-paths-iii/980-unique-paths-iii.cpp
@@ -1,5 +1,5 @@
 class Solution {
-public:
+private:
     int res = 0, empty = 1;
     void dfs(vector<vector<int>>& grid, int x, int y, int count) {
         if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() || grid[x][y] == -1)
@@ -17,7 +17,7 @@ public:
         dfs(grid, x, y-1, count+1);
         grid[x][y] = 0;
     }
-    
+public:
     int uniquePathsIII(vector<vector<int>>& grid) {
         int start_x=0, start_y=0;
         for (int i = 0; i < grid.size(); i++) {
