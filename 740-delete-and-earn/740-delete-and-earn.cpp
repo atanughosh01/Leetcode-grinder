2862823ca43366f2398d0e0fde72f2bf739a66f6
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-03-05 17:25:39 +0530

    Time: 22 ms (19.42%), Space: 14 MB (10.99%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,17 +1,37 @@
+/*
+    class Solution {
+    public:
+        int deleteAndEarn(vector<int>& nums) {
+            int n = 10001;
+            vector<int> values(n, 0);
+            for (int num : nums)
+                values[num] += num;
+            int take = 0, skip = 0;
+            for (int i = 0; i < n; i++) {
+                int takei = skip + values[i];
+                int skipi = max(skip, take);
+                take = takei;
+                skip = skipi;
+            }
+            return max(take, skip);
+        }
+    };
+*/
+    
+
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
         int n = 10001;
-        vector<int> values(n, 0);
-        for (int num : nums)
-            values[num] += num;
-        int take = 0, skip = 0;
-        for (int i = 0; i < n; i++) {
-            int takei = skip + values[i];
-            int skipi = max(skip, take);
-            take = takei;
-            skip = skipi;
+        vector<int> sum(n, 0);
+        vector<int> dp(n, 0);
+        for(auto num: nums)
+            sum[num] += num;
+        dp[0] = 0;
+        dp[1] = sum[1];
+        for(int i=2; i<n; i++){
+            dp[i] = max(dp[i-2] + sum[i], dp[i-1]);
         }
-        return max(take, skip);
+        return dp[n-1];
     }
 };
