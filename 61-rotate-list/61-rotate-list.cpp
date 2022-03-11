b10beb0931d39e9f0da9ef2170bfe220ec783c6f
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-03-11 09:11:44 +0530

    Time: 8 ms (73.85%), Space: 11.7 MB (55.87%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -10,28 +10,58 @@
  */
 
 
+
+/*
+    class Solution {
+    public:
+        ListNode* rotateRight(ListNode* head, int k) {
+            if(!head)
+                return head;
+
+            int len = 1; // number of nodes
+            ListNode *newH, *tail;
+            newH = tail=head;
+
+            while(tail->next) { // get the number of nodes in the list
+                tail = tail->next;
+                len++;
+            }
+            tail->next = head; // circle the link
+
+            if(k %= len) {
+                for(auto i=0; i<len-k; i++) tail = tail->next; // the tail node is the (len-k)-th node (1st node is head)
+            }
+
+            newH = tail->next; 
+            tail->next = NULL;
+            return newH;
+        }
+    };
+*/
+
+
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(!head)
-            return head;
+        if(head == NULL)
+            return NULL;
+        int listNum = 1;
+        ListNode* tail = head;
         
-        int len = 1; // number of nodes
-        ListNode *newH, *tail;
-        newH = tail=head;
-        
-        while(tail->next) { // get the number of nodes in the list
+        while(tail->next != NULL){
+            listNum++;
             tail = tail->next;
-            len++;
         }
-        tail->next = head; // circle the link
+        tail->next = head;
+        int newHeadIndex = listNum - k % listNum;
 
-        if(k %= len) {
-            for(auto i=0; i<len-k; i++) tail = tail->next; // the tail node is the (len-k)-th node (1st node is head)
+        for(int i = 0; i < newHeadIndex; i++){
+            tail = tail->next;
         }
         
-        newH = tail->next; 
+        head = tail->next;
         tail->next = NULL;
-        return newH;
+
+        return head;
     }
 };
