7a3adfc0ef3c8a4781e8868ece56216147475455
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-03-13 12:43:55 +0530

    Time: 6 ms (11.70%), Space: 6.3 MB (51.50%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,16 +1,42 @@
+/*
+    class Solution {
+    public:
+        bool isValid(string s) {
+            stack<char> st;
+            for(auto i:s) {
+                if(i=='(' or i=='{' or i=='[')
+                    st.push(i);
+                else {
+                    if(st.empty() or (st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or (st.top()=='[' and i!=']'))
+                        return false;
+                    st.pop();
+                }
+            }
+            return st.empty();
+        }
+    };
+*/
+
+
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<char> st;
-        for(auto i:s) {
-            if(i=='(' or i=='{' or i=='[')
-                st.push(i);
-            else {
-                if(st.empty() or (st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or (st.top()=='[' and i!=']'))
-                    return false;
-                st.pop();
-            }
-        }
-        return st.empty();
-    }
+	bool isValid(string x) {
+		int i = 0;
+		stack<int> s;
+		while (i < x.size()) {
+			if (!s.empty()) {
+				if ((s.top() == '(' && x[i] == ')') || (s.top() == '{' && x[i] == '}') || (s.top() == '[' && x[i] == ']')) {
+					s.pop();
+				}
+				else {
+					s.push(x[i]);
+				}
+			}
+			else {
+				s.push(x[i]);
+			}
+			i++;
+		}
+		return s.empty() ? true : false;
+	}
 };
