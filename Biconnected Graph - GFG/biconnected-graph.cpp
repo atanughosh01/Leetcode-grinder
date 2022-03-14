e76ae42f4ca8bba9e2172b1e6c8659797969bb49
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-03-14 20:16:11 +0530

    Added solution - LeetHub

---
+++ Biconnected Graph - GFG/biconnected-graph.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+
+// Problem : https://practice.geeksforgeeks.org/problems/biconnected-graph2528/1
+
+
+
+class Solution {
+public:
+    int biGraph(int arr[], int n, int e) {
+        if(n==1) return 1;
+        if(n==2 && e==1) return 1;
+        vector<int> g[n];
+        for(int i=0; i<2*e; i+=2) {
+           int u = arr[i];
+           int v = arr[i+1];
+           g[u].push_back(v);
+           g[v].push_back(u);
+        }
+        for(auto i:g){
+            if(i.size()<2) {
+                return 0;
+            }   
+        }
+       return 1;
+    }
+};
+
+
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,e;
+        cin>>n>>e;
+        
+        int arr[2*e];
+        for(int i=0; i<2*e; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        cout << ob.biGraph(arr,n,e) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
