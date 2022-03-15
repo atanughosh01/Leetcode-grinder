4e9e56244178e1f5dca50a92a491cbb4abca912e
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-03-16 02:55:22 +0530

    Time: 192 ms (90.47%), Space: 123.4 MB (90.46%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,16 +1,37 @@
+/*
+    class Solution {
+    public:
+        vector<int> pivotArray(vector<int>& nums, int pivot) {
+            int n = (int)nums.size();
+            vector<int> less, greater, equal, res;
+            for(int i=0; i<n; i++) {
+                if(nums[i] < pivot) less.emplace_back(nums[i]);
+                if(nums[i] > pivot) greater.emplace_back(nums[i]);
+                if(nums[i] == pivot) equal.emplace_back(nums[i]);
+            }
+            for(int x : less) res.emplace_back(x);
+            for(int y : equal) res.emplace_back(y);
+            for(int z : greater) res.emplace_back(z);
+            return res;
+        }
+    };
+*/
+
+
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int n = (int)nums.size();
-        vector<int> less, greater, equal, res;
-        for(int i=0; i<n; i++) {
-            if(nums[i] < pivot) less.emplace_back(nums[i]);
-            if(nums[i] > pivot) greater.emplace_back(nums[i]);
-            if(nums[i] == pivot) equal.emplace_back(nums[i]);
+        int low = 0, same = 0, high = nums.size();
+        for(auto n : nums) {
+            if(n < pivot)  ++same; 
+            else if(n > pivot) --high;
         }
-        for(int x : less) res.emplace_back(x);
-        for(int y : equal) res.emplace_back(y);
-        for(int z : greater) res.emplace_back(z);
+        vector<int> res(nums.size());
+        for(auto n : nums) {
+            if(n < pivot) res[low++] = n; 
+            else if(n == pivot) res[same++] = n;
+            else res[high++] = n;
+        }
         return res;
     }
 };
