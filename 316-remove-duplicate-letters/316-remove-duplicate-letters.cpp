87c99e8c0f386e2390ad1db2ace496d7ed403a13
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-03-18 17:43:59 +0530

    Time: 3 ms (73.49%), Space: 6.7 MB (66.29%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,20 +1,46 @@
+/*
+    class Solution {
+    public:
+        string removeDuplicateLetters(string s) {
+            string res="";
+            int lastPos[26] = {};
+            bool added[26] = {};
+            for (int i = 0; i < s.size(); i++) {
+                lastPos[s[i] - 'a'] = i;
+            }
+            for (int i = 0; i < s.size(); i++) {
+                if (added[s[i] - 'a']) continue;
+                while ( !res.empty() && res.back() > s[i] && lastPos[res.back() - 'a'] > i) {
+                    added[res.back() - 'a'] = false;
+                    res.pop_back();
+                }
+                res.push_back(s[i]);
+                added[s[i] - 'a'] = true;
+            }
+            return res;
+        }
+    };
+*/
+
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        string res="";
-        int lastPos[26] = {};
-        bool added[26] = {};
-        for (int i = 0; i < s.size(); i++) {
-            lastPos[s[i] - 'a'] = i;
+        vector<int> cnt(26,0)  , vis(26,0);
+        string res = "";
+        int n = s.size();
+        for(int i = 0; i<n; ++i) {
+            cnt[s[i] - 'a']++;
         }
-        for (int i = 0; i < s.size(); i++) {
-            if (added[s[i] - 'a']) continue;
-            while ( !res.empty() && res.back() > s[i] && lastPos[res.back() - 'a'] > i) {
-                added[res.back() - 'a'] = false;
-                res.pop_back();
+        for(int i = 0; i<n; ++i) {
+            cnt[s[i] - 'a']--;
+            if(!vis[s[i]- 'a']) {
+                while(res.size() > 0 && res.back() > s[i] && cnt[res.back() - 'a'] > 0) {
+                    vis[res.back() - 'a'] = 0;
+                    res.pop_back();
+                }
+                res += s[i];
+                vis[s[i] - 'a'] = 1;
             }
-            res.push_back(s[i]);
-            added[s[i] - 'a'] = true;
         }
         return res;
     }
