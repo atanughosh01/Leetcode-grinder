753ee228584def7d1394b463d08630102583a892
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-03-22 22:43:13 +0530

    Time: 65 ms (44.86%), Space: 21.2 MB (80.96%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -1,17 +1,34 @@
+/*
+    class Solution {
+    public:
+        string getSmallestString(int n, int k) {
+            string s(n,'a');
+            k -= n;
+            for(int i=n-1; i>=0; i--) {
+                if(k==0) break;
+                else if(k < 25) {
+                    s[i] = (char)('a' + k);
+                    k = 0;
+                } else {
+                    s[i] = (char)('a' + 25);
+                    k -= 25;
+                }
+            }
+            return s;
+        }
+    };
+*/
+
+
 class Solution {
 public:
     string getSmallestString(int n, int k) {
-        string s(n,'a');
+        string s(n, 'a');
         k -= n;
-        for(int i=n-1; i>=0; i--) {
-            if(k==0) break;
-            else if(k < 25) {
-                s[i] = (char)('a' + k);
-                k = 0;
-            } else {
-                s[i] = (char)('a' + 25);
-                k -= 25;
-            }
+        int ptr = n-1;
+        while (k > 0) {
+            s[ptr--] += min(k, 25);
+            k -= min(k, 25);
         }
         return s;
     }
