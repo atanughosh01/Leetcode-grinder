941f9774b472cc97f12302c6e018fa5165b6d826
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-03-24 19:40:59 +0530

    Time: 27 ms (17.26%), Space: 9.5 MB (19.47%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,21 +1,52 @@
-class Solution {
-private:
-    void sort(int i, int j, int m, int n, vector<vector<int>>& mat) {
-        for (; i < m && j < n; i++, j++) {
-            int a = i, b = j;
-            while (a < m && b < n) {
-                if (mat[a][b] < mat[i][j])
-                    swap(mat[a][b], mat[i][j]);
-                a++;
-                b++;
+/*
+    class Solution {
+    private:
+        void sort(int i, int j, int m, int n, vector<vector<int>>& mat) {
+            for (; i < m && j < n; i++, j++) {
+                int a = i, b = j;
+                while (a < m && b < n) {
+                    if (mat[a][b] < mat[i][j])
+                        swap(mat[a][b], mat[i][j]);
+                    a++;
+                    b++;
+                }
             }
         }
-    }
+    public:
+        vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+            int m = mat.size(), n = mat[0].size();
+            for (int i = 0; i < n; i++) sort(0, i, m, n, mat);
+            for (int i = 1; i < m; i++) sort(i, 0, m, n, mat);
+            return mat;
+        }
+    };
+*/
+
+
+class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+
+        unordered_map<int, vector<int>> mp;
         int m = mat.size(), n = mat[0].size();
-        for (int i = 0; i < n; i++) sort(0, i, m, n, mat);
-        for (int i = 1; i < m; i++) sort(i, 0, m, n, mat);
+
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                mp[i-j].emplace_back(mat[i][j]);
+            }
+        }
+
+        for (int x=-(n-1); x<m; x++) {
+            sort(mp[x].begin(), mp[x].end());
+        }
+
+        for (int i=m-1; i>=0; i--) {
+            for (int j=n-1; j>=0; j--) {
+                mat[i][j] = mp[i-j].back();
+                mp[i-j].pop_back();
+            }
+        }
+
         return mat;
     }
 };
