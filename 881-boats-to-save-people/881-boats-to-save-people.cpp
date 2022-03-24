e50842f8bf57c729f06dc2ba4cd1b5bcca9604d4
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-03-24 14:15:11 +0530

    Time: 56 ms (100.00%), Space: 44.9 MB (5.14%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -1,13 +1,35 @@
+/*
+    class Solution {
+    public:
+        int numRescueBoats(vector<int>& people, int limit) {
+            int maxBoats = 0, r = people.size() - 1, l = 0;
+            sort(people.begin(), people.end());
+            while (l <= r) {
+                if (people[l] + people[r] <= limit) l++;
+                r--;
+                maxBoats++;
+            }
+            return maxBoats;
+        }
+    };
+*/
+
+
 class Solution {
 public:
-    int numRescueBoats(vector<int>& people, int limit) {
-        int maxBoats = 0, r = people.size() - 1, l = 0;
-        sort(people.begin(), people.end());
-        while (l <= r) {
-            if (people[l] + people[r] <= limit) l++;
-            r--;
-            maxBoats++;
+    int numRescueBoats(vector<int>& people, int limit) {    
+        vector<int> bucket(limit + 1,0);
+        int j = limit, i = 0;
+        for(auto x: people) bucket[x]++;
+        int curr = limit, ans = 0;
+        while(i <= j){
+            while(i<=j && bucket[i]<=0) i++;                            
+            while(i<=j && bucket[j]<=0) j--;
+            if (i>j || (bucket[j]==0 && bucket[i]==0)) break; 
+            ans++;
+            if (j+i <= limit) bucket[i]--;
+            bucket[j]--; 
         }
-        return maxBoats;
+        return ans; 
     }
 };
