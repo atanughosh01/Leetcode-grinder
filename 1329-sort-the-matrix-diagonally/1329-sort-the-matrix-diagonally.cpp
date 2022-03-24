edd1aec5492102bb631bc2c52f415ed7787a4713
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-03-24 19:44:00 +0530

    Time: 15 ms (55.78%), Space: 9.4 MB (54.34%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -23,30 +23,59 @@
 */
 
 
+
+/*
+    class Solution {
+    public:
+        vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+
+            unordered_map<int, vector<int>> mp;
+            int m = mat.size(), n = mat[0].size();
+
+            for (int i=0; i<m; i++) {
+                for (int j=0; j<n; j++) {
+                    mp[i-j].emplace_back(mat[i][j]);
+                }
+            }
+
+            for (int x=-(n-1); x<m; x++) {
+                sort(mp[x].begin(), mp[x].end());
+            }
+
+            for (int i=m-1; i>=0; i--) {
+                for (int j=n-1; j>=0; j--) {
+                    mat[i][j] = mp[i-j].back();
+                    mp[i-j].pop_back();
+                }
+            }
+
+            return mat;
+        }
+    };
+*/
+
+
+
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-
-        unordered_map<int, vector<int>> mp;
+        
+        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> mp;
         int m = mat.size(), n = mat[0].size();
-
-        for (int i=0; i<m; i++) {
-            for (int j=0; j<n; j++) {
-                mp[i-j].emplace_back(mat[i][j]);
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                mp[i-j].push(mat[i][j]);
             }
         }
-
-        for (int x=-(n-1); x<m; x++) {
-            sort(mp[x].begin(), mp[x].end());
-        }
-
-        for (int i=m-1; i>=0; i--) {
-            for (int j=n-1; j>=0; j--) {
-                mat[i][j] = mp[i-j].back();
-                mp[i-j].pop_back();
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                mat[i][j] = mp[i-j].top();
+                mp[i-j].pop();
             }
         }
-
+        
         return mat;
     }
 };
