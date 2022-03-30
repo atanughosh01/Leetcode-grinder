e4af118cd1aaa61cdb6994310c8a153275d9f280
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-03-30 11:05:35 +0530

    Time: 8 ms (35.04%), Space: 9.5 MB (52.00%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,15 +1,35 @@
+/*
+    // TC : O(n*log(m))
+    class Solution {
+    public:
+        bool searchMatrix(vector<vector<int>>& mat, int target) {
+            int n = mat.size(), m = mat[0].size();
+            for (int i=0; i<n; i++) {
+                int l = 0, r = m-1;
+                while (l <= r) {
+                    int mid = l + (r-l)/2;
+                    if (target == mat[i][mid]) return true;
+                    if (target < mat[i][mid]) r = mid - 1;
+                    if (target > mat[i][mid]) l = mid + 1;
+                }
+            }
+            return false;
+        }
+    };
+*/
+
+
+// TC : O(log(m*n))
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
         int n = mat.size(), m = mat[0].size();
-        for (int i=0; i<n; i++) {
-            int l = 0, r = m-1;
-            while (l <= r) {
-                int mid = l + (r-l)/2;
-                if (target == mat[i][mid]) return true;
-                if (target < mat[i][mid]) r = mid - 1;
-                if (target > mat[i][mid]) l = mid + 1;
-            }
+        int l = 0, r = m*n - 1;
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+            if (target == mat[mid/m][mid%m]) return true;
+            if (target < mat[mid/m][mid%m]) r = mid - 1;
+            if (target > mat[mid/m][mid%m]) l = mid + 1;
         }
         return false;
     }
