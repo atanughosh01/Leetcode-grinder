59cd414693b3280aaf0eec9d631fb673ead5b23c
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-03-30 10:53:10 +0530

    Added solution - LeetHub

--- Counts Zeros Xor Pairs - GFG/counts-zeros-xor-pairs.cpp
+++ Counts Zeros Xor Pairs - GFG/counts-zeros-xor-pairs.cpp
@@ -35,14 +35,26 @@ int main()
 // Problem : https://practice.geeksforgeeks.org/problems/counts-zeros-xor-pairs0349/1
 
 
-
-long long int calculate(int arr[], int n) {
-    long long count = 0;
-    for (int i=0; i<n; i++) {
-        for (int j=i+1; j<n; j++) {
-            if (arr[i] == arr[j])
-                count++;
+/*
+    long long int calculate(int arr[], int n) {
+        long long count = 0;
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
+                if (arr[i] == arr[j])
+                    count++;
+            }
         }
+        return count;
     }
-    return count;
+*/
+
+long long calculate(int a[], int n) {
+    unordered_map<int,int> mp;
+    for(int i=0; i<n; i++) mp[a[i]]++;
+    long long ans = 0;
+    for(auto it : mp) {
+        long long cnt = it.second;
+        ans += ((cnt)*(cnt-1))/2;
+    }
+    return ans;
 }
