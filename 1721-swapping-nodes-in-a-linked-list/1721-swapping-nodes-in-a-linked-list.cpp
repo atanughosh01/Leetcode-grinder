bbd7ccb113bc2d95e678ccc11afd52d94a50b121
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-04-04 22:26:37 +0530

    Time: 751 ms (77.56%), Space: 180.1 MB (89.18%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -10,26 +10,45 @@
  */
 
 
+/*
+    class Solution {
+    public:
+        ListNode* swapNodes(ListNode* head, int k) {
+            ListNode* left = head;
+            ListNode* right = head;
+            ListNode* curr = head;
+            int counter = 1;
+            while(curr!=NULL){
+                if (counter<k){
+                    left = left->next;
+                }
+                if (counter>k){
+                    right = right->next;
+                }
+                curr = curr->next;
+                counter++;
+            }
+            int temp = left->val;
+            left->val = right->val;
+            right->val = temp;
+            return head;
+        }
+    };
+*/
+
+
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        ListNode* left = head;
-        ListNode* right = head;
-        ListNode* curr = head;
-        int counter = 1;
-        while(curr!=NULL){
-            if (counter<k){
-                left = left->next;
+        ListNode *n1 = nullptr, *n2 = nullptr;
+        for (auto p = head; p != nullptr; p = p->next) {
+            n2 = n2 == nullptr ? nullptr : n2->next;
+            if (--k == 0) {
+                n1 = p;
+                n2 = head;
             }
-            if (counter>k){
-                right = right->next;
-            }
-            curr = curr->next;
-            counter++;
         }
-        int temp = left->val;
-        left->val = right->val;
-        right->val = temp;
+        swap(n1->val, n2->val);
         return head;
     }
 };
