6922840ec0c8a6803ae49b77a13e164859207eab
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-04-11 10:40:47 +0530

    Time: 7 ms (33.62%), Space: 7.1 MB (12.87%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,112 +1,92 @@
 /*
-class Solution {
-public:
-  int myAtoi(string s) {
-      
-    const int len = s.size();
+    class Solution {
+    public:
+      int myAtoi(string s) {
 
-    if(len == 0){
-        return 0;
-    }
-
-    int index = 0;
-
-    // skipping white spaces
-    while(index < len && s[index] == ' '){
-        ++index;
-    }
-
-    // to handle sign cases
-    bool isNegative = false;
-
-    if(index < len){
-
-      if(s[index] == '-'){
-        isNegative = true;
-        ++index;
-      } else if (s[index] == '+'){
-          ++index;
-      }
-
-    }
-
-    int result = 0;
-
-    // converting digit(in character form) to integer form
-    // iterate until non-digit character is not found or we can say iterate till found character is a digit
-    while(index < len && isDigit(s[index])){
-
-      // s[index] - '0' is to convert the char digit into int digit eg: '5' - '0' --> 5
-      // or else it will store the ASCII value of 5 i.e. 53,
-      // so we do 53(ASCII of 5) - 48(ASCII of 0(zero)) to get 5 as int
-      int digit = s[index] - '0';
-
-      // to avoid integer overflow
-      if(result > (INT_MAX / 10) || (result == (INT_MAX / 10) && digit > 7)){
-          return isNegative ? INT_MIN : INT_MAX;
-      }
-
-      result = (result * 10) + digit; // adding digits at their desired place-value
-
-      ++index;
-    }
-      
-    return isNegative ? -result : result;
-  }
-    
-private:
-  bool isDigit(char ch){
-    return ch >= '0' && ch <= '9';
-  }
-};
-*/
-
-class Solution {
-public:
-    int myAtoi(string s) {
-        
         const int len = s.size();
-        
+
         if(len == 0){
             return 0;
         }
-        
+
         int index = 0;
+
+        // skipping white spaces
         while(index < len && s[index] == ' '){
             ++index;
         }
 
-        if(index == len){
-            return 0;
+        // to handle sign cases
+        bool isNegative = false;
+
+        if(index < len){
+
+          if(s[index] == '-'){
+            isNegative = true;
+            ++index;
+          } else if (s[index] == '+'){
+              ++index;
+          }
+
         }
-        
+
+        int result = 0;
+
+        // converting digit(in character form) to integer form
+        // iterate until non-digit character is not found or we can say iterate till found character is a digit
+        while(index < len && isDigit(s[index])){
+
+          // s[index] - '0' is to convert the char digit into int digit eg: '5' - '0' --> 5
+          // or else it will store the ASCII value of 5 i.e. 53,
+          // so we do 53(ASCII of 5) - 48(ASCII of 0(zero)) to get 5 as int
+          int digit = s[index] - '0';
+
+          // to avoid integer overflow
+          if(result > (INT_MAX / 10) || (result == (INT_MAX / 10) && digit > 7)){
+              return isNegative ? INT_MIN : INT_MAX;
+          }
+
+          result = (result * 10) + digit; // adding digits at their desired place-value
+
+          ++index;
+        }
+
+        return isNegative ? -result : result;
+      }
+
+    private:
+      bool isDigit(char ch){
+        return ch >= '0' && ch <= '9';
+      }
+    };
+*/
+
+
+class Solution {
+private:
+    bool isDigit(char ch){
+        return (ch >= '0' && ch <= '9');
+    }
+public:
+    int myAtoi(string s) {
+        const int len = s.size();
+        if(len == 0) return 0;
+        int index = 0;
+        while(index < len && s[index] == ' ') ++index;
+        if(index == len) return 0;
         char ch;
         bool isNegative = (ch = s[index]) == '-';
-        
-        if(isNegative || ch == '+'){
-            ++index;
-        }
-		
+        if(isNegative || ch == '+') ++index;
         const int maxLimit = INT_MAX / 10;
         int result = 0;
-        while(index < len && isDigit(ch = s[index])){ 
-
+        while(index < len && isDigit(ch = s[index])){
             int digit = ch - '0';
-
             if(result > maxLimit || (result == maxLimit && digit > 7)){
-                return isNegative ? INT_MIN : INT_MAX;
+                return (isNegative ? INT_MIN : INT_MAX);
             }
-
             result = (result * 10) + digit;
-            
             ++index;
         }
-        
-        return isNegative ? -result : result;
-    }
-    
-    private:
-    bool isDigit(char ch){
-        return ch >= '0' && ch <= '9';
+        return (isNegative ? -result : result);
     }
 };
