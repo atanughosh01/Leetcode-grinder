e85905efdcff21ec710d83c93040d862acae8ce0
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-04-18 20:45:45 +0530

    Time: 16 ms (90.27%), Space: 24.1 MB (60.91%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -11,20 +11,43 @@
  */
 
 
+/*
+    // Recursive
+    class Solution {
+    private:
+        int res = 0;
+    public:
+        void inorder(TreeNode* root, int& k) {
+            if (!root)
+                return;
+            inorder(root->left, k);
+            if (--k == 0)
+                res = root->val;
+            inorder(root->right, k);
+        }
+        int kthSmallest(TreeNode* root, int k) {
+            inorder(root, k);
+            return res;
+        }
+    };
+*/
+
+
 class Solution {
-private:
-    int res = 0;
 public:
-    void inorder(TreeNode* root, int& k) {
-        if (!root)
-            return;
-        inorder(root->left, k);
-        if (--k == 0)
-            res = root->val;
-        inorder(root->right, k);
-    }
     int kthSmallest(TreeNode* root, int k) {
-        inorder(root, k);
-        return res;
+        stack<TreeNode*> s;
+        while (root || !s.empty()) {
+            while (root) {
+                s.push(root);
+                root = root->left;
+            }
+            root = s.top();
+            s.pop();
+            if (--k == 0)
+                return root->val;
+            root = root->right;
+        }
+        return -1;
     }
 };
