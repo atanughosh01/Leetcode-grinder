246abbd3d9d40131a7950edcb3ab727abde72d81
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-04-26 11:38:33 +0530

    Added solution - LeetHub

--- Eulerian Path in an Undirected Graph - GFG/eulerian-path-in-an-undirected-graph.cpp
+++ Eulerian Path in an Undirected Graph - GFG/eulerian-path-in-an-undirected-graph.cpp
@@ -15,14 +15,14 @@ using namespace std;
 class Solution{
 public:
     int eulerPath(int N, vector<vector<int>> &graph){
-        vector<int>degree(N,0);
+        vector<int> degree(N,0);
         for(int i=0; i<N; ++i) {
-            int count=0;
+            int count = 0;
             for(int j=0; j<N; ++j) {
                 if(graph[i][j] == 1)
                     count++;
             }
-           degree[i]=count;
+           degree[i] = count;
         }
         int oddVertex = 0;
         for(int i=0; i<N; ++i) {
