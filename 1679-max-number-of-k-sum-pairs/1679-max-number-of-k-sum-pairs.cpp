f84f873847e9c537b38159ac1d9238259cf736d1
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-05-04 11:28:14 +0530

    Time: 185 ms (56.80%), Space: 67.6 MB (48.57%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,19 +1,42 @@
+/*
+    class Solution {
+    public:
+        int maxOperations(vector<int> &nums, int k) {
+            int cnt = 0;
+            unordered_map<int, int> mp;
+            for (int num : nums) mp[num]++;
+            for (int num : nums) {
+                auto itr = mp.find(k - num);
+                if (itr != mp.end() && mp[k - num] > 0) {
+                    mp[k - num]--;
+                    if (mp[num] > 0) {
+                        mp[num]--;
+                        cnt++;
+                    }
+                }
+            }
+            return cnt;
+        }
+    };
+*/
+
+
 class Solution {
 public:
-	int maxOperations(vector<int> &nums, int k) {
-		int cnt  = 0;
-		unordered_map<int, int> mp;
-		for (int num : nums) mp[num]++;
-		for (int num : nums) {
-			auto itr = mp.find(k - num);
-			if (itr != mp.end() && mp[k - num] > 0) {
-				mp[k - num]--;
-				if (mp[num] > 0) {
-					mp[num]--;
-					cnt++;
-				}
-			}
-		}
-		return cnt;
-	}
+    int maxOperations(vector<int>& nums, int k) {
+      int cnt = 0;
+      unordered_map<int, int> mp;
+      for (int num : nums) mp[num]++;           // count freq of nums
+      for (auto itr=mp.begin(); itr!=mp.end(); itr++) {
+        int num = itr->first, freq = itr->second;
+        if (k - num == num) cnt += freq/2;      // if num is half of k add half of it's count in ans
+        else if (mp.count(k - num)) {           // find k-num in nums and add min freq of num or k-num to ans
+          int mn = min(freq, mp[k-num]);
+          cnt += mn;
+          mp[num] -= mn;
+          mp[k-num] -= mn;
+        }
+      }
+      return cnt;
+    }
 };
