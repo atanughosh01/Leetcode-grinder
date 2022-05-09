6ce3a04a3ad08d8b535ce403cdc5398fd64cf14c
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-05-09 12:10:45 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (36.95%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -1,7 +1,15 @@
 class Solution {
-public:
-    static int removeElement(vector<int> &nums, int val) {
-        auto itr = remove(nums.begin(), nums.end(), val);
-        return distance(nums.begin(), itr);
+   public:
+    int removeElement(vector<int> &nums, int val) {
+        int len = nums.size(), idx = 0;
+        while (idx < len) {
+            if (nums[idx] == val) {
+                nums[idx] = nums[len - 1];
+                len--;
+            } else {
+                idx++;
+            }
+        }
+        return len;
     }
 };
