985520e12013d5b9cced4d213338636320b6618a
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-05-15 10:49:16 +0530

    Time: 186 ms (11.86%), Space: 61.9 MB (27.92%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -10,21 +10,49 @@
  * };
  */
 
+
+/*
+    class Solution {
+    public:
+        void dfs(TreeNode* root, int n, int &deepestSum, int &h){
+            if (!root) return;
+            if (n==h) deepestSum+=root->val;
+            if (n > h) {
+                deepestSum = root->val;
+                h = n;
+            }
+            dfs(root->left, n+1, deepestSum, h);
+            dfs(root->right, n+1, deepestSum, h);
+        }
+        int deepestLeavesSum(TreeNode* root) {
+            int deepestSum = 0, h = 1;
+            dfs(root, 1, deepestSum, h);
+            return deepestSum;
+        }
+    };
+*/
+
+
 class Solution {
 public:
-    void dfs(TreeNode* root, int n, int &deepestSum, int &h){
-        if (!root) return;
-        if (n==h) deepestSum+=root->val;
-        if (n > h) {
-            deepestSum = root->val;
-            h = n;
-        }
-        dfs(root->left, n+1, deepestSum, h);
-        dfs(root->right, n+1, deepestSum, h);
-    }
     int deepestLeavesSum(TreeNode* root) {
-        int deepestSum = 0, h = 1;
-        dfs(root, 1, deepestSum, h);
-        return deepestSum;
+        if (root==NULL) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int sum = 0;
+        while (!q.empty()) {
+            int n = (int)(q.size()); 
+            sum = 0;
+            for (int i=0; i<n; i++){
+                TreeNode* front = q.front();
+                q.pop();
+                sum += front->val;
+                if (front->left != NULL)
+                    q.push(front->left);
+                if (front->right != NULL)
+                    q.push(front->right);
+            }
+        }
+        return sum;
     }
 };
