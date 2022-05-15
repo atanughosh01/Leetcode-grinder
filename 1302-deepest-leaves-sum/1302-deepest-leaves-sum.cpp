af910468c441ed57a6a189a62f501e5c137643cc
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-05-15 10:51:24 +0530

    Time: 150 ms (30.40%), Space: 59.8 MB (53.32%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -33,26 +33,53 @@
 */
 
 
+/*
+    class Solution {
+    public:
+        int deepestLeavesSum(TreeNode* root) {
+            if (root==NULL) return 0;
+            queue<TreeNode*> q;
+            q.push(root);
+            int sum = 0;
+            while (!q.empty()) {
+                int n = (int)(q.size()); 
+                sum = 0;
+                for (int i=0; i<n; i++){
+                    TreeNode* front = q.front();
+                    q.pop();
+                    sum += front->val;
+                    if (front->left != NULL)
+                        q.push(front->left);
+                    if (front->right != NULL)
+                        q.push(front->right);
+                }
+            }
+            return sum;
+        }
+    };
+*/
+
+
 class Solution {
+private:
+    int deepestLevel = 1, sum = 0;
+    void traverse (TreeNode* root , int level){
+        if (!root) return;
+        if (level == deepestLevel){
+            sum += root->val;
+        }
+        if (level > deepestLevel){
+            deepestLevel = level;
+            sum = root->val;
+        }
+        traverse(root->left, level +1);
+        traverse(root->right, level + 1);
+    }
 public:
     int deepestLeavesSum(TreeNode* root) {
-        if (root==NULL) return 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        int sum = 0;
-        while (!q.empty()) {
-            int n = (int)(q.size()); 
-            sum = 0;
-            for (int i=0; i<n; i++){
-                TreeNode* front = q.front();
-                q.pop();
-                sum += front->val;
-                if (front->left != NULL)
-                    q.push(front->left);
-                if (front->right != NULL)
-                    q.push(front->right);
-            }
-        }
+        if (!root) return 0;
+        deepestLevel = 1, sum = 0;
+        traverse(root, 1);
         return sum;
     }
 };
