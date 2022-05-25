4f4e9fefe63dd49cd77345113e63a599cca49ea3
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-05-25 18:59:01 +0530

    Added solution - LeetHub

--- Maximum GCD of siblings of a binary tree - GFG/maximum-gcd-of-siblings-of-a-binary-tree.cpp
+++ Maximum GCD of siblings of a binary tree - GFG/maximum-gcd-of-siblings-of-a-binary-tree.cpp
@@ -11,18 +11,50 @@ using namespace std;
 
 
 /*
-// 1:- C++ solution having time complexity as O(E*log(E)) and space complexity as O(1) is as follows :-
+    // 1:- C++ solution having time complexity as O(E*log(E)) and space complexity as O(1) is as follows :-
+    
+    class Solution {
+       public:
+        int maxBinTreeGCD(vector<vector<int>> &arr, int N) {
+            if (N <= 2) return (0);
+            sort(arr.begin(), arr.end());
+            int ans = 0;
+            for (int i = 0; i < arr.size() - 1; i++) {
+                if (arr[i][0] == arr[i + 1][0]) {
+                    int gcd = __gcd(arr[i][1], arr[i + 1][1]);
+                    ans = max(ans, gcd);
+                }
+            }
+            return ans;
+        }
+    };
+*/
+
+
+// 2 :- C++ solution having time complexity as O(E) and space complexity as O(N+E) as follows :-
 
 class Solution {
-   public:
+private:
+    int find_max(vector<vector<int>> &arr) {
+        int mx = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            mx = max(mx, arr[i][0]);
+        }
+        return mx;
+    }
+public:
     int maxBinTreeGCD(vector<vector<int>> &arr, int N) {
-        if (N <= 2) return (0);
-        sort(arr.begin(), arr.end());
+        if (N <= 2) return 0;
+        vector<int> mp[find_max(arr) + 1], num;
+        for (int i = 0; i < arr.size(); i++) {
+            mp[arr[i][0]].push_back(arr[i][1]);
+            num.push_back(arr[i][0]);
+        }
         int ans = 0;
-        for (int i = 0; i < arr.size() - 1; i++) {
-            if (arr[i][0] == arr[i + 1][0]) {
-                int gcd = __gcd(arr[i][1], arr[i + 1][1]);
-                ans = max(ans, gcd);
+        for (int i = 0; i < num.size(); i++) {
+            int n = num[i];
+            if (mp[n].size() == 2) {
+                ans = max(ans, __gcd(mp[n][0], mp[n][1]));
             }
         }
         return ans;
