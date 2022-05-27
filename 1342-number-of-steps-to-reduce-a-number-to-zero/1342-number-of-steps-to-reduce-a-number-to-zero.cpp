3d0bffeafd7a590b64ba9f5d90f93a19056cbde8
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-05-27 23:52:56 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (74.11%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,6 +1,25 @@
+/*
+    class Solution {
+    public:
+        int numberOfSteps(int num) {
+            return num == 0 ? 0 : log2(num) + bitset<32>(num).count();
+        }
+    };
+*/
+
+
 class Solution {
 public:
     int numberOfSteps(int num) {
-        return num == 0 ? 0 : log2(num) + bitset<32>(num).count();
+        int count = 0;
+        while (num) {
+            if (num % 2) { // odd
+                --num;
+            } else { // even
+                num >>= 1; // num /= 2
+            }
+            ++count;
+        }
+        return count;
     }
 };
