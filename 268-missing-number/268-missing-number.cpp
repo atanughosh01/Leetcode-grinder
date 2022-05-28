85891b7a1989ddf1a18fff9f8ce8be1d25c5f3ba
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-05-28 14:00:19 +0530

    Time: 28 ms (46.47%), Space: 18 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,7 +2,7 @@
     class Solution {
     public:
         int missingNumber(vector<int> &nums) {
-            int sum = 0, n = nums.size(); // sum of all numbers initially 0
+            int sum = 0, n = nums.size();
             int total = n*(n + 1)/2;
             for (auto num : nums) {
                 sum += num;
@@ -13,16 +13,30 @@
 */
 
 
+/*
+    class Solution {
+    public:
+        int missingNumber(vector<int> &nums) {
+            int n = nums.size();
+            sort(nums.begin(), nums.end());
+            for(int i=0; i<n; i++) {
+                if(nums[i] != i) {
+                    return nums[i]-1;
+                }
+            }
+            return nums[n-1]+1;
+        }
+    };
+*/
+
+
 class Solution {
 public:
     int missingNumber(vector<int> &nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<n; i++) {
-            if(nums[i] != i) {
-                return nums[i]-1;
-            }
+        int sum = 0, n = nums.size();
+        for (auto num : nums) {
+            sum += num;
         }
-        return nums[n-1]+1;
+        return (n*(n+1)/2) - sum;
     }
 };
