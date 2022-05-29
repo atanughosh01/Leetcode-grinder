91bc3423a0a85ad0429bcb34bc43ad86a16ae219
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-05-29 22:59:04 +0530

    Time: 48 ms (88.18%), Space: 16.7 MB (46.02%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -2,23 +2,28 @@
     class Solution {
     public:
         int maxProduct(vector<string> &words) {
-            int best = 0, n = words.size();
-            vector<int> bitsets(n);
+            int ans = 0, n = words.size();
+            vector<vector<int>> dp(n, vector<int>(26));
             for (int i = 0; i < n; i++) {
-                string word = words[i];
-                int bitset = 0;
-                for (char &c : word) {
-                    bitset |= (1 << (c - 'a'));
+                for (int j = 0; j < words[i].size(); j++) {
+                    dp[i][words[i][j] - 'a']++;
                 }
-                for (int j = 0; j < i; j++) {
-                    if ((bitsets[j] & bitset) == 0) {
-                        int temp = (int)(word.size() * words[j].size());
-                        best = max(best, temp);
+            }
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    bool common = true;
+                    for (int k = 0; k < 26; k++) {
+                        if (dp[i][k] > 0 && dp[j][k] > 0) {
+                            common = false;
+                        }
+                    }
+                    if (common) {
+                        int cur = words[i].size() * words[j].size();
+                        ans = max(ans, cur);
                     }
                 }
-                bitsets[i] = bitset;
             }
-            return best;
+            return ans;
         }
     };
 */
@@ -27,27 +32,22 @@
 class Solution {
 public:
     int maxProduct(vector<string> &words) {
-        int ans = 0, n = words.size();
-        vector<vector<int>> dp(n, vector<int>(26));
+        int best = 0, n = words.size();
+        vector<int> bitsets(n);
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < words[i].size(); j++) {
-                dp[i][words[i][j] - 'a']++;
+            string word = words[i];
+            int bitset = 0;
+            for (char &c : word) {
+                bitset |= (1 << (c - 'a'));
             }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                bool common = true;
-                for (int k = 0; k < 26; k++) {
-                    if (dp[i][k] > 0 && dp[j][k] > 0) {
-                        common = false;
-                    }
-                }
-                if (common) {
-                    int cur = words[i].size() * words[j].size();
-                    ans = max(ans, cur);
+            for (int j = 0; j < i; j++) {
+                if ((bitsets[j] & bitset) == 0) {
+                    int temp = (int)(word.size() * words[j].size());
+                    best = max(best, temp);
                 }
             }
+            bitsets[i] = bitset;
         }
-        return ans;
+        return best;
     }
 };
