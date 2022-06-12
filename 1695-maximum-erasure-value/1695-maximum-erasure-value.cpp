7943e86814508aa97d57a4f20172e66692b09bdf
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-06-12 22:34:13 +0530

    Time: 320 ms (80.20%), Space: 89.4 MB (96.72%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -22,21 +22,49 @@
 */
 
 
+
+/*
+    class Solution {
+    public:
+        int maximumUniqueSubarray(vector<int> &nums) {
+            int sum = 0, max_sum = INT_MIN;
+            unordered_set<int> st;
+            int l = 0, r = 0;
+            while (r < nums.size()) {
+                while (st.count(nums[r]) > 0) {
+                    st.erase(nums[l]);
+                    sum -= nums[l];
+                    l++;
+                }
+                sum += nums[r];
+                st.insert(nums[r++]);
+                max_sum = max(max_sum, sum);
+            }
+            return max_sum;
+        }
+    };
+*/
+
+
+
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int> &nums) {
         int sum = 0, max_sum = INT_MIN;
-        unordered_set<int> st;
+        int m[100001] = {0};
         int l = 0, r = 0;
         while (r < nums.size()) {
-            while (st.count(nums[r]) > 0) {
-                st.erase(nums[l]);
-                sum -= nums[l];
-                l++;
+            if (m[nums[r]]) {
+                while ( m[nums[r]]) {
+                    sum -= nums[l];
+                    m[nums[l]]--;
+                    l++;
+                }
             }
+            m[nums[r]]++;
             sum += nums[r];
-            st.insert(nums[r++]);
             max_sum = max(max_sum, sum);
+            r++;
         }
         return max_sum;
     }
