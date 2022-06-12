24492a596488a72351c8f7fcc6a9936327fec4ef
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-06-12 22:26:53 +0530

    Time: 615 ms (32.08%), Space: 127.2 MB (35.12%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -0,0 +1,43 @@
+/*
+    class Solution {
+    public:
+        int maximumUniqueSubarray(vector<int> &nums) {
+            int n = nums.size(), max_sum = INT_MIN;
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                unordered_map<int, int> mp;
+                for (int j = i; j < n; j++) {
+                    sum += nums[j];
+                    mp[nums[j]]++;
+                    if (mp[nums[j]] > 1) {
+                        sum -= nums[j];
+                        break;
+                    }
+                }
+                max_sum = max(max_sum, sum);
+            }
+            return max_sum;
+        }
+    };
+*/
+
+
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int> &nums) {
+        int sum = 0, max_sum = INT_MIN;
+        unordered_set<int> st;
+        int l = 0, r = 0;
+        while (r < nums.size()) {
+            while (st.count(nums[r]) > 0) {
+                st.erase(nums[l]);
+                sum -= nums[l];
+                l++;
+            }
+            sum += nums[r];
+            st.insert(nums[r++]);
+            max_sum = max(max_sum, sum);
+        }
+        return max_sum;
+    }
+};
