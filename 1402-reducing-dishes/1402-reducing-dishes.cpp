46edd40c7e54eb4542ec0cb37bcdded9e912f3e1
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-06-14 23:50:27 +0530

    Time: 3 ms (88.41%), Space: 7.9 MB (91.30%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int maxSatisfaction(vector<int>& s) {
-        sort(s.begin(),s.end());
-        int n = (int)s.size(), sum=0;
-        for(int i=n-1;i>0;i--)
-            s[i-1]+=s[i];
-        int i=0;
-        while(i<n){
-            if(s[i]>=0) sum+=s[i];
-            i++;
-        }
-        return sum;
-    }
+	int maxSatisfaction(vector<int> &s) {
+		sort(s.begin(), s.end());
+		int n = s.size(), sum = 0;
+		for (int i = n - 1; i > 0; i--) {
+			s[i - 1] += s[i];
+		}
+		for (int i = 0; i < n; i++) {
+			if (s[i] >= 0)
+				sum += s[i];
+		}
+		return sum;
+	}
 };
