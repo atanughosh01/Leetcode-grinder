bdeb02bf2ba96967824f24d2ab63416b7c700c70
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-06-14 22:25:51 +0530

    Added solution - LeetHub

---
+++ Smallest factorial number - GFG/smallest-factorial-number.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+
+// Problem : https://practice.geeksforgeeks.org/problems/smallest-factorial-number5929/1
+
+
+
+class Solution {
+public:
+    int findNum(int n) {
+        int l = 1, r = 5*n;
+        while (l < r) {
+           int mid = (l+r)/2, cnt = 0;
+           for (int i = 5; i <= mid; i *= 5){
+               cnt += (mid / i);
+           }
+           if (cnt < n) l = mid+1;
+           else r = mid;
+        }
+        return l;
+    }
+};
+
+
+
+// { Driver Code Starts.
+
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        Solution ob;
+        cout <<ob.findNum(n) << endl;
+    }
+return 0;
+}  // } Driver Code Ends
