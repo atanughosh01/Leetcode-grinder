d4e3134094238495b91fc68fa60d540c095df24f
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-06-14 23:53:25 +0530

    Time: 345 ms (13.49%), Space: 84.4 MB (27.82%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -2,7 +2,7 @@
     // B R U T E
     class Solution {
     public:
-        vector<int> canSeePersonsCount(vector<int>& heights) {
+        vector<int> canSeePersonsCount(vector<int> &heights) {
             int n = (int)heights.size();
             vector<int> ans(n, 1);
             int currMax = 0;
@@ -22,10 +22,10 @@
 */
 
 /*
-    // Optimised
+    // O P T I M I S E D
     class Solution {
     public:
-        vector<int> canSeePersonsCount(vector<int>& heights) {
+        vector<int> canSeePersonsCount(vector<int> &heights) {
             int n = (int)heights.size();
             vector<int> ans(n), stack;
             for (int i = 0; i < n; ++i) {
@@ -46,12 +46,13 @@
 
 class Solution {
 public:
-    vector<int> canSeePersonsCount(vector<int>& heights) {
+    vector<int> canSeePersonsCount(vector<int> &heights) {
         vector<int> mono;
         for (int i = heights.size() - 1; i >= 0; --i) {
             int h = heights[i], cnt = 0;
-            for (; !mono.empty() && mono.back() < h; ++cnt)
+            for (; !mono.empty() && mono.back() < h; ++cnt) {
                 mono.pop_back();
+            }
             heights[i] = cnt + !mono.empty();
             mono.push_back(h);
         }
