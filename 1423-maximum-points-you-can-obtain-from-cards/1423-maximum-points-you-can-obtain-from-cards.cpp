d0b80bc5194677b71649d8c396ee2b861f2d5908
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-06-26 16:15:51 +0530

    Time: 94 ms (40.77%), Space: 45.7 MB (10.69%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,42 @@
+/*
+    class Solution {
+    private:
+        int sumOfWindow(vector<int> &arr, int startIdx, int windowSize) {
+            int ans = 0;
+            for (int i = startIdx; i < startIdx + windowSize; i++) {
+                ans += arr[i];
+            }
+            return ans;
+        }
+    public:
+        int maxScore(vector<int> &cardPoints, int k) {
+            int n = (int)(cardPoints.size()), ans = INT_MIN;
+            int total_pts = accumulate(cardPoints.begin(), cardPoints.end(), 0);
+            for (int i = 0; i <= k; i++) {
+                int sumOfRest = total_pts - sumOfWindow(cardPoints, i, n - k);
+                ans = max(ans, sumOfRest);
+            }
+            return ans;
+        }
+    };
+*/
+
+
+class Solution {
+public:
+    int maxScore(vector<int> &cardPoints, int k) {
+        int n = (int)(cardPoints.size()), ans = INT_MIN;
+        vector<int> preSumFromStart(n+1, 0), preSumFromBack(n+1, 0);
+        for (int i = 0; i < n; i++) {
+            preSumFromStart[i + 1] = preSumFromStart[i] + cardPoints[i];
+        }
+        for (int i = n - 1; i > 0; i--) {
+            preSumFromBack[i] = preSumFromBack[i + 1] + cardPoints[i];
+        }
+        reverse(preSumFromBack.begin(), preSumFromBack.end());
+        for (int i = 0; i <= k; i++) {
+            ans = max(ans, (preSumFromStart[i] + preSumFromBack[k - i]));
+        }
+        return ans;
+    }
+};
