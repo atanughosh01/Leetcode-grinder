109541064bc2aa57601cfd7108d0ae4134442b76
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-06-27 01:00:38 +0530

    Added solution - LeetHub

--- Change Bits - GFG/change-bits.cpp
+++ Change Bits - GFG/change-bits.cpp
@@ -12,11 +12,32 @@ using namespace std;
 
 
 
+/*
+    class Solution {
+    public:
+        vector<int> changeBits(int n) {
+            int k = log2(n) + 1;
+            return {(pow(2,k)-1)-n, (pow(2,k)-1)};
+        }
+    };
+*/
+
+
 class Solution {
+private:
+    int bits(int N) {
+        int cnt = 0;
+        while (N){
+            N >>= 1;
+            cnt++;
+        }
+        return cnt;
+    }
 public:
-    vector<int> changeBits(int n) {
-        int k = log2(n) + 1;
-        return {(pow(2,k)-1)-n, (pow(2,k)-1)};
+    vector<int> changeBits(int N) {
+        int x = bits(N);
+        x = (1<<x) - 1;
+        return {x-N, x};
     }
 };
 
