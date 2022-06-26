bcb5647b165bc148aeea429e00a582299a65f430
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-06-26 16:46:19 +0530

    Time: 63 ms (93.08%), Space: 42.4 MB (80.56%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -44,17 +44,38 @@
 */
 
 
+/*
+    class Solution {
+    public:
+        int maxScore(vector<int> &cardPoints, int k) {
+            int n = (int)(cardPoints.size());
+            int l = 0, r  = n - k;
+            int total_pts = accumulate(cardPoints.begin() + r, cardPoints.end(), 0);
+            int ans = total_pts;
+            while (r < n) {
+                total_pts += (cardPoints[l] - cardPoints[r]);
+                ans = max(ans, total_pts);
+                l++, r++;
+            }
+            return ans;
+        }
+    };
+*/
+
+
 class Solution {
 public:
     int maxScore(vector<int> &cardPoints, int k) {
-        int n = (int)(cardPoints.size());
-        int l = 0, r  = n - k;
-        int total_pts = accumulate(cardPoints.begin() + r, cardPoints.end(), 0);
-        int ans = total_pts;
-        while (r < n) {
-            total_pts += (cardPoints[l] - cardPoints[r]);
-            ans = max(ans, total_pts);
-            l++, r++;
+        int total_sum = accumulate(cardPoints.begin(), cardPoints.end(), 0);
+        int ans = 0, window_sum  = 0, n = (int)(cardPoints.size());
+        if (n == k) return total_sum;
+        for (int i = 0; i < n-k-1; i++) {
+            window_sum += cardPoints[i];
+        }
+        for (int i = n-k-1; i < n; i++) {
+            window_sum += cardPoints[i];
+            ans = max(ans, total_sum - window_sum);
+            window_sum -= cardPoints[i - (n-k-1)];
         }
         return ans;
     }
