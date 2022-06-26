03de0acc1b9164288d84d334aa1cc075fb0e174f
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-06-26 16:34:24 +0530

    Time: 93 ms (41.98%), Space: 42.4 MB (48.14%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -22,20 +22,39 @@
 */
 
 
+/*
+    class Solution {
+    public:
+        int maxScore(vector<int> &cardPoints, int k) {
+            int n = (int)(cardPoints.size()), ans = INT_MIN;
+            vector<int> preSumFromStart(n+1, 0), preSumFromBack(n+1, 0);
+            for (int i = 0; i < n; i++) {
+                preSumFromStart[i + 1] = preSumFromStart[i] + cardPoints[i];
+            }
+            for (int i = n - 1; i >= 0; i--) {
+                preSumFromBack[i] = preSumFromBack[i + 1] + cardPoints[i];
+            }
+            reverse(preSumFromBack.begin(), preSumFromBack.end());
+            for (int i = 0; i <= k; i++) {
+                ans = max(ans, (preSumFromStart[i] + preSumFromBack[k - i]));
+            }
+            return ans;
+        }
+    };
+*/
+
+
 class Solution {
 public:
     int maxScore(vector<int> &cardPoints, int k) {
-        int n = (int)(cardPoints.size()), ans = INT_MIN;
-        vector<int> preSumFromStart(n+1, 0), preSumFromBack(n+1, 0);
-        for (int i = 0; i < n; i++) {
-            preSumFromStart[i + 1] = preSumFromStart[i] + cardPoints[i];
-        }
-        for (int i = n - 1; i >= 0; i--) {
-            preSumFromBack[i] = preSumFromBack[i + 1] + cardPoints[i];
-        }
-        reverse(preSumFromBack.begin(), preSumFromBack.end());
-        for (int i = 0; i <= k; i++) {
-            ans = max(ans, (preSumFromStart[i] + preSumFromBack[k - i]));
+        int n = (int)(cardPoints.size());
+        int l = 0, r  = n - k;
+        int total_pts = accumulate(cardPoints.begin() + r, cardPoints.end(), 0);
+        int ans = total_pts;
+        while (r < n) {
+            total_pts += (cardPoints[l] - cardPoints[r]);
+            ans = max(ans, total_pts);
+            l++, r++;
         }
         return ans;
     }
