1b1f392eded8f9aeed79ef075c905ff9e6a585cc
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-06-29 01:39:55 +0530

    Time: 61 ms (82.88%), Space: 14.7 MB (99.08%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    int minDeletions(string s) {
-        vector<int> freq(26, 0);
-        for (char c : s) ++freq[c - 'a'];
-        sort(freq.begin(), freq.end(), greater<int>());
-        int keep = 0, prev = INT_MAX;
-        for (int f : freq) {
-            if (!f || !prev) break;
-            prev = min(f, prev - 1);
-            keep += prev;
+    int minDeletions(string &s) {
+        vector<int> freq (26, 0);        
+        for (char c : s) freq[c - 'a']++;
+        sort(freq.begin(), freq.end());
+        int del = 0;
+        for (int i = 24; i >= 0; i--) {
+            if (freq[i] == 0) break;            
+            if (freq[i] >= freq[i+1]){
+                int prev = freq[i];
+                freq[i] = max(0, freq[i+1] -1);
+                del += (prev - freq[i]); 
+            }
         }
-        return s.size() - keep;
+        return del;
     }
 };
