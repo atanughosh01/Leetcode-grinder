1584ee5edaaf642a2bab99af0b4f852d505730d2
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-06-28 23:53:41 +0530

    Time: 36 ms (76.78%), Space: 6.9 MB (99.83%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int expandAroundCenter(string s, int left, int right) {
+    int expandAroundCenter(string &s, int left, int right) {
         int L = left, R = right;
         while (L >= 0 && R < s.length() && s[L] == s[R]) {
             L--;
@@ -9,15 +9,15 @@ public:
         return R - L - 1;
     }
 public:
-    string longestPalindrome(string s) {
+    string longestPalindrome(string &s) {
         int n = s.length();
-        if(n == 0) return "";
+        if (n == 0) return "";
         int start = 0, end = 0;
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int len1 = expandAroundCenter(s, i, i);
             int len2 = expandAroundCenter(s, i, i+1);
             int len = max(len1, len2);
-            if(len > end - start) {
+            if (len > end - start) {
                 start = i - (len - 1)/2;
                 end = i + len/2;
             }
