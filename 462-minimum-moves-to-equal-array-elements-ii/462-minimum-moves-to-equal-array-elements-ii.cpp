a0d56a1946f8fc2a0cc949738cba77dc618025fd
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2022-06-30 20:00:57 +0530

    Time: 13 ms (75.41%), Space: 10.8 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -5,19 +5,9 @@ public:
         if (n == 1) return 0;
         if (n == 2) return abs(nums[1] - nums[0]);
         sort(nums.begin(), nums.end());
-        if (n % 2) {
-            int tar = nums[n/2];
-            for (int num : nums) {
-                ans += abs(tar - num);
-            }
-        } else {
-            int tar1 = nums[n/2 - 1], a1 = 0;
-            int tar2 = nums[n/2], a2 = 0;
-            for (int num : nums) {
-                a1 += abs(tar1 - num);
-                a2 += abs(tar2 - num);
-            }
-            ans = min(a1, a2);
+        int tar = nums[n/2];
+        for (int num : nums) {
+            ans += abs(tar - num);
         }
         return ans;
     }
