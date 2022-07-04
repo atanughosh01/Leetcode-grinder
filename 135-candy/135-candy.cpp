c4fb06d825a2fa2f7b08f045ab253ffd6218c626
Atanu Ghosh <2001atanughosh@gmail.com>
2022-07-04 23:25:48 +0530

    Time: 45 ms (24.10%), Space: 17.2 MB (92.48%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,20 +1,52 @@
+/*
+    class Solution {
+    public:
+        int candy(vector<int> &ratings) {
+            int n = (int)(ratings.size());
+            vector<int> count(n, 1);
+            for (int i = 1; i < n; i++) {
+                if (ratings[i] > ratings[i-1]) {
+                    count[i] = count[i-1] + 1;
+                }
+            }
+            for (int i = n-2; i >= 0; i--) {
+                if (ratings[i] > ratings[i+1]) {
+                    count[i] = max(count[i], count[i+1] + 1);
+                }
+            }
+            int total = 0;
+            for (int c : count) total += c;
+            return total;
+        }
+    };
+*/
+
+
 class Solution {
 public:
-    int candy(vector<int> &ratings) {
+    int candy(vector<int>& ratings) {
         int n = (int)(ratings.size());
-        vector<int> count(n, 1);
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i-1]) {
-                count[i] = count[i-1] + 1;
+        int candy = n, i = 1;
+        while (i < n) {
+            if (ratings[i] == ratings[i-1]) {
+                i++;
+                continue;
             }
-        }
-        for (int i = n-2; i >= 0; i--) {
-            if (ratings[i] > ratings[i+1]) {
-                count[i] = max(count[i], count[i+1] + 1);
+            int peak = 0;
+            while (ratings[i] > ratings [i-1]) {
+                peak++;
+                candy += peak;
+                i++;
+                if (i == n) return candy;
             }
+            int valley = 0;
+            while (i < n && (ratings[i] < ratings[i-1])) {
+                valley++;
+                candy += valley;
+                i++;
+            }
+            candy -= min(peak, valley);
         }
-        int total = 0;
-        for (int c : count) total += c;
-        return total;
+        return candy;
     }
 };
