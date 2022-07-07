0ba3fd2b04dbfee02260bb007f1fde7e8fe25a9d
Atanu Ghosh <2001atanughosh@gmail.com>
2022-07-07 23:04:17 +0530

    Time: 9 ms (48.94%), Space: 6.2 MB (96.49%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isInterleave(string &s1, string &s2, string &s3) {
+        int n = s1.size(), m = s2.size();
+        if (s3.length() != n + m) {
+            return false;
+        }
+        bool table[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++)
+            for (int j = 0; j < m + 1; j++) {
+                if (i == 0 && j == 0) {
+                    table[i][j] = true;
+                } else if (i == 0) {
+                    table[i][j] =
+                        (table[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
+                } else if (j == 0) {
+                    table[i][j] =
+                        (table[i - 1][j] && s1[i - 1] == s3[i + j - 1]);
+                } else {
+                    table[i][j] =
+                        (table[i - 1][j] && s1[i - 1] == s3[i + j - 1]) ||
+                        (table[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
+                }
+            }
+
+        return table[n][m];
+    }
+};
