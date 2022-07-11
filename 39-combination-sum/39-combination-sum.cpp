e35be8c7b87962e05b24a7344aa92e70a53ec2e5
Atanu Ghosh <2001atanughosh@gmail.com>
2022-07-12 01:50:42 +0530

    Time: 23 ms (39.72%), Space: 19.9 MB (31.65%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& nums, int target) {
         vector<vector<vector<int>>> dp(target+1);
-        dp[0]={{}};
-        for(int &i : nums) {
-            for(int j=i; j<=target; j++) {
-                for(auto v : dp[j-i])  {
+        dp[0] = {{}};
+        for (int &i : nums) {
+            for (int j=i; j<=target; j++) {
+                for (auto v : dp[j-i])  {
                     v.push_back(i);
                     dp[j].push_back(v);
                 }
