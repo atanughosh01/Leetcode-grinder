5300b181b3c880b32254866ff4845dc3a8628d50
Atanu Ghosh <2001atanughosh@gmail.com>
2022-07-17 23:35:30 +0530

    Time: 2 ms (52.03%), Space: 6.6 MB (57.43%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,19 +1,18 @@
 class Solution {
 private:
-    vector<string> mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans;
+    vector<string> ans, mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     void helper(string &digits, int i, string combi){	
-        if(i == digits.size()) {
+        if (i == digits.size()) {
             ans.push_back(move(combi));    
         } else {
-            for(auto c : mappings[digits[i] - '2']) {
+            for (char c : mappings[digits[i] - '2']) {
                 helper(digits, i + 1, combi + c);
             }
         }
     }
 public:
     vector<string> letterCombinations(string digits) {
-        if (digits == "")
-            return ans;
+        if (digits == "") return ans;
         helper(digits, 0, "");              
         return ans;
     }
