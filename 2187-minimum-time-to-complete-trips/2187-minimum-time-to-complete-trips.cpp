5af63e622cfb9b20942dbe23c9a39ae1426f7321
Atanu Ghosh <2001atanughosh@gmail.com>
2022-07-19 23:44:51 +0530

    Time: 441 ms (70.88%), Space: 94.5 MB (25.30%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 private:
      bool check(vector<int> &arr, int lim, long long x) {
         long long sum = 0;
@@ -13,6 +13,31 @@ public:
         while (lowTime < highTime) {
             mid = lowTime + (highTime - lowTime) / 2;
             if (!check(times, totalTrips, mid)) {
+                lowTime = mid + 1;
+            } else {
+                highTime = mid;
+            }
+        }
+        return lowTime;
+    }
+};
+
+
+
+class Solution {
+public:
+    long long minimumTime(vector<int> &times, int totalTrips) {
+         auto check = [&](long long x) {
+            long long sum = 0;
+            for (int &t : times) sum += x / t;
+            return sum >= totalTrips;
+        };
+        int minTime = *min_element(times.begin(), times.end());
+        long long lowTime = 0, mid = 0;
+        long long highTime = totalTrips * 1ll * minTime;
+        while (lowTime < highTime) {
+            mid = lowTime + (highTime - lowTime) / 2;
+            if (!check(mid)) {
                 lowTime = mid + 1;
             } else {
                 highTime = mid;
