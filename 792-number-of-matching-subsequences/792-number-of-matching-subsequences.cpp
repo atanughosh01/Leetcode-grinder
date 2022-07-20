8636a24acd1a23ea52cd2fb7e925c5263c89836f
Atanu Ghosh <2001atanughosh@gmail.com>
2022-07-20 20:53:10 +0530

    Time: 529 ms (35.80%), Space: 50 MB (48.09%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,15 +1,48 @@
+class Solution_1 {
+private:
+    bool isSubSequence(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+        int j = 0;
+        for (int i = 0; i < n && j < m; i++) {
+            if (s1[j] == s2[i]) {
+                j++;
+            }
+        }
+        return (j == m);
+    }
+public:
+    int numMatchingSubseq(string str, vector<string> &words) {
+        int res = 0;
+        for (string &word : words) {
+            if (isSubSequence(word, str)) {
+                res++;
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
 class Solution {
 public:
-    int numMatchingSubseq(string s, vector<string>& words) {
-        vector<const char*> waiting[128];
-        for (auto &w : words)
-            waiting[w[0]].push_back(w.c_str());
-        for (char c : s) {
-            auto advance = waiting[c];
-            waiting[c].clear();
-            for (auto it : advance)
-                waiting[*++it].push_back(it);
+    int numMatchingSubseq(string str, vector<string> &words) {
+        int res = 0;
+        unordered_map<char, vector<int>> mp;
+        for (int i = 0; i < str.size(); i++) {
+            mp[str[i]].push_back(i);
         }
-        return waiting[0].size();
+        for (int i = 0; i < words.size(); i++) {
+            string curr = words[i];
+            int latest = -1;
+            for (int j = 0; j < curr.size(); j++) {
+                auto it = upper_bound(mp[curr[j]].begin(), mp[curr[j]].end(), latest);
+                if (it == mp[curr[j]].end()) break;
+                latest = *it;
+                if (j == curr.size() - 1) res++;
+            }
+        }
+        return res;
     }
 };
