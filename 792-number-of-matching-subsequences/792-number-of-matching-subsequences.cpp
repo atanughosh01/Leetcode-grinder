a28e97952e1a557857d536a42c4e3c004d2347ef
Atanu Ghosh <2001atanughosh@gmail.com>
2022-07-20 23:38:16 +0530

    Time: 316 ms (62.05%), Space: 58 MB (40.42%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,3 +1,4 @@
+// T.L.E.
 class Solution_1 {
 private:
     bool isSubSequence(string s1, string s2) {
@@ -21,7 +22,6 @@ public:
         return res;
     }
 };
-
 
 
 
@@ -49,9 +49,7 @@ public:
 
 
 
-
-
-class Solution {
+class Solution_3 {
 public:
     int numMatchingSubseq(string str, vector<string> &words) {
         int res = 0;
@@ -71,6 +69,40 @@ public:
                 latest = *it;
             }
             if (found) res++;
+        }
+        return res;
+    }
+};
+
+
+
+// COPIED faster solution, but couldn't properly understand
+struct Node {
+    const string &word;
+    int index;
+    Node(const string &word, int index) : word(word), index(index) {}
+};
+
+class Solution {
+public:
+    int numMatchingSubseq(string &s, vector<string>& words) {
+        vector<Node> buckets[26];
+        for (string &word : words) {
+            char startingChar = word[0];
+            buckets[startingChar-'a'].push_back(Node(word, 0));
+        }
+        int res = 0;
+        for (char &c : s) {
+            auto currBucket = buckets[c-'a'];
+            buckets[c-'a'].clear();
+            for (auto &node : currBucket) {
+                node.index++;
+                if (node.index == node.word.size()) {
+                    res++;
+                } else {
+                    buckets[node.word[node.index]-'a'].push_back(move(node));
+                }
+            }
         }
         return res;
     }
