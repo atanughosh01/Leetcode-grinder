2f5db260319d2882cd3f3ccde1764a8352c14ac5
Atanu Ghosh <2001atanughosh@gmail.com>
2022-07-24 10:19:28 +0530

    Time: 8 ms (42.19%), Space: 9.4 MB (95.11%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,35 +1,34 @@
-/*
-    // TC : O(n*log(m))
-    class Solution {
-    public:
-        bool searchMatrix(vector<vector<int>>& mat, int target) {
-            int n = mat.size(), m = mat[0].size();
-            for (int i=0; i<n; i++) {
-                int l = 0, r = m-1;
-                while (l <= r) {
-                    int mid = l + (r-l)/2;
-                    if (target == mat[i][mid]) return true;
-                    if (target < mat[i][mid]) r = mid - 1;
-                    if (target > mat[i][mid]) l = mid + 1;
-                }
+// TC : O(m*log(n))
+class Solution_1 {
+public:
+    bool searchMatrix(vector<vector<int>> &mat, int target) {
+        int m = mat.size(), n = mat[0].size();
+        for (int i = 0; i < m; i++) {
+            int l = 0, r = n - 1;
+            while (l <= r) {
+                int mid = l + (r - l)/2;
+                if (target == mat[i][mid]) return true;
+                if (target < mat[i][mid]) r = mid - 1;
+                if (target > mat[i][mid]) l = mid + 1;
             }
-            return false;
         }
-    };
-*/
+        return false;
+    }
+};
+
 
 
 // TC : O(log(m*n))
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& mat, int target) {
-        int n = mat.size(), m = mat[0].size();
+    bool searchMatrix(vector<vector<int>> &mat, int target) {
+        int m = mat.size(), n = mat[0].size();
         int l = 0, r = m*n - 1;
         while (l <= r) {
-            int mid = l + (r-l)/2;
-            if (target == mat[mid/m][mid%m]) return true;
-            if (target < mat[mid/m][mid%m]) r = mid - 1;
-            if (target > mat[mid/m][mid%m]) l = mid + 1;
+            int mid = l + (r - l)/2;
+            if (target == mat[mid/n][mid%n]) return true;
+            if (target < mat[mid/n][mid%n]) r = mid - 1;
+            if (target > mat[mid/n][mid%n]) l = mid + 1;
         }
         return false;
     }
