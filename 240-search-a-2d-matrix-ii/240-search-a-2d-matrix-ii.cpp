e770c7c4ce0859d4c0b655dd377d85a5e84796b0
Atanu Ghosh <2001atanughosh@gmail.com>
2022-07-24 11:08:41 +0530

    Time: 155 ms (60.24%), Space: 14.8 MB (68.84%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,53 @@
+// TC : O(m*log(n)), SC: O(1)
+class Solution_1 {
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        for (int i = 0; i < m; i++) {
+            int l = 0, r  = n - 1;
+            while (l <= r) {
+                int mid = l + (r - l)/2;
+                if (target == matrix[i][mid]) return true;
+                else if (target > matrix[i][mid]) l = mid + 1;
+                else if (target < matrix[i][mid]) r = mid - 1;
+            }
+        }
+        return false;
+    }
+};
+
+
+
+// TC : O(n*log(m)), SC: O(1)
+class Solution_2 {
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        for (int i = 0; i < n; i++) {
+            int l = 0, r  = m - 1;
+            while (l <= r) {
+                int mid = l + (r - l)/2;
+                if (target == matrix[mid][i]) return true;
+                else if (target > matrix[mid][i]) l = mid + 1;
+                else if (target < matrix[mid][i]) r = mid - 1;
+            }
+        }
+        return false;
+    }
+};
+
+
+
+// TC : O(log(m+n)), SC: O(1)
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target) {
+        int row = 0, col = matrix[0].size() - 1;
+        while (col >= 0 && row <= matrix.size() - 1) {
+            if (target == matrix[row][col]) return true;
+            else if (target < matrix[row][col]) col--;
+            else if (target > matrix[row][col]) row++;
+        }
+        return false;
+    }
+};
