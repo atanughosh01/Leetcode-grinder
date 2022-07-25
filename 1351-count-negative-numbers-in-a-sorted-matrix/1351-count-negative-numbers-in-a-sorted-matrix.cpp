a465f0ceddf04c4d3b64c5836738962bdfdccd09
Atanu Ghosh <2001atanughosh@gmail.com>
2022-07-26 02:46:42 +0530

    Time: 22 ms (62.64%), Space: 10.5 MB (10.48%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// TC : O(m*n), SC : O(1)
+class Solution_1 {
 public:
     int countNegatives(vector<vector<int>> &grid) {
         int count = 0, n = grid[0].size();
@@ -7,6 +8,41 @@ public:
                 if (arr[i] < 0)  count++;
                 else break;
             }
+        }
+        return count;
+    }
+};
+
+
+
+// TC : O(m*log(n)), SC : O(1)
+class Solution {
+private:
+    int binarySearch(vector<int> &arr, int l, int r) {
+        if (arr.size() == 1 && arr[0] < 0) {
+            return -1;
+        }
+        if (arr.back() >= 0) {
+            return arr.size() - 1;
+        }
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] >= 0 && arr[mid + 1] < 0) {
+                return mid;
+            } else if (arr[mid] < 0 && arr[mid + 1] < 0) {
+                r = mid - 1;    // binarySearch(arr, 0, mid - 1);
+            } else {
+                l = mid + 1;    // binarySearch(arr, mid + 1, r);
+            }
+        }
+        return -1;
+    }
+public:
+    int countNegatives(vector<vector<int>> &grid) {
+        int count = 0, n = grid[0].size();
+        for (vector<int> &vec : grid) {
+            int nonNeg = binarySearch(vec, 0, vec.size()-1);
+            count += (n - nonNeg - 1);
         }
         return count;
     }
