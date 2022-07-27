952573613820c7ff58c8c86b899e859e7ded9b30
Atanu Ghosh <2001atanughosh@gmail.com>
2022-07-27 15:46:03 +0530

    Time: 195 ms (38.59%), Space: 42.1 MB (55.49%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,8 +1,17 @@
-class MyHashSet {
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
+
+
+class MyHashSet_1 {
 public:
-    vector<bool> hash_table = {};
+    vector<bool> hash_table;
     int len = 1e6 + 1;
-    MyHashSet() {
+    MyHashSet_1() {
         hash_table.resize(len, false);
     }
     
@@ -20,10 +29,41 @@ public:
 };
 
 
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet* obj = new MyHashSet();
- * obj->add(key);
- * obj->remove(key);
- * bool param_3 = obj->contains(key);
- */
+
+class MyHashSet {
+private:
+    int prime;
+    vector<list<int>> table;
+    
+    int hash(int key) {
+        return key % prime;
+    }
+    
+    list<int>::iterator search(int key) {
+        int h = hash(key);
+        return find(table[h].begin(), table[h].end(), key);
+    }
+public:
+    MyHashSet() : prime(1000), table(prime) {}
+    
+    void add(int key) {
+        int h = hash(key);
+        if (!contains(key)) {
+            table[h].push_back(key);
+        }
+    }
+    
+    void remove(int key) {
+        int h = hash(key);
+        auto it = search(key);
+        if (contains(key)) {
+            table[h].erase(it);
+        }
+    }
+    
+    bool contains(int key) {
+        int h = hash(key);
+        auto it = search(key);
+        return it != table[h].end();
+    }
+};
