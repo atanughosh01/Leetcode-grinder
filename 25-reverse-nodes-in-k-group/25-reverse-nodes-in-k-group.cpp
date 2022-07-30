bf39237fdf235595530ebd75a675b6ff3a080c68
Atanu Ghosh <2001atanughosh@gmail.com>
2022-07-31 02:41:00 +0530

    Time: 16 ms (89.53%), Space: 11.5 MB (71.93%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -10,7 +10,8 @@
  */
 
 
-class Solution {
+// Recursive, stack space wastage, O(n) time, O(n/k) stack space
+class Solution_1 {
 private:
     void reverseFirstGroup(ListNode *left, ListNode *right) {
         ListNode *prv = nullptr;
@@ -39,5 +40,57 @@ public:
         reverseFirstGroup(start, end);
         start->next = headOfNext;
         return end;
+    }
+};
+
+
+
+// Iterative, O(n) time, O(1) space;
+class Solution {
+private:
+    void reverse(ListNode *left, ListNode *right) {
+        ListNode *prv = nullptr;
+        ListNode *cur = left;
+        ListNode *nxt = left->next;
+        while (prv != right) {
+            cur->next = prv;
+            prv = cur;
+            cur = nxt;
+            if (nxt != nullptr) {
+                nxt = nxt->next;
+            }
+        }
+    }
+public:
+    ListNode *reverseKGroup(ListNode *head, int k) {
+        if (head->next == nullptr || k == 1) {
+            return head;
+        }
+
+        ListNode *dummy = new ListNode(-1);
+        dummy->next = head;
+
+        ListNode *preStart = dummy, *postEnd = head;
+        ListNode *start = head, *end = head;
+        int grpLen = 0;
+
+        while (end != nullptr) {
+            grpLen++;
+            
+            if (grpLen % k == 0) {
+                start = preStart->next;
+                postEnd = end->next;
+                
+                reverse(start, end);
+                
+                preStart->next = end;
+                start->next = postEnd;
+                preStart = start;
+                end = postEnd;
+            } else {
+                end = end->next;
+            }
+        }
+        return dummy->next;
     }
 };
