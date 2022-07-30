efd27cc3f341b634941186d1d26e9e1940bdfc75
Atanu Ghosh <2001atanughosh@gmail.com>
2022-07-30 20:18:39 +0530

    Time: 205 ms (85.39%), Space: 100.3 MB (77.53%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,71 @@
+class SolutionWrong {
+private:
+    bool isSubset(string &a, string &b) {
+        string x = a, y = b;
+        sort(x.begin(), x.end());
+        sort(y.begin(), y.end());
+        for (char &c : y) {
+            if (!binary_search(x.begin(), x.end(), c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    vector<string> wordSubsets(vector<string> &words1, vector<string> &words2) {
+        vector<string> res;
+        for (int i = 0; i < words1.size(); i++) {
+            for (int j = 0; j < words2.size(); j++) {
+                if (!isSubset(words1[i], words2[j])) {
+                   break; 
+                }
+                if (j == words2.size() - 1 && isSubset(words1[i], words2[j])) {
+                    res.push_back(words1[i]);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+
+class Solution {
+private:
+    bool isSubset(int *big, int *small) {
+        for (int i = 0; i < 26; i++) {
+            if (small[i] > big[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    vector<string> wordSubsets(vector<string> &words1, vector<string> &words2) {
+        vector<string> res;
+        int *freq2 = new int[26]{0};
+    
+        for (string &str2 : words2) {
+            int *temp = new int[26]{0};
+            for (char &c : str2) {
+                temp[c-'a']++;
+                freq2[c-'a'] = max(freq2[c-'a'], temp[c-'a']);
+            }
+        }
+        
+        for (string &str1 : words1) {
+            int *freq1 = new int[26]{0};
+            for (char &c : str1) {
+                freq1[c-'a']++;
+            }
+            
+            if (isSubset(freq1, freq2)) {
+                res.push_back(str1);
+            }
+        }
+        
+        return res;
+    }
+};
