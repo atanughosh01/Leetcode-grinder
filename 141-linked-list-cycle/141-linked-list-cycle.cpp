ab7f13f2f0016370f5b6d7bc335ee0a09322e359
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-01 00:48:48 +0530

    Time: 22 ms (27.95%), Space: 8 MB (57.68%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -8,13 +8,28 @@
  */
 
 
-class Solution {
+class Solution_1 {
 public:
     bool hasCycle(ListNode *head) {
         ListNode *slow = head, *fast = head;
         while (fast != nullptr && fast->next != nullptr) {
             slow = slow->next;
             fast = fast->next->next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+};
+
+
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while (fast != nullptr && fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next->next;
             if (slow == fast) return true;
         }
         return false;
