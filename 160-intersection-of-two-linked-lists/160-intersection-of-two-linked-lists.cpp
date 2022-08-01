c1247db1a4a1f96806df810bb888746917307968
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-01 11:29:00 +0530

    Time: 42 ms (98.26%), Space: 14.6 MB (31.79%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,7 +8,8 @@
  */
 
 
-class Solution {
+// Most bruteforce, O(A.len * B.len) time, O(1) space
+class BruteForce {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode *a = headA, *b = headB;
@@ -21,5 +22,39 @@ public:
             a = a->next;
         }
         return nullptr;
+    }
+};
+
+
+
+// O(A.len * B.len) time, O(1) space
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int lenA = 0, lenB = 0;
+        ListNode *a = headA, *b = headB;
+        while (a != nullptr) {
+            lenA++;
+            a = a->next;
+        }
+        while (b != nullptr) {
+            lenB++;
+            b = b->next;
+        }
+        a = headA, b = headB;
+        if (lenA > lenB) {
+            swap(lenA, lenB);
+            swap(a, b);
+        }
+        while (lenB != lenA) {
+            lenB--;
+            b = b->next;
+        }
+        while (a != nullptr && b != nullptr) {
+            if (a == b) return a;
+            a = a->next;
+            b = b->next;
+        }
+        return a;
     }
 };
