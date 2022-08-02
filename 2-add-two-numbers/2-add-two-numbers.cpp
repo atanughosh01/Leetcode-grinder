a6baba74fe281cac0c7dbae47202eb2e2b6c60c2
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-03 01:10:26 +0530

    Time: 51 ms (64.20%), Space: 71.6 MB (12.23%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -9,18 +9,52 @@
  * };
  */
 
+
+class Solution_Recursive {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr) return l2;
+        if (l2 == nullptr) return l1;
+        
+        int sum = l1->val + l2->val;
+        ListNode *newNode = new ListNode(sum % 10);
+        newNode->next = addTwoNumbers(l1->next, l2->next);
+        
+        if (sum > 9) {
+            int carry = sum / 10;
+            ListNode *carryNode = new ListNode(carry);
+            newNode->next = addTwoNumbers(newNode->next, carryNode);
+        }
+        
+        return newNode;
+    }
+};
+
+
+
+// O(max(l1.len, l2.len)) time, O(max(l1.len, l2.len)) space
 class Solution {
 public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        ListNode preHead(0), *p = &preHead;
-        int extra = 0;
-        while (l1 || l2 || extra) {
-            if (l1) extra += l1->val, l1 = l1->next;
-            if (l2) extra += l2->val, l2 = l2->next;
-            p->next = new ListNode(extra % 10);
-            extra /= 10;
-            p = p->next;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *preHead = new ListNode(0);
+        ListNode *temp = preHead;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int sum = 0;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            ListNode *newNode = new ListNode(sum % 10);
+            temp->next = newNode;
+            temp = newNode;
         }
-        return preHead.next;
+        return preHead->next;
     }
 };
