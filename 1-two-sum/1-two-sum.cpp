8b0f9728ef4fb58a6e37fed3eee9736eb68a5cad
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-03 23:32:18 +0530

    Time: 633 ms (22.65%), Space: 10.3 MB (74.39%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,22 +1,24 @@
+// O(N^2) APPROACH
 class Solution {
 public:    
-    vector<int> twoSum(vector<int>& nums, int tar) {
-        
-        // O(N^2) APPROACH
-        /*
-        for(int i=0; i<(int)nums.size(); i++) {
-            for(int j=i+1; j<(int)nums.size(); j++) {
-                if(nums[i]+nums[j] == tar) {
+    vector<int> twoSum(vector<int> &nums, int tar) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i+1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == tar) {
                     return {i, j};
                 }
             }
         }
         return {};
-        */
-        
-        
-        // O(N) APPROACH
-        
+    }
+};
+
+
+
+// O(N) APPROACH
+class Solution_1 {
+public:    
+    vector<int> twoSum(vector<int> &nums, int tar) {        
         int n = (int)nums.size();
         unordered_map<int,int> ump;
         for(int i=0; i<n; i++) {
