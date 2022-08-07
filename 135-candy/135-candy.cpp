d353f253db5637ff196f3fca6958ec992ef71081
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-07 22:47:47 +0530

    Time: 27 ms (78.56%), Space: 17.1 MB (98.58%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,30 +1,28 @@
-/*
-    class Solution {
-    public:
-        int candy(vector<int> &ratings) {
-            int n = (int)(ratings.size());
-            vector<int> count(n, 1);
-            for (int i = 1; i < n; i++) {
-                if (ratings[i] > ratings[i-1]) {
-                    count[i] = count[i-1] + 1;
-                }
+class Solution_1 {
+public:
+    int candy(vector<int> &ratings) {
+        int n = (int)(ratings.size());
+        vector<int> count(n, 1);
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i-1]) {
+                count[i] = count[i-1] + 1;
             }
-            for (int i = n-2; i >= 0; i--) {
-                if (ratings[i] > ratings[i+1]) {
-                    count[i] = max(count[i], count[i+1] + 1);
-                }
-            }
-            int total = 0;
-            for (int c : count) total += c;
-            return total;
         }
-    };
-*/
+        for (int i = n-2; i >= 0; i--) {
+            if (ratings[i] > ratings[i+1]) {
+                count[i] = max(count[i], count[i+1] + 1);
+            }
+        }
+        int total = 0;
+        for (int c : count) total += c;
+        return total;
+    }
+};
 
 
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
+    int candy(vector<int> &ratings) {
         int n = (int)(ratings.size());
         int candy = n, i = 1;
         while (i < n) {
