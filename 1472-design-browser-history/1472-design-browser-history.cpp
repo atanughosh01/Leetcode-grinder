72d5b4b84510d909b3fed4e3a0c5bc3ec2f553b2
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-15 19:50:14 +0530

    Time: 354 ms (13.28%), Space: 122.6 MB (5.56%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.cpp
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -1,17 +1,19 @@
-class BrowserHistory {
+// BrowserHistory_Solution_Using_Doubly_LinkedList
+class BrowserHistory_LL {
 private:
+    // STL Doubly LinkedList in c++
     list<string> history;
     list<string> :: iterator it;
 
 public:
-    BrowserHistory(string homepage) {
+    BrowserHistory_LL(string homepage) {
         history.push_back(homepage);
         it = history.begin();
     }
     
     void visit(string url) {
         auto del = it;
-        del++;
+        ++del;
         history.erase(del, history.end());
         history.push_back(url);
         it++;
@@ -33,6 +35,45 @@ public:
         return *it;
     }
 };
+
+
+
+
+// Solution Using Two Stacks
+class BrowserHistory {
+private:
+    stack<string> history;
+    stack<string> future;
+    
+public:
+    BrowserHistory(string homepage) {
+        history.push(homepage);
+        future = stack<string>();       // Reset the forward stack
+    }
+    
+    void visit(string url) {
+        history.push(url);
+        future = stack<string>();       // Reset the forward stack
+    }
+    
+    string back(int steps) {
+        while (steps-- && history.size() > 1) {  // Always keep at least one element in the stack
+            future.push(history.top());
+            history.pop();
+        }
+        return history.top();
+    }
+    
+    string forward(int steps) {
+        while(steps-- && future.size() > 0) {
+            history.push(future.top());
+            future.pop();
+        }
+        return history.top();
+    }
+};
+
+
 
 
 /**
