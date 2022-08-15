36e961ad4ae0a3e3ebfde12a0a9026c4198bb414
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-15 11:06:45 +0530

    Time: 28 ms (28.26%), Space: 7.8 MB (43.03%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -10,14 +10,14 @@ public:
             {'D', 500},
             {'M', 1000}
         };                      
-       int sum = T[s.back()];
-       for (int i = s.length() - 2; i >= 0; --i)  {
-           if (T[s[i]] < T[s[i + 1]]) {
-               sum -= T[s[i]];
-           } else {
+        int sum = T[s.back()];
+        for (int i = s.length() - 2; i >= 0; --i)  {
+            if (T[s[i]] < T[s[i + 1]]) {
+                sum -= T[s[i]];
+            } else {
                sum += T[s[i]];
-           }
-       }
-       return sum;
+            }
+        }
+        return sum;
     }
 };
