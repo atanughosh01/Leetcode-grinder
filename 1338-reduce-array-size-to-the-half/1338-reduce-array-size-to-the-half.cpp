a656a4d1776f28e5391ece0de66e9d137e0cdceb
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-19 23:42:44 +0530

    Time: 315 ms (60.56%), Space: 78.3 MB (63.73%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -3,7 +3,7 @@ public:
     int minSetSize(vector<int> &arr) {
         unordered_map<int, int> m;
         priority_queue<int> pq;
-        for (int n : arr) ++m[n];
+        for (int n : arr) m[n]++;
         for (auto &p : m) pq.push(p.second);
         int res = 0, cnt = 0;
         while (cnt < arr.size() / 2) {
