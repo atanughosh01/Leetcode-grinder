a3c11d9edf5ba63e0754568428b0def1ee392607
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-19 12:45:13 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (73.46%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -1,6 +1,15 @@
-class Solution {
+class Solution_1 {
 public:
     int sum(int num1, int num2) {
         return (num1 + num2);
     }
 };
+
+
+
+#define Actions class Solution {
+#define speak public: int sum(
+#define louder int num1, int num2) {
+#define than return num1 + num2; }
+#define words };
+Actions speak louder than words
