297e6df21f6da1007b5b16f8b8a99d04850f75e3
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-22 10:54:38 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (88.37%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,8 +1,18 @@
-class Solution {
+class Solution_1 {
 public:
     bool isPowerOfFour(int n) {
         long long a = 1;
         while (a < n) a *= 4;
         return a == n;
+    }
+};
+
+
+
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        long long n = (long long)num;
+        return (((n - 1) & n) == 0) && ((n - 1) % 3 == 0);
     }
 };
