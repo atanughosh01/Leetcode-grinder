935a3ce264193136431f560f36a74e5f59ae97a4
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-24 23:28:27 +0530

    Time: 34 ms (42.10%), Space: 13.2 MB (73.51%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,13 +1,11 @@
-/*
-
-class Solution {
+class Solution_1 {
 public:
-    int findPairs(vector<int>& nums, int k){
+    int findPairs(vector<int> &nums, int k) {
         int n = (int)nums.size();
         set<pair<int, int>> s;
-        for(int i=0; i<n; ++i){
-            for(int j=i+1; j<n; ++j){
-                if(abs(nums[i]-nums[j]) == k){
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (abs(nums[i] - nums[j]) == k) {
                     int ff = min(nums[i], nums[j]);
                     int ss = max(nums[i], nums[j]);
                     s.insert({ff, ss});
@@ -19,28 +17,28 @@ public:
 };
 
 
+
 // I have no idea why my Binary Search Implementation isn't working, LOL
-class Solution {
+class Solution_2 {
 private:
-    int binSrch(vector<int>& arr, int l, int r, int x){
+    int binSrch(vector<int> &arr, int l, int r, int x) {
         if (r >= l) {
             int mid = l + (r - l) / 2;
-            if (arr[mid] == x)
-                return mid;
-            if (arr[mid] > x)
-                return binSrch(arr, l, mid - 1, x);
+            if (arr[mid] == x) return mid;
+            if (arr[mid] > x) return binSrch(arr, l, mid - 1, x);
             return binSrch(arr, mid + 1, r, x);
         }
         return -1;
     }
-public:
-    int findPairs(vector<int>& nums, int k){
+
+   public:
+    int findPairs(vector<int> &nums, int k) {
         int n = (int)nums.size();
         set<pair<int, int>> s;
-        for(int i=0; i<n; ++i){
-            int tar = k+nums[i];
-            int idx = binSrch(nums, 0, n-1, tar);
-            if(idx != -1 and idx != i) {
+        for (int i = 0; i < n; ++i) {
+            int tar = k + nums[i];
+            int idx = binSrch(nums, 0, n - 1, tar);
+            if (idx != -1 and idx != i) {
                 s.insert({nums[i], tar});
             }
         }
@@ -48,20 +46,21 @@ public:
     }
 };
 
-*/
 
+
+// O(N)
 class Solution {
 public:
-  int findPairs(vector<int>& nums, int k) {
-    if (k < 0)
-        return 0;
-    unordered_map<int, int> mp;
-    for (int n : nums) mp[n]++;
-    int cnt = 0;
-    for (auto p : mp) {
-      if ((!k and p.second>1) or (k and mp.count(p.first+k)))
-          ++cnt;
+    int findPairs(vector<int> &nums, int k) {
+        if (k < 0) return 0;
+        unordered_map<int, int> mp;
+        for (int n : nums) mp[n]++;
+        int cnt = 0;
+        for (auto p : mp) {
+            if ((!k && p.second > 1) || (k && mp.count(p.first + k))) {
+                cnt++;
+            }
+        }
+        return cnt;
     }
-    return cnt;
-  }
 };
