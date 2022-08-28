0549b3158077a16d8a0e86751c9c050155972295
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-28 09:58:09 +0530

    Time: 66 ms (20.00%), Space: 13.5 MB (40.00%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,5 +1,5 @@
-class Solution {
-   public:
+class Solution_1 {
+public:
     vector<int> answerQueries(vector<int> &nums, vector<int> &queries) {
         sort(nums.begin(), nums.end());
         for (int i = 1; i < nums.size(); i++) {
@@ -14,5 +14,27 @@ class Solution {
             ans.emplace_back(j);
         }
         return ans;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> answerQueries(vector<int> &nums, vector<int> &queries) {
+        sort(nums.begin(), nums.end());
+        vector<int> res;
+        for (int i = 0; i < queries.size(); i++) {
+            int sum = 0, sz = 0;
+            for (int j = 0; j < nums.size(); j++) {
+                if (sum + nums[j] <= queries[i]) {
+                    sz++;
+                    sum += nums[j];
+                } else {
+                    break;
+                }
+            }
+            res.push_back(sz);
+        }
+        return res;
     }
 };
