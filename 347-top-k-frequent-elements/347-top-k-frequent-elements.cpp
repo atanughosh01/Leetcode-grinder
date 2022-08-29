659c02d9f3bfd76d249f33b233c6a10a6252cf82
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-29 22:05:46 +0530

    Time: 31 ms (30.58%), Space: 13.6 MB (89.47%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,18 +1,18 @@
 class Solution {
 private:
-    static bool compare(pair<int,int>& a, pair<int,int>& b){
+    static bool cmp(pair<int, int> &a, pair<int, int> &b){
        return a.second > b.second;
     }
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        for (int& num : nums)
-            mp[num]++;
-        vector<pair<int,int>> v(mp.begin(), mp.end());
-        sort(v.begin(), v.end(), compare);
+    vector<int> topKFrequent(vector<int> &nums, int k) {
+        unordered_map<int, int> mp;
+        for (int num : nums) mp[num]++;
+        vector<pair<int, int>> v(mp.begin(), mp.end());
+        sort(v.begin(), v.end(), cmp);
         nums.clear();
-        for(int i=0; i<k; i++)
-            nums.emplace_back(v[i].first);
+        for (int i = 0; i < k; i++) {
+            nums.push_back(v[i].first);
+        }
         return nums;
     }
 };
