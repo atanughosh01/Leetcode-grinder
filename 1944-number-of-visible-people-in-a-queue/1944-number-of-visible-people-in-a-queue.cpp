5b7dc832eb909ecbd167678ba6288fec15725828
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-30 22:02:41 +0530

    Time: 284 ms (54.21%), Space: 84.6 MB (24.47%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -1,47 +1,46 @@
-/*
-    // B R U T E
-    class Solution {
-    public:
-        vector<int> canSeePersonsCount(vector<int> &heights) {
-            int n = (int)heights.size();
-            vector<int> ans(n, 1);
-            int currMax = 0;
-            for(int i=0; i<n-1; ++i) {
-                currMax = heights[i+1];
-                for(int j=i+2; j<n; ++j) {
-                    if(heights[j] > currMax and heights[i] > currMax) {
-                        currMax = heights[j];
-                        ans[i]++;
-                    }
+// B R U T E
+class Solution_1 {
+public:
+    vector<int> canSeePersonsCount(vector<int> &heights) {
+        int n = (int)heights.size();
+        vector<int> ans(n, 1);
+        int currMax = 0;
+        for(int i=0; i<n-1; ++i) {
+            currMax = heights[i+1];
+            for(int j=i+2; j<n; ++j) {
+                if(heights[j] > currMax and heights[i] > currMax) {
+                    currMax = heights[j];
+                    ans[i]++;
                 }
             }
-            ans[n-1] = 0;
-            return ans;
         }
-    };
-*/
+        ans[n-1] = 0;
+        return ans;
+    }
+};
 
-/*
-    // O P T I M I S E D
-    class Solution {
-    public:
-        vector<int> canSeePersonsCount(vector<int> &heights) {
-            int n = (int)heights.size();
-            vector<int> ans(n), stack;
-            for (int i = 0; i < n; ++i) {
-                while (!stack.empty() and heights[stack.back()] <= heights[i]) {
-                    ans[stack.back()]++;
-                    stack.pop_back();
-                }
-                if (!stack.empty()) {
-                    ans[stack.back()]++;
-                }
-                stack.push_back(i);
+
+
+// O P T I M I S E D
+class Solution_2 {
+public:
+    vector<int> canSeePersonsCount(vector<int> &heights) {
+        int n = (int)heights.size();
+        vector<int> ans(n), stack;
+        for (int i = 0; i < n; ++i) {
+            while (!stack.empty() and heights[stack.back()] <= heights[i]) {
+                ans[stack.back()]++;
+                stack.pop_back();
             }
-            return ans;
+            if (!stack.empty()) {
+                ans[stack.back()]++;
+            }
+            stack.push_back(i);
         }
-    };
-*/
+        return ans;
+    }
+};
+
 
 
 class Solution {
