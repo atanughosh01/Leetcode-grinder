ebcc656b1f401fb6c8388c89baf0a5f1db2d6908
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-30 21:57:44 +0530

    Time: 35 ms (15.16%), Space: 16.4 MB (56.71%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     vector<int> buildArray(vector<int> &nums) {
         vector<int> res;
@@ -6,5 +6,21 @@ public:
             res.push_back(nums[num]);
         }
         return res;
+    }
+};
+
+
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int> &nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+			nums[i] = nums[i] + n * (nums[nums[i]] % n);
+		}
+		for (int i = 0; i < n; i++) {
+			nums[i] = nums[i] / n;
+		}
+		return nums;
     }
 };
