4c1a7ee6f5e7cae30998b135ce048e4a8944291d
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-30 21:43:06 +0530

    Time: 438 ms (58.77%), Space: 129.1 MB (24.48%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
@@ -1,34 +1,34 @@
-/*
-    class Solution {
-    public:
-        long long maxPoints(vector<vector<int>>& points) {
-            vector<vector<long long>> dp(points.size(), vector<long long>(points[0].size(), -1));
+class Solution_1 {
+public:
+    long long maxPoints(vector<vector<int>> &points) {
+        vector<vector<long long>> dp(points.size(), vector<long long>(points[0].size(), -1));
 
-            for (int i = 0; i < points[0].size(); ++i) {
-                dp[0][i] = points[0][i];
-            }
+        for (int i = 0; i < points[0].size(); ++i) {
+            dp[0][i] = points[0][i];
+        }
 
-            for (int i = 1; i < points.size(); ++i) {
-                for (int j = 0; j < points[i].size(); ++j) {
-                    for (int k = 0; k < points[i].size(); ++k) {
-                        dp[i][j] = max(dp[i][j], dp[i - 1][k] - abs(k - j) + points[i][j]);
-                    }
+        for (int i = 1; i < points.size(); ++i) {
+            for (int j = 0; j < points[i].size(); ++j) {
+                for (int k = 0; k < points[i].size(); ++k) {
+                    dp[i][j] = max(dp[i][j], dp[i - 1][k] - abs(k - j) + points[i][j]);
                 }
             }
-
-            long long max_ans = -1;
-            for (const auto v : dp.back()) {
-                max_ans = max(max_ans, v);
-            }
-
-            return max_ans;
         }
-    };
-*/
+
+        long long max_ans = -1;
+        for (const auto v : dp.back()) {
+            max_ans = max(max_ans, v);
+        }
+
+        return max_ans;
+    }
+};
+
+
 
 class Solution {
 public:
-    long long maxPoints(vector<vector<int>>& points) {
+    long long maxPoints(vector<vector<int>> &points) {
         vector<vector<long long>> dp(points.size(), vector<long long>(points[0].size(), -1));
         
         for (int i = 0; i < points[0].size(); ++i) {
@@ -55,7 +55,7 @@ public:
         }
         
         long long max_ans = -1;
-        for (const auto v : dp.back()) {
+        for (const auto &v : dp.back()) {
             max_ans = max(max_ans, v);
         }
         
