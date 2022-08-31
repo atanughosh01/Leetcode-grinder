d79a9a4e91e4b19aab4c029ec6d4d30922cfff70
Atanu Ghosh <2001atanughosh@gmail.com>
2022-08-31 23:41:18 +0530

    Time: 5 ms (87.91%), Space: 6.4 MB (72.28%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -5,7 +5,7 @@ public:
         res[0] = poured;
         for (int row = 1; row <= query_row; row++)
             for (int i = row; i >= 0; i--) {
-                res[i] = max(0.0, (res[i] - 1) / 2);
+                res[i] = max(0.0, (res[i] - 1.0) / 2.0);
                 res[i + 1] += res[i];
             }   
         return min(res[query_glass], 1.0);
