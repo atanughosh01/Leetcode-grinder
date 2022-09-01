745d0a74f80012e19882d7f7dc13bebee2533d7d
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-01 20:30:51 +0530

    Time: 7 ms (10.96%), Space: 5.9 MB (46.18%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,38 @@
+class Solution_1 {
+public:
+    double myPow(double x, int n) {
+        if (n == 0 || x == 1.0) return 1.0;            
+        double ans = 1.0;
+        if (n > 0) {
+            while (n--) {
+                ans *= x;
+            }
+        } else {
+            while (n++ < 0) {
+                ans /= x;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n < 0) {
+            x = 1 / x;
+        } 
+        long long num = labs(n);
+        double pow = 1;
+        while(num) {
+            if (num & 1) {
+                pow *= x;
+            }
+            x *= x;
+            num >>= 1;
+        }
+        return pow;
+    }
+};
