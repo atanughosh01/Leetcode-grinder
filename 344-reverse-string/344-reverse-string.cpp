f7728cd3b9827b9804429b9169b4ba8d672896cb
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-01 19:46:16 +0530

    Time: 55 ms (8.62%), Space: 23.2 MB (40.99%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,37 +1,40 @@
-/*
-    class Solution {
-    public:
-        void reverseString(vector<char>& s) {
-            int n = (int)s.size();
-            for (int i=0; i<n/2; i++) {
-                swap(s[i], s[n-i-1]);
-            }
-            return;
+class Solution_1 {
+public:
+    void reverseString(vector<char> &s) {
+        reverse(s.begin(), s.end());
+    }
+};
+
+
+class Solution_2 {
+public:
+    void reverseString(vector<char> &s) {
+        int n = s.size();
+        for (int i = 0; i < n/2; i++) {
+            swap(s[i], s[n - i - 1]);
         }
-    };
-*/
-
-
-
-/*
-    class Solution {
-    public:
-        void reverseString(vector<char>& s){
-            return reverse(s.begin(), s.end());
-        }
-    };
-*/
+    }
+};
 
 
 class Solution {
 public:
-    void reverseString(vector<char>& s){
-        for(int l = 0, r = s.size() - 1; l < r; ++l, --r) {
-            if(s[l] != s[r]) {
+    void reverseString(vector<char> &s) {
+        // for (int l = 0, r = s.size() - 1; l < r; ++l, --r) {
+        //     if (s[l] != s[r]) {
+        //         s[l] ^= s[r]; 
+        //         s[r] ^= s[l];
+        //         s[l] ^= s[r];
+        //     }
+        // }
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if (s[l] != s[r]) {
                 s[l] ^= s[r]; 
                 s[r] ^= s[l];
                 s[l] ^= s[r];
             }
+            l++, r--;
         }
     }
 };
