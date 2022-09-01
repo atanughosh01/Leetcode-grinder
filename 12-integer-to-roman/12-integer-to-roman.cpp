f441c56f3475dca4045a8770fcda3db94e52f265
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-01 19:06:36 +0530

    Time: 10 ms (69.76%), Space: 6 MB (79.09%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// O(N) Solution
+class Solution_1 {
 private:
     const int val[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     const string rom[13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
@@ -12,5 +13,20 @@ public:
             }
         }
         return roman;
+    }
+};
+
+
+
+// O(1) Solution
+class Solution {
+private:
+    const string ones[10] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    const string tens[10] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    const string hdrs[10] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    const string thsd[10] = {"", "M", "MM", "MMM"};
+public:
+    string intToRoman(int num) {
+        return thsd[num/1000] + hdrs[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 };
