ed3c4affe9d7e00cc78762c31e3c9853a8a91818
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-02 23:26:22 +0530

    Time: 17 ms (66.75%), Space: 8.4 MB (93.37%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,54 +1,37 @@
-/*
-
-class Solution {
+class Solution_1 {
 public:
-    string removeKdigits(string num, int k) {
-        while(k--) {  
-            int n = (int)num.size();
-            int idx = 0;
-            while(idx+1<n and num[idx]<=num[idx+1])
-                idx++;
+    string removeKdigits(string &num, int k) {
+        while (k--) {  
+            int n = num.size(), idx = 0;
+            while (idx+1<n && num[idx] <= num[idx+1]) idx++;
             num.erase(idx, 1);
         }
         int cnt = 0;
-        while (cnt<(int)num.size()-1 and num[cnt]=='0')
-            cnt++;
+        while (cnt<num.size()-1 && num[cnt]=='0') cnt++;
         num.erase(0, cnt);
-        
-        if(num=="")
-            return "0";
-        
+        if (num=="") return "0";
         return num;
     }
 };
 
-*/
-
 
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        
-        string res;
-        int n = (int)num.size();
-        int keep = n - k;
+        string res = "";
+        int n = num.size(), keep = n - k;
         for (int i=0; i<n; i++) {
-            while ((int)res.size()>0 and res.back()>num[i] and k>0) {
+            while (res.size()>0 && res.back()>num[i] && k>0) {
                 res.pop_back();
                 k--;
             }
             res.push_back(num[i]);
         }
         res.erase(keep, string::npos);
-        
         int cnt = 0;
-        while (cnt<(int)res.size()-1 and res[cnt]=='0')
-            cnt++;
+        while (cnt<res.size()-1 && res[cnt]=='0') cnt++;
         res.erase(0, cnt);
-        
-        if(res=="")
-            return "0";
-        
+        if (res=="") return "0";
         return res;
     }
 };
