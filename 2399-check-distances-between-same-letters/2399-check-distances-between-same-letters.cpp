cb188b1896bd85d71ddb7d35e265ec51c6666ea4
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-04 10:25:31 +0530

    Time: 13 ms (50.00%), Space: 13.1 MB (50.00%) - LeetHub

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.cpp
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// O(n) time, O(n) space, n = s.length
+class Solution_1 {
 public:
     bool checkDistances(string &s, vector<int> &dist) {
         unordered_map<char, vector<int>> mp;
@@ -9,6 +10,23 @@ public:
             if (dist[key - 'a'] != abs(val[1] - val[0] - 1)) {
                 return false;
             }
+        }
+        return true;
+    }
+};
+
+
+// O(n) time, O(1) space, n = s.length
+class Solution {
+public:
+    bool checkDistances(string &s, vector<int> &dist) {
+        int pos[26]{0};
+        for (int i = 0; i < s.size(); ++i) {
+            int n = s[i] - 'a';
+            if (pos[n] > 0 && dist[n] + pos[n] != i) {
+                return false;
+            }
+            pos[n] = i + 1;
         }
         return true;
     }
