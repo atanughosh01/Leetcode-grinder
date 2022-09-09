c43aeea0e0883e11b113bdceed4f5d93d951d5fa
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-09 20:26:10 +0530

    Time: 4 ms (66.45%), Space: 8.3 MB (38.44%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -1,5 +1,5 @@
 // Bruteforce | Time O(n^3) | Space O(1) | n = arr.length
-class Solution {
+class Solution_1 {
 public:
     int sumOddLengthSubarrays(vector<int> &arr) {
         int n = arr.size(), res = 0;
@@ -9,6 +9,26 @@ public:
                     for (int k = i; k <= j; k++) {
                         res += arr[k];
                     }
+                }
+            }
+        }
+        return res;
+    }
+};
+
+
+
+// Slightly Optimized | Time O(n^2) | Space O(1) | n = arr.length
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int> &arr) {
+        int n = arr.size(), res = 0, sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if ((j - i) % 2 == 0) {
+                    res += sum;
                 }
             }
         }
