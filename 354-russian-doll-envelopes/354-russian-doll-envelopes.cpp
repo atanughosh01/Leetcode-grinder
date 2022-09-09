d03ced1fe548bab7e6b27a50ac065d070fc99a74
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-09 12:04:11 +0530

    Time: 732 ms (24.77%), Space: 77.6 MB (67.39%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,21 +1,14 @@
 class Solution {
-private:
-    static bool cmp(const vector<int> &a, const vector<int> &b) {
-        if (a[0] == b[0]) {
-            return (a[1] > b[1]);
-        }
-        return (a[0] < b[0]);
-    }
 public:
     int maxEnvelopes(vector<vector<int>> &env) {
-        int n = env.size();
-        sort(env.begin(), env.end(), cmp);
+        sort(env.begin(), env.end(), [](const vector<int> &a, const vector<int> &b) {
+            return (a[0] == b[0]) ? (a[1] > b[1]) : (a[0] < b[0]);
+        });
         vector<int> res;
-        for (int i = 0; i < n; i++){
-            int elm = env[i][1];
-            int idx = lower_bound(res.begin(), res.end(), elm) - res.begin();
-            if (idx >= res.size()) res.push_back(elm);
-            else res[idx] = elm;
+        for (const vector<int> &v : env) {
+            int idx = lower_bound(res.begin(), res.end(), v[1]) - res.begin();
+            if (idx >= res.size()) res.push_back(v[1]);
+            else res[idx] = v[1];
         }
         return res.size();
     }
