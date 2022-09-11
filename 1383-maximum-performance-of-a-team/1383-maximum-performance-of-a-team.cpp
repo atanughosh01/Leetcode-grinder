8c896fd8c26e15be1e14706521a405c9a3d2cd2e
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-11 23:10:23 +0530

    Time: 176 ms (27.11%), Space: 35.4 MB (88.03%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int maxPerformance(int n, vector<int> &speed, vector<int> &efficiency, int k) {
         const int mod = 1e9 + 7;
-        vector<pair<int, int>> ess(n);
+        vector<pair<int, int>> engineers(n);
         for (int i = 0; i < n; ++i) {
-            ess[i] = {efficiency[i], speed[i]};
+            engineers[i] = {efficiency[i], speed[i]};
         }
-        sort(ess.rbegin(), ess.rend());
+        sort(engineers.rbegin(), engineers.rend());
         long long sumS = 0, res = 0;
         priority_queue<int, vector<int>, greater<int>> pq; //min heap
-        for (auto &[e, s] : ess) {
+        for (auto &[e, s] : engineers) {
             pq.emplace(s);
             sumS += s;
             if (pq.size() > k) {
