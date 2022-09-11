77297394a91bc689e352f11bb83b66f90edbc1b4
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-11 11:23:35 +0530

    Time: 50 ms (60.00%), Space: 9.2 MB (100.00%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// O(n) Time | O(n) Space | n = s.length
+class Solution_1 {
 public:
 	int partitionString(string &s) {
 		int cnt = 1;
@@ -13,5 +14,23 @@ public:
 			}
 		}
 		return cnt;
+	}
+};
+
+
+
+// O(n) Time | O(1) Space | n = s.length
+class Solution {
+public:
+	int partitionString(string &s) {
+		int pos[26]{0}, cnt = 0, lastIdx = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (pos[s[i] - 'a'] >= lastIdx) {
+                cnt++;
+                lastIdx = i + 1;
+            }
+            pos[s[i] - 'a'] = i + 1;
+        }
+        return cnt;
 	}
 };
