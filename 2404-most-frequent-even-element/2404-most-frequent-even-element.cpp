b15f8d50421f47f0316beeafefdd7d7deb5c5631
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-11 11:02:42 +0530

    Time: 173 ms (25.00%), Space: 38 MB (25.00%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// Two Pass | O(n) Time | O(n) Space | n = nums.length
+class Solution_1 {
 public:
     int mostFrequentEven(vector<int> &nums) {
         int cnt = 0;
@@ -15,5 +16,25 @@ public:
             }
         }
         return -1;
+    }
+};
+
+
+// One Pass | O(n) Time | O(n) Space | n = nums.length
+class Solution {
+public:
+    int mostFrequentEven(vector<int> &nums) {
+        unordered_map<int, int> mp;
+        int val = INT_MAX, freq = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                mp[num]++;
+                if (mp[num] > freq || (mp[num] == freq && num < val)) {
+                    val = num;
+                    freq = mp[num];
+                }
+            }
+        }
+        return freq ? val : -1;
     }
 };
