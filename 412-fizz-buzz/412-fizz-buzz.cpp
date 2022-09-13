588d58b222a8822f03ee69ac04631d143030eba9
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-13 23:49:01 +0530

    Time: 11 ms (9.33%), Space: 7.9 MB (63.77%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.cpp
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     vector<string> fizzBuzz(int n) {
         vector<string> res(n);
@@ -8,6 +8,30 @@ public:
             else if (i % 5 == 0) res[i-1] = "Buzz";
             else res[i-1] = to_string(i);
         }        
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> res;
+        for (int i = 1, fizz = 0, buzz = 0; i <= n; i++) {
+            fizz++, buzz++;
+            if (fizz == 3 && buzz == 5) {
+                res.push_back("FizzBuzz");
+                fizz = 0, buzz = 0;
+            } else if (fizz == 3) {
+                res.push_back("Fizz");
+                fizz = 0;
+            } else if (buzz == 5) {
+                res.push_back("Buzz");
+                buzz = 0;
+            } else {
+                res.push_back(to_string(i));
+            }
+        }
         return res;
     }
 };
