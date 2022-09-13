2ea0b9af94f2124f0cbb72980e08e6cc8e08ab29
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-13 23:32:43 +0530

    Time: 7 ms (85.46%), Space: 7.7 MB (62.51%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
@@ -5,7 +5,7 @@ private:
             c=='A' || c=='E' || c=='I' || c=='O' || c=='U';
     }
 public:
-    string reverseVowels(string s) {
+    string reverseVowels(string &s) {
         int i = 0, j = s.size()-1;
         while (i < j) {
             while (i < j && !isVowel(s[i])) i++;
