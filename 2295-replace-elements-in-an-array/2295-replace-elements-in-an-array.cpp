89ffb012c89fb5f0abd0ab4f012b551ae33e199c
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-14 23:27:10 +0530

    Time: 646 ms (73.16%), Space: 110.7 MB (98.50%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     vector<int> arrayChange(vector<int> &nums, vector<vector<int>> &oprs) {
         unordered_map<int, int> mp;
@@ -8,6 +8,25 @@ public:
         for (vector<int> &opr : oprs) {
             nums[mp[opr[0]]] = opr[1];
             mp[opr[1]] = mp[opr[0]];
+        }
+        return nums;
+    }
+};
+
+
+
+const int N = 1e6 + 2;
+int arr[N]{0};
+
+class Solution {
+public:
+    vector<int> arrayChange(vector<int> &nums, vector<vector<int>> &oprs) {
+        for (int i = 0; i < nums.size(); i++) {
+            arr[nums[i]] = i;
+        }
+        for (vector<int> &opr : oprs) {
+            nums[arr[opr[0]]] = opr[1];
+            arr[opr[1]] = arr[opr[0]];
         }
         return nums;
     }
