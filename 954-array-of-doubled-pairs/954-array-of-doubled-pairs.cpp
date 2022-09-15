c1ed48c6083e565de07abd98e57f388a71fb8419
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-15 22:52:52 +0530

    Time: 149 ms (82.50%), Space: 58 MB (55.00%) - LeetHub

--- 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
@@ -2,29 +2,6 @@ class Solution_1 {
 public:
     bool canReorderDoubled(vector<int> &arr) {
         unordered_map<int, int> mp;
-        vector<int> keys;
-        for (int &x : arr) mp[x]++;
-        for (auto &[key, val] : mp) {
-            keys.push_back(key);
-        }
-        sort(keys.begin(), keys.end(), [](const int &a, const int &b) {
-            return abs(a) < abs(b);
-        });
-        for (int &key : keys) {
-            if (mp[key] > mp[2 * key]) {
-                return false;
-            }
-            mp[2 * key] -= mp[key];
-        }
-        return true;
-    }
-};
-
-
-class Solution {
-public:
-    bool canReorderDoubled(vector<int> &arr) {
-        unordered_map<int, int> mp;
         long long sum = 0;
         for (int &x : arr) {
             sum += x;
@@ -43,6 +20,31 @@ public:
                 return false;
             }
             mp[2 * key] -= mp[key];
+        }
+        return true;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    bool canReorderDoubled(vector<int> &arr) {
+        sort(arr.begin(), arr.end());
+        unordered_map<int, int> mp;
+        long long sum = 0;
+        for (int &x : arr) {
+            sum += x;
+            mp[x]++;
+        }
+        if (sum % 3 != 0) return false;
+        for (int &x : arr) {
+            if (mp[x] == 0) continue;
+            if (x < 0 && x % 2 != 0) return false;
+            int y = x > 0 ? x*2 : x/2;
+            if (mp[y] == 0) return false;
+            mp[x]--, mp[y]--;
         }
         return true;
     }
