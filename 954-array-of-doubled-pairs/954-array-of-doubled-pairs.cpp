46e346e0b49d87c7dc256b020c27d9e3b2aa05d7
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-15 22:45:33 +0530

    Time: 259 ms (33.28%), Space: 57.7 MB (99.06%) - LeetHub

--- 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     bool canReorderDoubled(vector<int> &arr) {
         unordered_map<int, int> mp;
@@ -15,6 +15,24 @@ public:
                 return false;
             }
             mp[2 * key] -= mp[key];
+        }
+        return true;
+    }
+};
+
+
+class Solution {
+public:
+    bool canReorderDoubled(vector<int> &arr) {
+        sort(arr.begin(), arr.end());
+        unordered_map<int, int> mp;
+        for (int &x : arr) mp[x]++;
+        for (int &x : arr) {
+            if (mp[x] == 0) continue;
+            if (x < 0 && x % 2 != 0) return false;
+            int y = x > 0 ? x*2 : x/2;
+            if (mp[y] == 0) return false;
+            mp[x]--, mp[y]--;
         }
         return true;
     }
