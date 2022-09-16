a0d4a31b5c424c11bd2ed4e89bc26bc9cbe58f98
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-16 09:47:14 +0530

    Time: 898 ms (25.24%), Space: 119.9 MB (71.96%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -4,14 +4,14 @@ private:
     int solve(vector<int> &nums, vector<int> &muls, int start, int end, int idx) {
         if (idx == muls.size()) return 0;
         if (dp[start][idx] != INT_MAX) return dp[start][idx];
-        int score1 = muls[idx] * nums[start] + solve(nums, muls, start + 1, end, idx + 1);
-        int score2 = muls[idx] * nums[end] + solve(nums, muls, start, end - 1, idx + 1);
-        return dp[start][idx] = max(score1, score2);
+        int pickLeft = (muls[idx] * nums[start]) + solve(nums, muls, start + 1, end, idx + 1);
+        int pickRight = (muls[idx] * nums[end]) + solve(nums, muls, start, end - 1, idx + 1);
+        return dp[start][idx] = max(pickLeft, pickRight);
     }
 public:
     int maximumScore(vector<int> &nums, vector<int> &multipliers) {
-        int m = multipliers.size();
+        int m = multipliers.size(), n = nums.size();
         dp = vector<vector<int>>(m + 1, vector<int>(m + 1, INT_MAX));
-        return solve(nums, multipliers, 0, nums.size() - 1, 0);
+        return solve(nums, multipliers, 0, n - 1, 0);
     }
 };
