80cf96fcde9e070809c679e1a3f600a505e48501
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-16 23:07:10 +0530

    Time: 1086 ms (43.01%), Space: 93.6 MB (99.74%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -1,9 +1,9 @@
 typedef pair<long long, int> pli;
 
-class Solution {
+class Solution_1 {
 public:
     int mostBooked(int n, vector<vector<int>> &meetings) {
-        int cnt[101] = {};
+        int cnt[101]{0};
         sort(begin(meetings), end(meetings));
         priority_queue<int, vector<int>, greater<int>> available;
         priority_queue<pli, vector<pli>, greater<pli>> busy;
@@ -26,6 +26,32 @@ public:
             }
             cnt[room]++;
             busy.push({start + duration, room});
+        }
+        return max_element(begin(cnt), end(cnt)) - begin(cnt);
+    }
+};
+
+
+
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>> &meetings) {
+        int cnt[101]{0};
+        sort(meetings.begin(), meetings.end());
+        priority_queue<pli, vector<pli>, greater<pli>> busy;
+        for (int i = 0; i < n; i++) {
+            busy.push({meetings[0][0], i});
+        }
+        for (auto &meeting : meetings) {
+            while (busy.top().first < meeting[0]) {
+                busy.push({meeting[0], busy.top().second});
+                busy.pop();
+            }
+            auto [start, room] = busy.top();
+            long long duration = meeting[1] - meeting[0];
+            busy.pop();
+            busy.push({start + duration, room});
+            cnt[room]++;
         }
         return max_element(begin(cnt), end(cnt)) - begin(cnt);
     }
