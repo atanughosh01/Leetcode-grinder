deae2d6de0209bbc39e81468a711c1eb03b0dd6b
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-18 11:23:12 +0530

    Time: 11 ms (52.14%), Space: 9.8 MB (99.80%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,36 +1,14 @@
 class Solution {
 public:
-    /*
-    // Time Complexity = O(N*M), Space Complexity = O(N)
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        set<int> st;        
-        for(int itr = 0; itr < nums1.size(); itr++){
-            for(int jtr = 0; jtr < nums2.size(); jtr++){
-                if(nums1[itr] == nums2[jtr]){
-                    st.insert(nums1[itr]);
-                    break;
-                }
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2) {
+        vector<int> res;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        for (int &x : nums2) {
+            if (binary_search(nums1.begin(), nums1.end(), x) && (res.empty() || res.back() != x)) {
+                res.push_back(x);
             }
         }
-
-        vector<int> vt;
-        for(auto itr = st.begin(); itr != st.end(); itr++){
-            vt.push_back(*itr);
-        }
-        return vt;
-    }
-    */
-    
-    // Time Complexity = O(N), Space Complexity = O(N)
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> st(nums1.begin(), nums1.end());
-        vector<int> res;
-        for(int itr = 0; itr < nums2.size(); itr++){
-            if(st.find(nums2[itr]) != st.end()){
-                res.emplace_back(nums2[itr]);
-                st.erase(nums2[itr]);
-            }            
-        }
-    return res;
+        return res;
     }
 };
