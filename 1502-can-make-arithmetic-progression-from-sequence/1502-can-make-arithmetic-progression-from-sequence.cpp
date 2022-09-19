d16d5dc44f4406ece1ea181283b5502cbf030374
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-19 19:50:55 +0530

    Time: 10 ms (23.92%), Space: 9.1 MB (35.59%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// O(n * log(n)) Time | O(1) Space
+class Solution_1 {
 public:
     bool canMakeArithmeticProgression(vector<int> &arr) {
         sort(arr.begin(), arr.end());
@@ -6,6 +7,33 @@ public:
         for (int i = 2; i < arr.size(); i++) {
             if (arr[i] - arr[i-1] != d) {
                 return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+// O(n) Time | O(1) Space
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int> &arr) {
+        int n = arr.size();
+        int mn = INT_MAX, mx = INT_MIN;
+        for (int &x : arr) {
+            mn = min(mn, x);
+            mx = max(mx, x);
+        }
+        if (mx == mn) return true;
+        if ((mx - mn) % (n - 1) != 0) return false;
+        int d = (mx - mn) / (n - 1), i = 0;
+        while (i < n) {
+            if (arr[i] == mn + i * d) i++;
+            else if ((arr[i] - mn) % d != 0) return false;
+            else {
+                int pos = (arr[i] - mn) / d;
+                if (pos < i || arr[pos] == arr[i]) return false;
+                swap(arr[i], arr[pos]);
             }
         }
         return true;
