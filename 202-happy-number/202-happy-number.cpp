4e742ba923a793b2b1c0a68747bb3b19036b47f8
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-19 22:36:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (63.31%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// Approach 1: Detect Cycles with a HashSet
+class Solution_1 {
 private:
     int getNext(int n) {
         int totalSum = 0;
@@ -14,6 +15,79 @@ public:
         unordered_set<int> seen;
         while (n != 1 && seen.find(n) == seen.end()) {
             seen.insert(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+};
+
+
+
+// Approach 2: Floyd's Cycle-Finding Algorithm
+class Solution_2 {
+private:
+    int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n /= 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+public:
+    bool isHappy(int n) {
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while (fastRunner != 1 && slowRunner != fastRunner) {
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
+        }
+        return fastRunner == 1;
+    }
+};
+
+
+
+// Approach 3: Hardcoding the Only Cycle (Advanced)
+class Solution_3 {
+private:
+    unordered_set<int> cycleMembers = {4, 16, 37, 58, 89, 145, 42, 20};
+    int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n /= 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+public:
+    bool isHappy(int n) {
+        while (n != 1 && cycleMembers.find(n) == cycleMembers.end()) {
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+};
+
+
+
+// An Alternative Implementation
+class Solution {
+private:
+    int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n /= 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+public:
+    bool isHappy(int n) {
+        while (n != 1 && n != 4) {
             n = getNext(n);
         }
         return n == 1;
