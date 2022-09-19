f51f0033976b0718cf42499e8bc6e3459b5786d5
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-19 23:11:54 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (70.96%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res;
+        vector<vector<int>> res = {{1}};
+        if (numRows == 1) return res;        
         vector<int> arr;
-        res.push_back({1});     
-        if (numRows == 1) return res;     
         for (int i = 1; i < numRows; i++) {
-            arr = {};
+            arr.clear();
             for (int j = 0; j < i + 1; j++) {
                 int sum = 0;
                 if (j < i) sum += res[i-1][j];
