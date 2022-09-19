1e5826dd5d2d1fd032c8558d06a08c5ad444a422
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-19 22:45:00 +0530

    Time: 208 ms (37.02%), Space: 36.5 MB (77.86%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     vector<vector<string>> findDuplicate(vector<string> &paths) {
         unordered_map<string, vector<string>> files;
@@ -12,7 +12,6 @@ public:
                 while (path[i] != '(') i++;
                 file = path.substr(j, i - j);
                 j = i;
-                // while (path[i] != ')') i++;
                 while (path[i++] != ')');
                 content = path.substr(j, i - j);
                 files[content].push_back(dir + '/' + file);
@@ -25,4 +24,36 @@ public:
         }
         return res;
     }
+};
+
+
+
+class Solution {
+public:
+	vector<vector<string>> findDuplicate(vector<string> &paths) {
+		vector<vector<string>> res;
+		unordered_map<string, vector<string>> contMap;
+		for (const string &path : paths) {
+			int i = 0;
+			while (path[i] != ' ') i++;
+			string pStr = path.substr(0, i);
+			for (int j = i + 1, k = 0; i < path.size(); i++)
+				if (path[i] == '(') k = i + 1;
+				else if (path[i] == ')') {
+					string pathFile = pStr + '/' + path.substr(j, k - j - 1);
+					string cont = path.substr(k, i - k);
+					if (contMap.find(cont) == contMap.end()) {
+						contMap[cont] = {};
+					}
+					contMap[cont].push_back(pathFile);
+					j = i + 2;
+				}
+		}
+		for (auto &[key, value] : contMap) {
+			if (value.size() > 1) {
+				res.push_back(value);
+			}
+		}
+		return res;
+	}
 };
