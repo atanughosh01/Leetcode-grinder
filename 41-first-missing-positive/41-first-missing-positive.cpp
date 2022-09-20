d7c6e4c1fadc2d2a31595679fc048c7245ff4fda
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-20 22:43:47 +0530

    Time: 128 ms (67.90%), Space: 31.3 MB (88.59%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -6,6 +6,9 @@ public:
             while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
                 swap(nums[nums[i] - 1], nums[i]);
             }
+            // if (nums[i] != i + 1) {
+            //     return i + 1;
+            // }
         }
         for (int i = 0; i < n; i++) {
             if (nums[i] != i + 1) {
