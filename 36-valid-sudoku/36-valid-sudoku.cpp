a35858bd52010acdadc27117de497d73df961019
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-20 20:01:25 +0530

    Time: 56 ms (17.73%), Space: 18 MB (72.38%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
 	bool isValidSudoku(vector<vector<char>> &board) {
 		vector<unordered_set<int>> rows(9), cols(9), blocks(9);
@@ -18,4 +18,25 @@ public:
 		}
 		return true;
 	}
+};
+
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>> &board) {
+        int rows[9][9]{0}, cols[9][9]{0}, blocks[9][9]{0};
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '0' - 1;
+                    int  k = (i / 3) * 3 + j / 3;
+                    if (rows[i][num] || cols[j][num] || blocks[k][num]) {
+                        return false;
+                    }
+                    rows[i][num] = cols[j][num] = blocks[k][num] = 1;
+                }
+            }
+        }
+        return true;
+    }
 };
