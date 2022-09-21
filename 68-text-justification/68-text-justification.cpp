4a2b69f7d002d48332f6da385f0ba4898591a281
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-21 21:14:35 +0530

    Time: 5 ms (26.57%), Space: 7.5 MB (53.59%) - LeetHub

--- 68-text-justification/68-text-justification.cpp
+++ 68-text-justification/68-text-justification.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     vector<string> fullJustify(vector<string> &words, int maxWidth) {
         vector<string> res;
@@ -23,4 +23,50 @@ public:
         }
         return res;
     }
+};
+
+
+
+
+
+class Solution {
+private:
+	string leftJustify(vector<string> &arr, int diff, int l, int r) {
+		string ans = arr[l];
+		int spacesOnRight = diff - (r - l - 1);
+		for (int i = l + 1; i < r; i++) {
+			ans += (" " + arr[i]);
+		}
+		ans += string(spacesOnRight, ' ');
+		return ans;
+	}
+	string middleJustify(vector<string> &arr, int diff, int l, int r) {
+		string ans = arr[l];
+		int spacesNeeded = r - l - 1;
+		int spaces = diff / spacesNeeded;
+		int extraSpaces = diff % spacesNeeded;
+		for (int i = l + 1; i < r; i++) {
+			int spacesToApply = spaces + (extraSpaces-- > 0);
+			ans += (string(spacesToApply, ' ') + arr[i]);
+		}
+		return ans;
+	}
+public:
+	vector<string> fullJustify(vector<string> &words, int maxWidth) {
+		vector<string> res;
+		int i = 0, n = words.size();
+		while (i < n) {
+			int j = i + 1, lineLength = words[i].size();
+			while (j < n && (lineLength + words[j].size() + (j - i - 1) < maxWidth)) {
+				lineLength += words[j].size();
+				j++;
+			}
+			int diff = maxWidth - lineLength;
+			int wordsCount = j - i;
+			if (wordsCount == 1 || j >= n) res.push_back(leftJustify(words, diff, i, j));
+			else res.push_back(middleJustify(words, diff, i, j));
+			i = j;
+		}
+		return res;
+	}
 };
