3f1862b4956bfe9823411882046fe602a44bf1fa
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-21 23:59:03 +0530

    Time: 9 ms (93.16%), Space: 7.2 MB (99.98%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,20 +1,20 @@
 class Solution {
-public:
-    string minWindow(string s, string t) {
+   public:
+    string minWindow(string &s, string &t) {
         if (s.size() == 0 || t.size() == 0) return "";
-        vector<int> remaining(128, 0);
+        int remaining[128]{0};
+        for (const char &c : t) remaining[c]++;
         int required = t.size();
-        for (int i = 0; i < required; i++) remaining[t[i]]++;
         int min = INT_MAX, start = 0, left = 0, i = 0;
         while (i <= s.size() && start < s.size()) {
-            if(required) {
+            if (required) {
                 if (i == s.size()) break;
                 remaining[s[i]]--;
                 if (remaining[s[i]] >= 0) required--;
                 i++;
             } else {
                 if (i - start < min) {
-                    min = i -start;
+                    min = i - start;
                     left = start;
                 }
                 remaining[s[start]]++;
@@ -22,6 +22,6 @@ public:
                 start++;
             }
         }
-        return min == INT_MAX? "" : s.substr(left, min);
+        return min == INT_MAX ? "" : s.substr(left, min);
     }
 };
