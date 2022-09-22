cc5fa542bc3d377de64ade053c6fec302e89a0f7
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-22 20:39:54 +0530

    Time: 16 ms (8.38%), Space: 7.3 MB (30.20%) - LeetHub

---
+++ 541-reverse-string-ii/541-reverse-string-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseStr(string &s, int k) {
+        int i = 0, n = s.size();
+        while (i < n) {
+            int j = min(i + k - 1, n - 1);
+            int next_i = j + k + 1;
+            while (i < j) swap(s[i++], s[j--]);
+            i = next_i;
+        }
+        return s;
+    }
+};
