83bd6a3722323857a081f0f3341b655a622aad91
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-22 20:33:20 +0530

    Time: 7 ms (72.11%), Space: 6.9 MB (96.89%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,3 +1,34 @@
+// O(n) time | O(n) space
+class Solution_1 {
+private: 
+    vector<string> split(string &s, const char &c) {
+        int n = s.size(), l = 0, r = 0;
+        s.push_back(c);
+        vector<string> res;
+        while (s[r] == c) r++;
+        l = r;
+        while (r < n) {
+            while (s[r] != c) r++;
+            string word = s.substr(l, r - l);
+            res.push_back(word);
+            while (s[r] == c) r++;
+            l = r;
+        }
+        return res;
+    }
+public:
+    string reverseWords(string &s) {
+	    vector<string> v = split(s, ' ');
+        string res = v.back();
+        for (int i = v.size() - 2; i >= 0; i--) {
+            res += " " + v[i];
+        }
+        return res;
+    }
+};
+
+
+// O(n) time | O(1) space
 class Solution {
 public:
     string reverseWords(string &s) {
