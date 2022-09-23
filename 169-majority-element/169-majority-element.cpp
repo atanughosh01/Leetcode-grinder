ef6a76fca32643425a0561845d9efc5f75aafa87
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-23 19:55:33 +0530

    Time: 40 ms (33.03%), Space: 19.5 MB (92.11%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     int majorityElement(vector<int> &nums) {
         int n = nums.size(), res = 0;
@@ -8,5 +8,18 @@ public:
             if (val > n/2) return key;
         }
         return -1;
+    }
+};
+
+
+class Solution {
+public:
+    int majorityElement(vector<int> &nums) {
+        int res = 0, cnt = 0;
+        for (int &x : nums) {
+            if (cnt == 0) res = x;
+            cnt += (x == res) ? 1 : -1;
+        }
+        return res;
     }
 };
