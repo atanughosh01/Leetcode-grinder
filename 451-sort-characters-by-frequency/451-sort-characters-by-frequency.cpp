21d78ff001176005773374617efb9f5940554909
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-24 10:51:02 +0530

    Time: 35 ms (12.85%), Space: 15.8 MB (5.04%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,11 +1,34 @@
-class Solution {
+// O(n * log(n)) Time | O(1) Time | n = s.length
+class Solution_1 {
 public:
     string frequencySort(string &s) {
-        int freq[256]{0};
+        int freq[128]{0};
         for (const char &c : s) freq[c]++;
         sort(s.begin(), s.end(), [&](const char &a, const char &b) {
             return freq[a] == freq[b] ? a < b : freq[a] > freq[b];
         });
         return s;
     }
+};
+
+
+// O(2 * n) => O(n) Time | O(n*n + n) Space => O(n^2) | n = s.length
+class Solution {
+public:
+	string frequencySort(string &s) {
+		int n = s.size();
+		unordered_map<char, int> freq;
+		vector<string> bucket(n + 1, "");
+		string res;
+		for (char &c : s) freq[c]++;        // max size = 128 | O(128) Space => Constant Space && O(n) Time
+		for (auto &it : freq) {             // O(128) Time => Constant Time
+			char c = it.first;
+			int cnt = it.second;
+			bucket[cnt] += string(cnt, c);  // O(n * n) Space => O(n^2) Space
+		}
+		for (int i = n; i >= 0; i--) {      // O(n) Time
+			res += bucket[i];               // Total O(n) Space
+		}
+		return res;
+	}
 };
