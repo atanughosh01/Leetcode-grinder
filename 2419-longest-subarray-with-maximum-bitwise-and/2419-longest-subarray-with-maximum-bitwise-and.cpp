4071f967d512e2246ab07bd85de3c025240aad1e
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-25 10:06:02 +0530

    Time: 199 ms (50.00%), Space: 82.3 MB (33.33%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
@@ -5,12 +5,9 @@ public:
         int res = 0, cur = 0;
         for (int &x : nums) {
             if (x == mx) cur++;
-            else {
-                res = max(res, cur);
-                cur = 0;
-            }
+            else cur = 0;
+            res = max(res, cur);
         }
-        res = max(cur, res);
         return res;
     }
 };
