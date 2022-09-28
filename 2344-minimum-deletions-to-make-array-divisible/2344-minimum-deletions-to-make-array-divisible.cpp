823544dafe61867ce32e6dfca2fd8387d9f6fde0
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-28 10:46:41 +0530

    Time: 424 ms (14.38%), Space: 88 MB (99.04%) - LeetHub

---
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minOperations(vector<int> &nums, vector<int> &numsDivide) {
+        sort(nums.begin(), nums.end());
+        int g = numsDivide[0], n = nums.size();
+        for (int &n : numsDivide) g = __gcd(g, n);
+        for (int i = 0; i < n; i++) {
+            if (g % nums[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
