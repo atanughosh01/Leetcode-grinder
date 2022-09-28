74d285c8d57103e220e756de6948f66bb06cd48c
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-28 19:54:44 +0530

    Time: 11 ms (18.06%), Space: 6.3 MB (99.70%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -3,16 +3,17 @@ public:
     string removeDuplicateLetters(string &s) {
         string res = "";
         int lastPos[26]{0}, n = s.size();
-        bool added[26]{false};
+        bool visited[26]{false};
         for (int i = 0; i < n; i++) lastPos[s[i] - 'a'] = i;
         for (int i = 0; i < n; i++) {
-            if (added[s[i] - 'a']) continue;
-            while (!res.empty() && res.back() > s[i] && lastPos[res.back() - 'a'] > i) {
-                added[res.back() - 'a'] = false;
-                res.pop_back();
+            if (!visited[s[i] - 'a']) {
+                while (!res.empty() && res.back() > s[i] && lastPos[res.back() - 'a'] > i) {
+                    visited[res.back() - 'a'] = false;
+                    res.pop_back();
+                }
+                res.push_back(s[i]);
+                visited[s[i] - 'a'] = true;
             }
-            res.push_back(s[i]);
-            added[s[i] - 'a'] = true;
         }
         return res;
     }
