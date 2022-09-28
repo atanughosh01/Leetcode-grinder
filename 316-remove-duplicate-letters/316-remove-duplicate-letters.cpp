864237947403b60c24cb921c80ca4721da4561be
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-28 19:55:08 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (94.73%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     string removeDuplicateLetters(string &s) {
         string res = "";
@@ -8,6 +8,30 @@ public:
         for (int i = 0; i < n; i++) {
             if (!visited[s[i] - 'a']) {
                 while (!res.empty() && res.back() > s[i] && lastPos[res.back() - 'a'] > i) {
+                    visited[res.back() - 'a'] = false;
+                    res.pop_back();
+                }
+                res.push_back(s[i]);
+                visited[s[i] - 'a'] = true;
+            }
+        }
+        return res;
+    }
+};
+
+
+
+class Solution {
+public:
+    string removeDuplicateLetters(string &s) {
+        string res = "";
+        int cnt[26]{0}, n = s.size();
+        bool visited[26]{false};
+        for (int i = 0; i < n; i++) cnt[s[i] - 'a']++;
+        for (int i = 0; i < n; i++) {
+            cnt[s[i] - 'a']--;
+            if (!visited[s[i] - 'a']) {
+                while (!res.empty() && res.back() > s[i] && cnt[res.back() - 'a'] > 0) {
                     visited[res.back() - 'a'] = false;
                     res.pop_back();
                 }
