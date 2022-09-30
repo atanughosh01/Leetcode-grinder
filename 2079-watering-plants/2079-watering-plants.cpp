9cbfb651081db1da6c6fc9f71b6362c7934d8795
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-30 23:25:22 +0530

    Time: 16 ms (9.03%), Space: 8.3 MB (83.30%) - LeetHub

--- 2079-watering-plants/2079-watering-plants.cpp
+++ 2079-watering-plants/2079-watering-plants.cpp
@@ -1,24 +1,22 @@
-/*
-    class Solution {
-    public:
-        int wateringPlants(vector<int> &p, int capacity) {
-            int res = 0, can = capacity;
-            for (int i = 0; i < p.size(); ++i) {
-                if (can < p[i]) {
-                    res += i * 2;
-                    can = capacity;
-                }
-                can -= p[i];
+class Solution_1 {
+public:
+    int wateringPlants(vector<int> &p, int capacity) {
+        int res = 0, can = capacity;
+        for (int i = 0; i < p.size(); ++i) {
+            if (can < p[i]) {
+                res += i * 2;
+                can = capacity;
             }
-            return res + p.size();
+            can -= p[i];
         }
-    };
-*/
+        return res + p.size();
+    }
+};
 
 
 class Solution {
 public:
-    int wateringPlants(vector<int>& P, int C) {
+    int wateringPlants(vector<int> &P, int C) {
         int ans = 0, cur = C;
         for(int i = 0; i < size(P); i++) {
             if(P[i] > cur) {      // water available less than whats needed
