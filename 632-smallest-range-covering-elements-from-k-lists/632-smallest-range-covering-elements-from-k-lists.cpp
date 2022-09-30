cf80591ccc52abdb40314a858973bd8cb1ee2d12
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-30 23:13:28 +0530

    Time: 114 ms (45.02%), Space: 17 MB (49.42%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> smallestRange(vector<vector<int>> &nums) {
         vector<int> res;
         vector<pair<int, int>> pairs;
+        unordered_map<int, int> count;
         int i = 0, k = 0, n = nums.size();
         for (int x = 0; x < n; x++) {
-            for (auto n : nums[x]) {
+            for (int &n : nums[x]) {
                 pairs.push_back({n, x});
             }
         }
         sort(pairs.begin(), pairs.end());
-        unordered_map<int, int> count;
         for (int j = 0; j < pairs.size(); j++) {
             if (!count[pairs[j].second]++) k++;
             if (k == n) {
