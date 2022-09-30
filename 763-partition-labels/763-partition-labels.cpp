747cc16e7f92b25a7a690e67cc4fa3a6e5e168f7
Atanu Ghosh <2001atanughosh@gmail.com>
2022-09-30 23:26:33 +0530

    Time: 7 ms (56.88%), Space: 6.7 MB (69.27%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -3,11 +3,9 @@ public:
     vector<int> partitionLabels(string &s) {
         unordered_map<char, int> mp;
         vector<int> res;
-        for (int i = 0; i < s.size(); i++) {
-            mp[s[i]] = i;
-        }
-        int prev = -1, max_i = 0;
-        for (int i = 0; i < s.size(); i++) {
+        int prev = -1, max_i = 0, n = s.size();
+        for (int i = 0; i < n; i++) mp[s[i]] = i;
+        for (int i = 0; i < n; i++) {
             max_i = max(max_i, mp[s[i]]);
             if (max_i == i) {
                 res.push_back(max_i - prev);
