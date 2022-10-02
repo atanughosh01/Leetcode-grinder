1971c6d15d700814e6d62c13ac392e6f3c830fbc
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-02 10:17:38 +0530

    Time: 3 ms (50.00%), Space: 6 MB (20.00%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.cpp
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     int commonFactors(int a, int b) {
         int g = __gcd(a, b), cnt = 0;
@@ -6,5 +6,20 @@ public:
             cnt += (g % i == 0);
         }
         return cnt;
+    }
+};
+
+
+
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        a = gcd(a, b);
+        int sr = sqrt(a);
+        int res = sr * sr == a ? -1 : 0;
+        for (int i = 1; i <= sr; ++i) {
+            res += 2 * (a % i == 0);
+        }
+        return res;
     }
 };
