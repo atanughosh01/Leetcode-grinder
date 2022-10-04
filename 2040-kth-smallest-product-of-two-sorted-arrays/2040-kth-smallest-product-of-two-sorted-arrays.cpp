4e84467b836642204dee8b7f8360c3a86c1d9cd7
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-04 23:31:09 +0530

    Time: 1240 ms (64.76%), Space: 94.2 MB (50.48%) - LeetHub

--- 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.cpp
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.cpp
@@ -1,22 +1,55 @@
 class Solution {
-public:
-    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
-        auto func = [&](double val) {
-            long long ans = 0;
-            for (auto &x : nums1) {
-                if (x < 0) ans += nums2.end() - lower_bound(nums2.begin(), nums2.end(), ceil(val / x));
-                else if (x == 0) {
-                    if (0 <= val) ans += nums2.size();
-                } else ans += upper_bound(nums2.begin(), nums2.end(), floor(val / x)) - nums2.begin();
-            }
-            return ans;
-        };
-        long long l = -pow(10ll, 10), r = pow(10ll, 10) + 1;
-        while (l < r) {
+private:
+    int findmaxIndex(vector<int> &nums2, long long val, long long midval) {
+        int n = nums2.size(), l = 0, r = n - 1, res = -1;
+        while (l <= r) {
             long long mid = l + (r - l) / 2;
-            if (func(mid) < k) l = mid + 1;
-            else r = mid;
+            if (val * nums2[mid] <= midval) {
+                res = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
-        return l;
+        return res + 1;
+    }
+
+    int findminIndex(vector<int> &nums2, long long val, long long midval) {
+        int n = nums2.size(), l = 0, r = n - 1, res = n;
+        while (l <= r) {
+            long long mid = l + (r - l) / 2;
+            if (val * nums2[mid] <= midval) {
+                res = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return n - res;
+    }
+
+    bool check(long long midval, vector<int> &nums1, vector<int> &nums2, long long k) {
+        long long cnt = 0;
+        for (int i = 0; i < nums1.size(); i++) {
+            long long val = nums1[i];
+            if (val == 0 && midval >= 0) cnt += nums2.size();
+            else if (val > 0) cnt += findmaxIndex(nums2, val, midval);
+            else if (val < 0) cnt += findminIndex(nums2, val, midval);
+        }
+        return cnt >= k;
+    }
+
+public:
+    long long kthSmallestProduct(vector<int> &nums1, vector<int> &nums2, long long k) {
+        long long l = -1e10, r = 1e10, res = -1;
+        while (l <= r) {
+            long long mid = (l + r) / 2;
+            if (check(mid, nums1, nums2, k)) {
+                res = mid;
+                r = mid - 1;
+            } else
+                l = mid + 1;
+        }
+        return res;
     }
 };
