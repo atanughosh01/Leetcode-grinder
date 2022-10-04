f42807120d364a6b3542950eb6d1bb3845752bf5
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-04 23:04:17 +0530

    Time: 2367 ms (24.28%), Space: 94.2 MB (50.48%) - LeetHub

--- 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.cpp
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
-        auto fn = [&](double val) {
-            long long ans = 0; 
-            for (auto& x : nums1) {
-                if (x < 0) ans += nums2.end() - lower_bound(nums2.begin(), nums2.end(), ceil(val/x)); 
+        auto func = [&](double val) {
+            long long ans = 0;
+            for (auto &x : nums1) {
+                if (x < 0) ans += nums2.end() - lower_bound(nums2.begin(), nums2.end(), ceil(val / x));
                 else if (x == 0) {
-                    if (0 <= val) ans += nums2.size(); 
-                } else ans += upper_bound(nums2.begin(), nums2.end(), floor(val/x)) - nums2.begin(); 
+                    if (0 <= val) ans += nums2.size();
+                } else ans += upper_bound(nums2.begin(), nums2.end(), floor(val / x)) - nums2.begin();
             }
-            return ans; 
-        }; 
-        
-        long long lo = -pow(10ll, 10), hi = pow(10ll, 10)+1;
-        while (lo < hi) {
-            long long mid = lo + (hi - lo)/2; 
-            if (fn(mid) < k) lo = mid + 1; 
-            else hi = mid; 
+            return ans;
+        };
+        long long l = -pow(10ll, 10), r = pow(10ll, 10) + 1;
+        while (l < r) {
+            long long mid = l + (r - l) / 2;
+            if (func(mid) < k) l = mid + 1;
+            else r = mid;
         }
-        return lo; 
+        return l;
     }
 };
