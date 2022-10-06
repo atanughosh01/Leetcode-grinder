2ad8fbd4c5a56e446e50c80f8347b0f6781df693
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-06 18:12:43 +0530

    Time: 169 ms (98.80%), Space: 89.5 MB (69.66%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-    int maximumScore(vector<int>& nums, int k) {
-        int n  =(int)nums.size(), x;
-		int mi = nums[k], ans=nums[k];                   //Start with the given k
-		for(int i=k,j=k;i>=0 || j<n ;) {    // Move in the array with the help of two pointer in both direction
-            while(j<n && nums[j]>=mi) ++j;  //we move in right direction until our minimum, that is mi will not change
-            while(i>=0 && nums[i]>=mi) --i; //we move in left until our minimum, that is mi will not change
-            ans=max(ans,mi*(j-i-1));        // update answer everytime
-
-            // Here, We have two potential minimum, which is nums[i] and nums[j] in any direction
-            // So we move to that minimum which will be greater between the two minimum then again 
-            // apply the above two step, that is find the longest array in with the given minimum
-            // This we can understand with some testcases
-            if(i>=0 and j<n)
-                mi = ((nums[i] > nums[j])? nums[i] : nums[j]); 
-            else if(j<n)                    // if i<0 we can move only in right direction
-                mi = nums[j];
-            else if(i>=0)                   // if j>=n we can move only in left direction
-                mi = nums[i];
+    int maximumScore(vector<int> &nums, int k) {
+        int n  = nums.size(), mi = nums[k], res = nums[k];
+        for (int i = k, j = k; i >= 0 || j < n;) {
+            while (j < n && nums[j] >= mi) j++;
+            while (i >= 0 && nums[i] >= mi) i--;
+            res = max(res, mi * (j - i - 1));
+            if (i >= 0 && j < n) mi = max(nums[i], nums[j]);
+            else if (j < n) mi = nums[j];
+            else if (i >= 0) mi = nums[i];
         }
-        return ans;
+        return res;
     }
 };
