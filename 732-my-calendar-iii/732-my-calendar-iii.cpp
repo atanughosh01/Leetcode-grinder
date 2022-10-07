292ddc7aa03e5069f98ba12fda3c58a080d639bf
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-07 20:28:38 +0530

    Time: 1451 ms (5.03%), Space: 308.2 MB (5.03%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -1,18 +1,33 @@
+// Time Complexity:O(NlogC) | Space Complexity: O(NlogC)
+// N be the number of events booked, C be the largest time (i.e. 10^9 in this problem)
+
 class MyCalendarThree {
-    map<int, int> timeline;
+private:
+    unordered_map<int, int> vals;
+    unordered_map<int, int> lazy;
+
 public:
     MyCalendarThree() {}
-    
-    int book(int start, int end) {
-        timeline[start]++;
-        timeline[end]--;
-        int ongoing = 0, k = 0;
-        for (auto &[key, val] : timeline) {
-            k = max(k, ongoing += val);
+
+    void update(int start, int end, int left, int right, int idx) {
+        if (start > right || end < left) return;
+        if (left >= start && right <= end) {
+            lazy[idx]++;
+            vals[idx]++;
+        } else {
+            int mid = left + (right - left) / 2;
+            update(start, end, left, mid, idx * 2);
+            update(start, end, mid + 1, right, idx * 2 + 1);
+            vals[idx] = lazy[idx] + max(vals[idx * 2], vals[idx * 2 + 1]);
         }
-        return k;
+    }
+
+    int book(int start, int end) {
+        update(start, end - 1, 1, 1000000000, 1);
+        return vals[1];
     }
 };
+
 
 
 /**
