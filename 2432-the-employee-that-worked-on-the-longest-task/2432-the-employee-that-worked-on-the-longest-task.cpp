ca0f939576a5dd6af862236b516893a34a044064
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-09 10:32:37 +0530

    Time: 167 ms (25.00%), Space: 35.5 MB (25.00%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int hardestWorker(int n, vector<vector<int>> &logs) {
-        int d = logs[0][1], id = logs[0][0], z = logs.size();
-        for (int i = 1; i < z; i++) {
+        int d = logs[0][1], id = logs[0][0], N = logs.size();
+        for (int i = 1; i < N; i++) {
             int ans = logs[i][1] - logs[i - 1][1];
             if (ans == d) id = min(id, logs[i][0]);
             if (ans > d) {
