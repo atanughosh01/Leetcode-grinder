00cdaa9b0f80e39a6b39bbee348eab551812bc14
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-11 19:47:13 +0530

    Time: 11 ms (49.86%), Space: 9.8 MB (60.92%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -12,8 +12,8 @@ public:
 };
 
 
-// Optimal | O(2*n) == O(n) Time | O(1) Space
-class Solution {
+// Optimal | O(n) Time | O(1) Space
+class Solution_2 {
 public:
     vector<int> shuffle(vector<int> &nums, int n) {
         for (int i = n; i < 2 * n; i++) {
@@ -25,5 +25,22 @@ public:
             nums[idx + 1] = nums[i] / 1024;
         }
         return nums;
+    }
+};
+
+
+// Most Optimal | O(n) Time | O(1) Space
+class Solution {
+public:
+    vector<int> shuffle(vector<int> &nums, int n) {        
+        for (int i = n; i < 2 * n; i++) {
+            nums[i] = (nums[i] << 10) | nums[i - n];
+        }
+        int idx = 0;
+        for (int i = n; i < 2 * n; i++, idx += 2) {
+            nums[idx] = nums[i] & 1023;
+            nums[idx + 1] = nums[i] >> 10;
+        }
+        return nums; 
     }
 };
