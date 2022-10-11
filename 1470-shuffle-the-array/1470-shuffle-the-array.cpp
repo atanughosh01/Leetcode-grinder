c065a64603569ef360a25baa17fe723cc74b27ee
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-11 19:43:58 +0530

    Time: 3 ms (96.85%), Space: 9.6 MB (96.66%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// Bruteforce | O(n) Time | O(2*n) Space
+class Solution_1 {
 public:
     vector<int> shuffle(vector<int> &nums, int n) {
         vector<int> res;
@@ -7,5 +8,22 @@ public:
             res.push_back(nums[n + i]);
         }
         return res;
+    }
+};
+
+
+// Optimal | O(2*n) == O(n) Time | O(1) Space
+class Solution {
+public:
+    vector<int> shuffle(vector<int> &nums, int n) {
+        for (int i = n; i < 2 * n; i++) {
+            nums[i] = (nums[i] * 1024) + nums[i - n];
+        }
+        int idx = 0;
+        for (int i = n; i < 2 * n; i++, idx += 2) {
+            nums[idx] = nums[i] % 1024;
+            nums[idx + 1] = nums[i] / 1024;
+        }
+        return nums;
     }
 };
