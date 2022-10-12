2c5e5b95ce891e39b6ea3dac66c2cfa0dacb0e54
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-12 23:30:36 +0530

    Time: 84 ms (73.62%), Space: 17 MB (49.31%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>> &nums) {
+        vector<int> res;
+        vector<pair<int, int>> pairs;
+        unordered_map<int, int> count;
+        int i = 0, k = 0, n = nums.size();
+        for (int x = 0; x < n; x++) {
+            for (int &n : nums[x]) {
+                pairs.push_back({n, x});
+            }
+        }
+        sort(pairs.begin(), pairs.end());
+        for (int j = 0; j < pairs.size(); j++) {
+            if (!count[pairs[j].second]++) k++;
+            if (k == n) {
+                while (count[pairs[i].second] > 1) count[pairs[i++].second]--;
+                if (res.empty() || res[1] - res[0] > pairs[j].first - pairs[i].first) {
+                    res = {pairs[i].first, pairs[j].first};
+                }
+            }
+        }
+        return res;
+    }
+};
