f58c2564a050e6fcc7c7d4bd9e4787a09ffbd3d5
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-14 21:08:17 +0530

    Time: 11 ms (25.40%), Space: 5.9 MB (71.07%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,40 @@
+class Solution_1 {
+public:
+    bool judgeSquareSum(int c) {
+        for (long long a = 0; a * a <= c; a++) {
+            for (long long b = 0; b * b <= c; b++) {
+                if (a * a + b * b == c) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+
+
+class Solution_2 {
+public:
+    bool judgeSquareSum(int c) {
+        for (long long a = 0; a * a <= c; a++) {
+            long long b = c - (int)(a * a), s = 0;
+            for (long long i = 1; s < b; i += 2) s += i;
+            if (s == b) return true;
+        }
+        return false;
+    }
+};
+
+
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for (long long a = 0; a * a <= c; a++) {
+            long double b = sqrt(c - (a * a));
+            if ((long long)b == b) return true;
+        }
+        return false;
+    }
+};
