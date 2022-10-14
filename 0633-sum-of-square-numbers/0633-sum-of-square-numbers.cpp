4937f24fd824c24d2a0f90d465e9c49c4adccc2d
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-14 21:15:17 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (26.07%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,6 +1,6 @@
 class Solution_1 {
 public:
-    bool judgeSquareSum(int c) {
+    bool judgeSquareSum(int &c) {
         for (long long a = 0; a * a <= c; a++) {
             for (long long b = 0; b * b <= c; b++) {
                 if (a * a + b * b == c) {
@@ -16,7 +16,7 @@ public:
 
 class Solution_2 {
 public:
-    bool judgeSquareSum(int c) {
+    bool judgeSquareSum(int &c) {
         for (long long a = 0; a * a <= c; a++) {
             long long b = c - (int)(a * a), s = 0;
             for (long long i = 1; s < b; i += 2) s += i;
@@ -28,12 +28,28 @@ public:
 
 
 
-class Solution {
+class Solution_3 {
 public:
-    bool judgeSquareSum(int c) {
+    bool judgeSquareSum(int &c) {
         for (long long a = 0; a * a <= c; a++) {
             long double b = sqrt(c - (a * a));
             if ((long long)b == b) return true;
+        }
+        return false;
+    }
+};
+
+
+
+class Solution {
+public:
+    bool judgeSquareSum(int &c) {
+        long long l = 0, r = sqrt(c);
+        while (l <= r) {
+            long long curr = l * l + r * r;
+            if (curr == c) return true;
+            else if (curr < c) l++;
+            else r--;
         }
         return false;
     }
