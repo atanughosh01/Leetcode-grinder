a9b7968e96e38ee65f2e611ef08c543971a1bd0a
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-14 23:16:15 +0530

    Time: 16 ms (15.79%), Space: 8.9 MB (68.12%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -5,7 +5,7 @@ public:
         for (int j = 1, count = 1; j <= n; j++, count++) {
             if (j == n || chars[j] != chars[j - 1]) {
                 chars[idx++] = chars[j - 1];
-                if (count >= 2) {
+                if (count > 1) {
                     string str = to_string(count);
                     for (char &digit : str) {
                         chars[idx++] = digit;
