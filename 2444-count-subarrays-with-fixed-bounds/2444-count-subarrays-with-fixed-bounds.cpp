8a9e1f26947ad3da60e67efe3824222e14026605
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-16 09:52:47 +0530

    Time: 287 ms (11.11%), Space: 70.3 MB (11.11%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,27 +1,47 @@
+class Solution_1 {
+public:
+	long long countSubarrays(vector<int> &nums, int minK, int maxK) {
+		long long res = 0;
+		bool minFound = false, maxFound = false;
+		int start = 0, minStart = 0, maxStart = 0;
+		for (int i = 0; i < nums.size(); i++) {
+			int num = nums[i];
+			if (num < minK || num > maxK) {
+				minFound = maxFound = false;
+				start = i + 1;
+			}
+			if (num == minK) {
+				minFound = true;
+				minStart = i;
+			}
+			if (num == maxK) {
+				maxFound = true;
+				maxStart = i;
+			}
+			if (minFound && maxFound) {
+				res += (min(minStart, maxStart) - start + 1);
+			}
+		}
+		return res;
+	}
+};
+
+
+
 class Solution {
 public:
-    long long countSubarrays(vector<int> &nums, int minK, int maxK) {
-        long long res = 0;
-        bool minFound = false, maxFound = false;
-        int start = 0, minStart = 0, maxStart = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int num = nums[i];
-            if (num < minK || num > maxK) {
-                minFound = maxFound = false;
-                start = i+1;
-            }
-            if (num == minK) {
-                minFound = true;
-                minStart = i;
-            }
-            if (num == maxK) {
-                maxFound = true;
-                maxStart = i;
-            }
-            if (minFound && maxFound) {
-                res += (min(minStart, maxStart) - start + 1);
-            }
-        }
-        return res;
-    }
+	long long countSubarrays(vector<int> &nums, int minK, int maxK) {
+		long long res = 0;
+		int  j = 0, jmin = -1, jmax = -1, n = nums.size();
+		for (int i = 0; i < n; ++i) {
+			if  (nums[i] < minK || nums[i] > maxK) {
+				jmin = jmax = -1;
+				j = i + 1;
+			}
+			if (nums[i] == minK) jmin = i;
+			if (nums[i] == maxK) jmax = i;
+			res += max(0, min(jmin, jmax) - j + 1);
+		}
+		return res;
+	}
 };
