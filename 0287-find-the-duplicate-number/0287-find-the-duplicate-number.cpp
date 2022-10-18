09658756876a4f6024fe909cefb451c63e33a415
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-18 19:49:07 +0530

    Time: 339 ms (25.11%), Space: 61.3 MB (30.03%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,9 +1,28 @@
-class Solution {
+class Solution_1 {
 public:
     int findDuplicate(vector<int> &nums) {
         while (nums[0] != nums[nums[0]]) {
             swap(nums[0], nums[nums[0]]);
         }
         return nums[0];
+    }
+};
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int> &nums){
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        fast = 0;
+        while (fast != slow) {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return slow;
     }
 };
