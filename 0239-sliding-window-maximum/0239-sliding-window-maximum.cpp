a2bb0c6497c64206e42e830131d4dcf1b7e8f56f
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-21 22:45:52 +0530

    Time: 674 ms (35.68%), Space: 134.8 MB (60.88%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -37,6 +37,25 @@ public:
 
 
 // Optimal | O(n) Time | O(n) + O(k) Space
+class Solution_3 {
+public:
+    vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+        vector<int> res;
+        deque<int> dq;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            while (!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
+            dq.push_back(i);
+            if (!dq.empty() && dq.front() == i - k) dq.pop_front();
+            if (i >= k - 1) res.push_back(nums[dq.front()]);
+        }
+        return res;
+    }
+};
+
+
+
+// Optimal | O(n) Time | O(n) + O(k) Space
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int> &nums, int k) {
@@ -46,8 +65,7 @@ public:
         for (int i = 0; i < n; i++) {
             while (!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
             dq.push_back(i);
-            // if (l > dq.front()) dq.pop_front();
-            if (!dq.empty() && dq.front() == i - k) dq.pop_front();
+            if (l > dq.front()) dq.pop_front();
             if (i >= k - 1) res.push_back(nums[dq.front()]), l++;
         }
         return res;
