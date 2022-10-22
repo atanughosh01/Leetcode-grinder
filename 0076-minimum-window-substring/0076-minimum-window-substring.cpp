a8b92e80014de1254546b465400db03a21c1acad
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-22 11:21:45 +0530

    Time: 5 ms (98.11%), Space: 7.4 MB (99.68%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string minWindow(string &s, string &t) {
+        int rem[128]{0}, req = t.size(), m = s.size();
+        int min = INT_MAX, start = 0, left = 0, i = 0;
+        for (char &c : t) rem[c]++;
+        while (i <= m && start < m) {
+            if (req != 0) {
+                if (i == m) break;
+                if (rem[s[i]]-- >= 1) req--;
+                i++;
+            } else {
+                if (i - start < min) {
+                    min = i - start;
+                    left = start;
+                }
+                if (rem[s[start]]++ >= 0) req++;
+                start++;
+            }
+        }
+        return (min == INT_MAX) ? "" : s.substr(left, min);
+    }
+};
