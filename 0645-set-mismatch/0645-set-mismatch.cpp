282e4d0ab85b2049b101f2bafe440d96272bf8b2
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-23 20:41:28 +0530

    Time: 54 ms (76.26%), Space: 21.3 MB (95.87%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     vector<int> findErrorNums(vector<int> &nums) {
         int n = nums.size(), absent, repeat;
@@ -9,5 +9,27 @@ public:
             if (mp[i] == 2) repeat = i;
         }
         return {repeat, absent};
+    }
+};
+
+
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int> &nums) {
+        int n = nums.size();
+        vector<int> res(2, -1);
+        for (int i = 0; i < n; i++) {
+            while (nums[i] != i + 1){
+                if (nums[i] == nums[nums[i] - 1]) {
+                    res[0] = nums[i];
+                    res[1] = i + 1;
+                    break;
+                } else {
+                    swap(nums[i], nums[nums[i] - 1]);
+                }
+            }
+        }
+        return res;
     }
 };
