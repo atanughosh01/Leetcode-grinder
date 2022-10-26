11648e861e4523406e464f4a07ce2cbd9a74d792
Atanu Ghosh <2001atanughosh@gmail.com>
2022-10-26 11:18:38 +0530

    Time: 698 ms (5.02%), Space: 113.7 MB (83.55%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,57 @@
+class Solution_1 {
+public:
+    bool checkSubarraySum(vector<int> &nums, int k) {
+        int n = nums.size();
+        if (n == 1) return false;
+        for (int i = 0; i < n; i++) {
+            int sum = nums[i];
+            for (int j = i + 1; j < n; j++) {
+                sum += nums[j];
+                if (sum % k == 0) return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+class Solution_2 {
+public:
+    bool checkSubarraySum(vector<int> &nums, int k) {
+        int n = nums.size();
+        vector<int> pref(n, nums[0]);
+        for (int i = 1; i < n; i++) {
+            pref[i] = pref[i - 1] + nums[i];
+            if (pref[i] % k == 0) return true;
+        }
+        for (int i = 2; i < n; i++) {
+            for (int j = 0; j <= i - 2; j++) {
+                if ((pref[i] - pref[j]) % k == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int> &nums, int k) {
+        int n = nums.size(), sum = 0;
+        if (n == 1) return false;
+        unordered_map<int,int> mp;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            int rem = sum % k;
+            if (rem == 0 && i > 0) return true;
+            if (mp.find(rem) != mp.end()) {
+                if (i - mp[rem] > 1) return true;
+            } else {
+               mp[rem] = i;
+            }
+        }
+        return false;
+    }
+};
