697e453f1d9d9071ef1f4d350a68f140a98e6fa6
Atanu Ghosh <2001atanughosh@gmail.com>
2022-11-06 10:53:48 +0530

    Time: 16 ms (22.22%), Space: 8.7 MB (77.78%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// O(n) time | O(n) space
+class Solution_1 {
 public:
     vector<int> applyOperations(vector<int> &nums) {
         int n = nums.size();
@@ -20,5 +21,28 @@ public:
             }
         }
         return res;
+    }
+};
+
+
+
+// O(n) time | O(1) space
+class Solution {
+public:
+    vector<int> applyOperations(vector<int> &nums) {
+        int n = nums.size(), j = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+        for (int &x : nums) {
+            if (x != 0) {
+                nums[j++] = x;
+            }
+        }
+        while (j < n) nums[j++] = 0;
+        return nums;
     }
 };
