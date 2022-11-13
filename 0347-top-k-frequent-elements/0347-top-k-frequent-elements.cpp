9a8ad1768197803ffa29b97e44efa917949e6657
Atanu Ghosh <2001atanughosh@gmail.com>
2022-11-13 23:28:38 +0530

    Time: 45 ms (16.68%), Space: 13.6 MB (90.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,14 +1,31 @@
-class Solution {
-private:
-	static bool compare(const pair<int, int> &a, const pair<int, int> &b) {
-		return a.second > b.second;
-	}
+class Solution_1 {
 public:
 	vector<int> topKFrequent(vector<int> &nums, int k) {
 		unordered_map<int, int> mp;
 		for (int &num : nums) mp[num]++;
 		vector<pair<int, int>> v(mp.begin(), mp.end());
-		sort(v.begin(), v.end(), compare);
+		sort(v.begin(), v.end(), [](const pair<int, int> &a, const pair<int, int> &b) {
+            return a.second > b.second;
+        });
+		nums.clear();
+		for (int i = 0; i < k; i++) {
+			nums.push_back(v[i].first);
+		}
+		return nums;
+	}
+};
+
+
+
+class Solution {
+public:
+	vector<int> topKFrequent(vector<int> &nums, int k) {
+		unordered_map<int, int> mp;
+		for (int &num : nums) mp[num]++;
+		vector<pair<int, int>> v(mp.begin(), mp.end());
+		sort(v.begin(), v.end(), [](auto &a, auto &b) {
+            return a.second > b.second;
+        });
 		nums.clear();
 		for (int i = 0; i < k; i++) {
 			nums.push_back(v[i].first);
