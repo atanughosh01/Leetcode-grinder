d4274063035a635424bc370dce42fa9af88bd7ae
Atanu Ghosh <2001atanughosh@gmail.com>
2022-11-14 19:45:32 +0530

    Time: 12 ms (56.05%), Space: 8.3 MB (40.88%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,19 +1,19 @@
 // O(n) time | O(n) space
-class Solution_1 {
+class Solution {
 private: 
     vector<string> split(string &s, const char &c) {
         int n = s.size(), l = 0, r = 0;
         s.push_back(c);
         vector<string> res;
-        while (s[r] == c) r++;
-        l = r;
-        while (r < n) {
+        do {
             while (s[r] != c) r++;
             string word = s.substr(l, r - l);
-            res.push_back(word);
+            if (!word.empty()) {
+                res.push_back(word);
+            }
             while (s[r] == c) r++;
             l = r;
-        }
+        } while (r < n);
         return res;
     }
 public:
@@ -24,24 +24,5 @@ public:
             res += " " + v[i];
         }
         return res;
-    }
-};
-
-
-// O(n) time | O(1) space
-class Solution {
-public:
-    string reverseWords(string &s) {
-        int n = s.size(), i = 0, j = 0;
-        while (i < n) {
-            while (i < n && s[i] == ' ') i++;
-            if (i < n && j > 0) s[j++] = ' ';
-            int start = j;
-            while (i < n && s[i] != ' ') s[j++] = s[i++];
-            reverse(s.begin() + start, s.begin() + j);
-        }
-        s.resize(j);
-        reverse(s.begin(), s.end());
-        return s;
     }
 };
