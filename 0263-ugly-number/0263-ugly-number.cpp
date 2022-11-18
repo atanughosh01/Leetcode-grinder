ba3a160c93b86ce537de1f159d0336db2aa71c42
Atanu Ghosh <2001atanughosh@gmail.com>
2022-11-18 18:24:22 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (69.56%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,10 +1,30 @@
-class Solution {
+class Solution_1 {
 public:
     bool isUgly(int n) {
         if (n <= 0) return false;
         while (n % 2 == 0) n /= 2;
         while (n % 3 == 0) n /= 3;
         while (n % 5 == 0) n /= 5;
+        return n == 1;
+    }
+};
+
+
+
+class Solution {
+private:
+    int keepDividingWhenDivisible(int dividend, int divisor) {
+        while (dividend % divisor == 0) {
+            dividend /= divisor;;
+        }
+        return dividend;
+    }
+public:
+    bool isUgly(int n) {
+        if (n <= 0) return false;
+        for (int factor : {2, 3, 5}) {
+            n = keepDividingWhenDivisible(n, factor);
+        }
         return n == 1;
     }
 };
