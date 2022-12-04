5ab17325317a01901935b5d844964c38bbaa382e
Atanu Ghosh <2001atanughosh@gmail.com>
2022-12-04 09:59:47 +0530

    Time: 235 ms (33.33%), Space: 52.4 MB (8.33%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -1,15 +1,33 @@
+class Solution_1 {
+public:
+	long long dividePlayers(vector<int> &skill) {
+		sort(skill.begin(), skill.end());
+		long long res = 0, sum = 0;
+		int n = skill.size();
+        sum = skill[0] + skill[n - 1];
+		for (int i = 0; i < n / 2; i++) {
+			if (sum != (skill[i] + skill[n - i - 1])) {
+				return -1;
+			}
+			res += (skill[i] * 1ll * skill[n - i - 1]);
+		}
+		return res;
+	}
+};
+
+
 class Solution {
 public:
 	long long dividePlayers(vector<int> &skill) {
 		sort(skill.begin(), skill.end());
-		long long res = 0, pdt = 1;
-		int n = skill.size(), s = skill[0] + skill[n - 1];
-		for (int i = 0; i < n / 2; i++) {
-			if (s != (skill[i] + skill[n - i - 1])) {
+		long long res = 0, sum = 0;
+		int n = skill.size(), l = 0, r = n - 1;
+        sum = skill[0] + skill[n - 1];
+		while (l <= r) {
+			if (sum != skill[l] + skill[r]) {
 				return -1;
 			}
-			pdt = skill[i] * 1ll * skill[n - i - 1];
-			res += pdt;
+			res += (skill[l++] * 1ll * skill[r--]);
 		}
 		return res;
 	}
