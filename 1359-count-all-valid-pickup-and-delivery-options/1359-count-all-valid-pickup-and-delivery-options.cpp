3952b06cdc761800e46fb5b0c0bb69db026edcf4
Atanu Ghosh <2001atanughosh@gmail.com>
2023-09-10 19:47:14 +0530

    Time: 2 ms (56.67%), Space: 6 MB (43.33%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int countOrders(int n) {
-        long res = 1;
-        const long long mod = (long long)1e9 + 7;
-        for (int i = 1; i <= n; ++i)
+        long long res = 1;
+        const long long mod = (long long)(1e9 + 7);
+        for (int i = 1; i <= n; ++i) {
             res = (res * (i * 2 - 1) * i) % mod;
+        }
         return res;
     }
 };
