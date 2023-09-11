209b8e0ee489b6d21ba030f40c0da81e2e59c6a4
Atanu Ghosh <2001atanughosh@gmail.com>
2023-09-11 20:16:21 +0530

    Time: 7 ms (87.01%), Space: 12.6 MB (80.59%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -14,16 +14,17 @@ public:
 };
 
 
+
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int> &groupSizes) {
         vector<vector<int>> res;
-        unordered_map<int, vector<int>> m;
+        unordered_map<int, vector<int>> mp;
         for (int i = 0; i < groupSizes.size(); i++) {
-            m[groupSizes[i]].push_back(i);
-            if (m[groupSizes[i]].size() == groupSizes[i]) {
-                res.push_back(m[groupSizes[i]]);
-                m[groupSizes[i]].clear();
+            mp[groupSizes[i]].push_back(i);
+            if (mp[groupSizes[i]].size() == groupSizes[i]) {
+                res.push_back(mp[groupSizes[i]]);
+                mp[groupSizes[i]].clear();
             }
         }
         return res;
