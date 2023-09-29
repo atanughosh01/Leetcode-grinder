44f1aac3c58072cfc01996f02f2985fe449ac9d6
Atanu Ghosh <2001atanughosh@gmail.com>
2023-09-29 20:42:42 +0530

    Time: 279 ms (75.92%), Space: 110.9 MB (56.02%) - LeetHub

--- 2412-minimum-money-required-before-transactions/2412-minimum-money-required-before-transactions.cpp
+++ 2412-minimum-money-required-before-transactions/2412-minimum-money-required-before-transactions.cpp
@@ -3,9 +3,9 @@ public:
     long long minimumMoney(vector<vector<int>> &transactions) {
         int mx = 0;
         long long res = 0;
-        for (auto &v : transactions) {
-            mx = max(mx, min(v[0], v[1]));
-            res += max(0, v[0] - v[1]);
+        for (auto &t : transactions) {
+            mx = max(mx, min(t[0], t[1]));
+            res += max(0, t[0] - t[1]);
         }
         res += mx;
         return res;
