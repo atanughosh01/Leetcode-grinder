dd551715ef429a3699b99a6231aa2815984940d7
Atanu Ghosh <2001atanughosh@gmail.com>
2023-10-23 00:45:37 +0530

    Time: 129 ms (77.63%), Space: 89.6 MB (80.92%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     int maximumScore(vector<int> &nums, int k) {
         int n  = nums.size(), mi = nums[k], res = nums[k];
@@ -11,5 +11,26 @@ public:
             else if (i >= 0) mi = nums[i];
         }
         return res;
+    }
+};
+
+
+
+class Solution {
+public:
+    int maximumScore(vector<int> &nums, int k) {
+        int n = nums.size(), left = k, right = k;
+        int ans = nums[k], currMin = nums[k];
+        while (left > 0 || right < n - 1) {
+            if ((left > 0 ? nums[left - 1]: 0) < (right < n - 1 ? nums[right + 1] : 0)) {
+                right++;
+                currMin = min(currMin, nums[right]);
+            } else {
+                left--;
+                currMin = min(currMin, nums[left]);
+            }
+            ans = max(ans, currMin * (right - left + 1));
+        }
+        return ans;
     }
 };
