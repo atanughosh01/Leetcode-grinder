7d3a2b99ae23c4207e41b7e884b105ec25242aef
Atanu Ghosh <2001atanughosh@gmail.com>
2023-11-28 00:15:47 +0530

    Time: 7 ms (72.07%), Space: 6.3 MB (74.68%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        unsigned int orig = x, back_x = 0;
+        while (x > 0) {
+            back_x = (back_x * 10) + (x % 10);
+            x /= 10;
+        }
+        return orig == back_x;
+        
+    }
+};
