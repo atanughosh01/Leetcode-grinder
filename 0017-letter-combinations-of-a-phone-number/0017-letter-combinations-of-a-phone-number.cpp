b7149a7a71a5b7e971fbe1a3b86ce16012e6e057
Atanu Ghosh <2001atanughosh@gmail.com>
2023-11-29 23:01:20 +0530

    Time: 3 ms (40.98%), Space: 7.3 MB (14.81%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    vector<string> ans, mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    void helper(string &digits, int i, string comb){	
+        if (i == digits.size()) {
+            ans.push_back(move(comb));    
+        } else {
+            for (char c : mappings[digits[i] - '2']) {
+                helper(digits, i + 1, comb + c);
+            }
+        }
+    }
+public:
+    vector<string> letterCombinations(string &digits) {
+        if (digits.empty()) return ans;
+        helper(digits, 0, "");              
+        return ans;
+    }
+};
