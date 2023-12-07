794cb59812e64f54cc4262c1fd05997aee50f70d
Atanu Ghosh <2001atanughosh@gmail.com>
2023-12-07 22:33:30 +0530

    Time: 15 ms (33.67%), Space: 14.5 MB (40.37%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int> &nums, int target) {
+        vector<vector<vector<int>>> dp(target + 1);
+        dp[0] = {{}};
+        for (int &i : nums) {
+            for (int j = i; j <= target; j++) {
+                for (auto v : dp[j - i])  {
+                    v.push_back(i);
+                    dp[j].push_back(v);
+                }
+            }
+        }
+        return dp[target];
+    }
+};
