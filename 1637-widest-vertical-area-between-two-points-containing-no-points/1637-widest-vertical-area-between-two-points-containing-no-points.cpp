4889f51fa99420711d729e7905a908faa0fe54fe
Atanu Ghosh <2001atanughosh@gmail.com>
2023-12-21 10:15:13 +0530

    Time: 245 ms (45.16%), Space: 81.7 MB (12.74%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.cpp
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -1,32 +1,32 @@
-/*
-    class Solution {
-    public:
-        int maxWidthOfVerticalArea(vector<vector<int>>& points) {
-            int res = INT_MIN;
-            set<int> st;
-            for(auto &x : points) {
-                st.insert(x[0]);
-            }
-            auto j = st.begin();
-            for(auto itr=st.begin(); itr != st.end();) {
-                j = itr;
-                itr++;
-                res = max(res, *itr - *j);
-            }
-            return res;
+class Solution_1 {
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>> &points) {
+        int res = INT_MIN;
+        set<int> st;
+        for (auto &point : points) {
+            st.insert(point[0]);
         }
-    };
-*/
+        auto j = st.begin();
+        for (auto itr = st.begin(); itr != st.end();) {
+            j = itr;
+            itr++;
+            res = max(res, *itr - *j);
+        }
+        return res;
+    }
+};
+
+
 
 class Solution {
 public:
-    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+    int maxWidthOfVerticalArea(vector<vector<int>> &points) {
         int res = 0;
         set<int> st;
-        for(auto &x : points) {
-            st.insert(x[0]);
+        for (auto &point : points) {
+            st.insert(point[0]);
         }
-        for(auto itr=next(st.begin()); itr != st.end(); itr++) {
+        for (auto itr = next(st.begin()); itr != st.end(); itr++) {
             res = max(res, *itr - *prev(itr));
         }
         return res;
