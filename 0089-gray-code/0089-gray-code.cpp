c5fad53c3e398fe25affede4792ddf5b3a0b4112
Atanu Ghosh <2001atanughosh@gmail.com>
2023-12-21 10:28:46 +0530

    Time: 4 ms (70.55%), Space: 11.5 MB (95.67%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> res(1 << n);
+        for (int i = 0; i < (1 << n); i++) {
+            res[i] = i ^ (i >> 1);
+        }
+        return res;
+    }
+};
