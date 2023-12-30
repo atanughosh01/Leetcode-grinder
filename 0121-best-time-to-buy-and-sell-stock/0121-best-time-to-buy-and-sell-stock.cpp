4067725060fe0bf1c52390cb86dc471292836856
Atanu Ghosh <2001atanughosh@gmail.com>
2023-12-30 19:22:24 +0530

    Time: 176 ms (5.18%), Space: 93.7 MB (29.30%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,28 @@
+// Bruteforce | O(n*n) Time | O(1) Space | n = prices.length
+class Solution_1 {
+public:
+    int maxProfit(vector<int> &prices) {
+        int n = prices.size(), res = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int profit = prices[j] - prices[i];
+                res = max(res, profit);
+            }
+        }
+        return res;
+    }
+};
+
+
+// Optimal | O(n) Time | O(1) Space | n = prices.length
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        int profit = 0, buyPrice = INT_MAX;
+        for (int &price : prices) {
+            buyPrice = min(buyPrice, price);
+            profit = max(profit, price - buyPrice);
+        }
+        return profit;
+    }
+};
