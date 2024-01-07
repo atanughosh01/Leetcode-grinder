5525a57a9e95531ee16d05462d849057b0a85378
Atanu Ghosh <2001atanughosh@gmail.com>
2024-01-07 10:47:27 +0530

    Time: 703 ms (45.00%), Space: 135.6 MB (66.94%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,14 +1,14 @@
-#define LL long long
+typedef long long LL;
 
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& A) {
-        int n = A.size();
+    int numberOfArithmeticSlices(vector<int> &nums) {
+        int n = nums.size();
         LL ans = 0;
         vector<map<LL, int>> cnt(n);
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                LL delta = (LL)A[i] - (LL)A[j];
+                LL delta = (LL)nums[i] - (LL)nums[j];
                 int sum = 0;
                 if (cnt[j].find(delta) != cnt[j].end()) {
                     sum = cnt[j][delta];
@@ -17,7 +17,6 @@ public:
                 ans += sum;
             }
         }
-
         return (int)ans;
     }
 };
