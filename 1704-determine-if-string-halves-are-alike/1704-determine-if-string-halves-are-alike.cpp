ba6090f57ef4404769355d9d39dc4675200d59bd
Atanu Ghosh <2001atanughosh@gmail.com>
2024-01-12 23:15:15 +0530

    Time: 6 ms (12.88%), Space: 7.2 MB (24.18%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -2,16 +2,16 @@ class Solution {
 private:
     int countVowel(int start, int end, string &s) {
         const string vowels = "aeiouAEIOU";
-        int answer = 0;
+        int ans = 0;
         for (int i = start; i < end; i++) {
             if (vowels.find(s[i]) != string::npos) {
-                answer++;
+                ans++;
             }
         }
-        return answer;
+        return ans;
     }
 public:
-    bool halvesAreAlike(string s) {
+    bool halvesAreAlike(string &s) {
         int n = s.size();
         return countVowel(0, n / 2, s) == countVowel(n / 2, n, s);
     }
