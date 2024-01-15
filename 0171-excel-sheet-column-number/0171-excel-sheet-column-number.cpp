fd597164ba977d1eb474dc17233a194606f7a6a7
Atanu Ghosh <2001atanughosh@gmail.com>
2024-01-15 11:49:55 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (82.00%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -0,0 +1,28 @@
+class Solution_1 {
+public:
+    int titleToNumber(string &col) {
+        int a[26], res = 0;
+        for (int i = 0; i < 26; i++) {
+            a[i] = i+1;
+        }
+        for (char &x : col) {
+            res *= 26;
+            res += a[(x-'0')-17];
+        }
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    int titleToNumber(string &col) {
+        int res = 0;
+        for (char &x : col) {
+            int ch = x - 'A' + 1;
+            res *= 26;
+            res += ch;
+        }
+        return res;
+    }
+};
