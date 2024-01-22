268991675450a2f3d86f2de1fc54d6fb53fce356
Atanu Ghosh <2001atanughosh@gmail.com>
2024-01-22 11:53:15 +0530

    Time: 14 ms (78.01%), Space: 18.8 MB (5.75%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
+// O(n) time, O(n) space
+class SolutionRecursive {
+public:
+    ListNode* removeElements(ListNode *head, int target) {
+        if (head == nullptr) return head;
+        head->next = removeElements(head->next, target);
+        return head ->val == target ? head->next : head;
+    }
+};
+
+
+// O(n) time, O(1) space
+class Solution_1 {
+public:
+    ListNode* removeElements(ListNode *head, int target) {
+        if (head == nullptr) return head;
+        ListNode *preHead = new ListNode(0);
+        preHead->next = head;
+        ListNode *tail = preHead;
+        while (tail != nullptr && tail->next != nullptr) {
+            if (tail->next->val == target) {
+                ListNode *temp = tail->next;
+                tail->next = temp->next;
+                delete temp;
+            } else {
+                tail = tail->next;
+            }
+        }
+        return preHead->next;
+    }
+};
+
+
+// O(n) time, O(1) space
+class Solution {
+public:
+    ListNode* removeElements(ListNode *head, int target) {
+        if (!head) return head;
+        ListNode *preHead = new ListNode(0);
+        preHead->next = head;
+        ListNode *tail = preHead;
+        while (tail && tail->next) {
+            if (tail->next->val == target) {
+                ListNode *temp = tail->next;
+                tail->next = temp->next;
+                delete temp;
+            } else {
+                tail = tail->next;
+            }
+        }
+        return preHead->next;
+    }
+};
