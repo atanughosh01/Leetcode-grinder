5f491955e40cd0bcab64d8b700553ef2c20ef889
Atanu Ghosh <2001atanughosh@gmail.com>
2024-01-27 20:10:28 +0530

    Time: 8 ms (98.95%), Space: 16.8 MB (19.74%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>> &p) {
+        if (p.empty()) return true;
+        vector<int> indegree(n, 0);
+        vector<vector<int>> graph(n);
+        for (vector<int> &x : p) {
+            graph[x[1]].push_back(x[0]);
+            indegree[x[0]]++;
+        }
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+                indegree[i]--;
+            }
+        }
+        if (q.empty()) return false;
+        int count = 0;
+        while (!q.empty()) {
+            for (int i = 0; i < q.size(); i++) {
+                int curr = q.front();
+                q.pop();
+                count++;
+                for (int child : graph[curr]) {
+                    indegree[child]--;
+                    if (indegree[child] == 0) {
+                        q.push(child);
+                    }
+                }
+            }   
+        }
+        return (count == n);
+    }
+};
