f7db985ccd1ad64695b33fb51087a7514485f4dd
Atanu Ghosh <2001atanughosh@gmail.com>
2024-02-06 19:35:14 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (16.83%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,45 @@
+// Recursion
+class Solution_1 {
+   public:
+    int addDigits(int num) {
+        int sum = 0;
+        while (num) {
+            sum += (num % 10);
+            num /= 10;
+        }
+        if (sum < 10)
+            return sum;
+        else
+            return addDigits(sum);
+    }
+};
+
+// Iteration
+class Solution_2 {
+   public:
+    int addDigits(int num) {
+        int sum = 0;
+        while (num > 9) {
+            while (num) {
+                sum += (num % 10);
+                num /= 10;
+            }
+            num = sum;
+            sum = 0;
+        }
+        return num;
+    }
+};
+
+// O(1) Time Complexity
+class Solution {
+   public:
+    int addDigits(int num) {
+        if (num == 0)
+            return 0;
+        else if (num % 9 == 0)
+            return 9;
+        else
+            return num % 9;
+    }
+};
