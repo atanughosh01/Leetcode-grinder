87f6f1a5b4d2d7bf4cf61c22d2532fce9739a0d3
Atanu Ghosh <2001atanughosh@gmail.com>
2024-02-26 22:36:49 +0530

    Time: 4 ms (69.11%), Space: 13.1 MB (23.86%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,32 @@
+// O(N * log(N)) time
+class Solution_1 {
+public:
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2) {
+        vector<int> res;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        for (int &x : nums2) {
+            if (binary_search(nums1.begin(), nums1.end(), x) && (res.empty() || res.back() != x)) {
+                res.push_back(x);
+            }
+        }
+        return res;
+    }
+};
+
+
+// O(N) time in average case
+class Solution {
+public:
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2) {
+        unordered_set<int> st(nums1.begin(), nums1.end());
+        vector<int> res;
+        for (int &x : nums2) {
+            if (st.find(x) != st.end()) {
+                res.push_back(x);
+                st.erase(x);
+            }
+        }
+        return res;
+    }
+};
