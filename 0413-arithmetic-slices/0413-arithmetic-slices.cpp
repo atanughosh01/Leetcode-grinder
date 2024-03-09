edcf69db83d41f3779720bb4260b93ca8fccf255
Atanu Ghosh <2001atanughosh@gmail.com>
2024-03-09 11:18:18 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (32.09%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int> &nums) {
+        int n = nums.size();
+        if (n < 3) return 0;
+        vector<int> dp(n, 0);
+        if (nums[2] - nums[1] == nums[1] - nums[0]) dp[2] = 1;
+        int res = dp[2];
+        for (int i = 3; i < n; ++i) {
+            if (nums[i] + nums[i - 2] == 2 * nums[i - 1]) {
+                dp[i] = dp[i - 1] + 1;
+            }
+            res += dp[i];
+        }
+        return res;
+    }
+};
