d923fedf83dc20a2fd238216ba2ce7043507c45e
Atanu Ghosh <2001atanughosh@gmail.com>
2024-03-25 10:25:44 +0530

    Time: 3 ms (36.14%), Space: 7.1 MB (55.02%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long getNxtPowOf2(int n) {
+        if ((int)log2(n) == log2(n)) {
+            return n*2;
+        }
+        long long pow = 1;
+        while (pow < n) pow *= 2;
+        return pow;
+    }
+
+    long long findComplement(int num) {
+        return getNxtPowOf2(num) - num -1; 
+    }
+};
