4b91b44cbf6d00110dff0ed414ff1bf17ad0a4f5
Atanu Ghosh <2001atanughosh@gmail.com>
2024-03-26 11:02:26 +0530

    Time: 46 ms (47.85%), Space: 43.7 MB (55.21%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -9,9 +9,9 @@ public:
         }
         for (int i = 0; i < n; i++) {
             if (nums[i] != i + 1) {
-                return i + 1;
+                return (i + 1);
             }
         }
-        return n + 1;
+        return (n + 1);
     }
 };
