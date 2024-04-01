68eb70c95fed54de8f5b97c6f5d70d5fac08a338
Atanu Ghosh <2001atanughosh@gmail.com>
2024-04-01 18:53:59 +0530

    Time: 4 ms (47.87%), Space: 7.6 MB (87.89%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int lengthOfLastWord(string &s) {
-        int len = 0;
         while (s.back() == ' ') s.pop_back();
+        int len = 0, n = s.size();
         for (int i = s.size() - 1; i >= 0; i--) {
             if (s[i] == ' ') break;
             len++;
