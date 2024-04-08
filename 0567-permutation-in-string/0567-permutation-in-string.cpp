8d8e4691fc93a0e4407df054f81c1c84a4e9c742
Atanu Ghosh <2001atanughosh@gmail.com>
2024-04-08 23:00:28 +0530

    Time: 8 ms (59.92%), Space: 8.2 MB (99.55%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,37 @@
+class Solution_1 {
+private:
+	string srt(string &s) {
+		string a = s;
+		sort(a.begin(), a.end());
+		return a;
+	}
+public:
+	bool checkInclusion(string s1, string s2) {
+		int m = s1.size(), n = s2.size();
+		for (int i = -1; i < n - m; ++i) {
+			string sub = s2.substr(i + 1, m);
+			if (srt(s1) == srt(sub)) {
+				return true;
+            }
+		}
+		return false;
+	}
+};
+
+
+class Solution {
+public:
+	bool checkInclusion(string &s1, string &s2) {
+		int mp[26] = {0};
+		for (char &c : s1) mp[c - 'a']++;
+		int j = 0, i = 0, count_chars = s1.size();
+		while (j < s2.size()) {
+			if (mp[s2[j++] - 'a']-- > 0) count_chars--;
+			if (count_chars == 0) return true;
+			if (j - i == s1.size() && mp[s2[(i++)] - 'a']++ >= 0) {
+				count_chars++;
+            }
+		}
+		return false;
+	}
+};
