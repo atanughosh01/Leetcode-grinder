6751d3c68c5ca359d918756a53d9598db47ba359
Atanu Ghosh <2001atanughosh@gmail.com>
2024-04-17 11:37:40 +0530

    Time: 168 ms (31.02%), Space: 67 MB (24.81%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    static bool cmp(const vector<int> &a, vector<int> &b) {
+        return a[1] < b[1];
+    }
+public:
+    int scheduleCourse(vector<vector<int>> &courses) {
+        if (courses.size() <= 0) return 0;
+        sort(courses.begin(), courses.end(), cmp);
+        priority_queue<int> pq;
+        int sum = 0;
+        for (auto course : courses) {
+            sum += course[0];
+            pq.push(course[0]);
+            if (sum > course[1]) {
+                sum -= pq.top();
+                pq.pop();
+            }
+        }
+        return ((int)pq.size());
+    }
+};
