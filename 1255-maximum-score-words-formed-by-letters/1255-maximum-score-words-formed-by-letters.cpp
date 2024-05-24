8d5bdd593514597513638d4d87f240bc9778a8ce
Atanu Ghosh <2001atanughosh@gmail.com>
2024-05-24 11:41:23 +0530

    Time: 6 ms (75.34%), Space: 17.1 MB (66.02%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
@@ -1,23 +1,26 @@
 class Solution {
+private:
+	int dfs(vector<string> &ws, vector<int> &cnt, vector<int> &score, int i) {
+		if (i >= ws.size()) {
+			return 0;
+		}
+		int skipGain = dfs(ws, cnt, score, i + 1);
+		int gain = 0, formed = 1;
+		vector<int> cnt1(cnt.begin(), cnt.end());
+		for (char &c : ws[i]) {
+			if (--cnt1[c - 'a'] < 0) {
+				formed = 0;
+			}
+			gain += score[c - 'a'];
+		}
+		return max(skipGain, formed ? gain + dfs(ws, cnt1, score, i + 1) : 0);
+	}
 public:
-    int dfs(vector<string>& ws, vector<int> &cnt, vector<int> &score, int i) {
-        if (i >= ws.size())
-            return 0;
-        auto skipGain = dfs(ws, cnt, score, i + 1), gain = 0, formed = 1;
-        vector<int> cnt1(begin(cnt), end(cnt));
-        for (auto ch : ws[i]) {
-            if (--cnt1[ch - 'a'] < 0)
-                formed = 0;
-            gain += score[ch - 'a'];
-        }
-        return max(skipGain, formed ? 
-            gain + dfs(ws, cnt1, score, i + 1) : 0);
-    }
-    
-    int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
-        vector<int> cnt(26);
-        for (auto ch : letters)
-            ++cnt[ch - 'a'];
-        return dfs(words, cnt, score, 0);
-    }
+	int maxScoreWords(vector<string> &words, vector<char> &letters, vector<int> &score) {
+		vector<int> cnt(26);
+		for (char &ch : letters) {
+			++cnt[ch - 'a'];
+		}
+		return dfs(words, cnt, score, 0);
+	}
 };
