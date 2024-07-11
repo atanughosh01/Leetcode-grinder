8482a98ad5df53dbd94f41f06efa6adb65b6fe00
Atanu Ghosh <2001atanughosh@gmail.com>
2024-07-11 09:53:09 +0530

    Time: 71 ms (77.16%), Space: 49.5 MB (70.53%) - LeetHub

---
+++ 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.cpp
@@ -0,0 +1,39 @@
+// O(n * m) Time | O(1) Space | n = nums.length, m = queries.length
+class Solution_1 {
+public:
+    vector<int> sumEvenAfterQueries(vector<int> &nums, vector<vector<int>> &queries) {
+        vector<int> res;
+        for (vector<int> &query : queries) {
+            nums[query[1]] += query[0];
+            int sum = 0;
+            for (int &num : nums) {
+                if (num % 2 == 0) {
+                    sum += num;
+                }
+            }
+            res.push_back(sum);
+        }
+        return res;
+    }
+};
+
+// O(n) + O(m) Time | O(1) Space | n = nums.length, m = queries.length
+class Solution {
+public:
+    vector<int> sumEvenAfterQueries(vector<int> &nums, vector<vector<int>> &queries) {
+        vector<int> res;
+        int sum = 0;
+        for (int &num : nums) {
+            if (num % 2 == 0) {
+                sum += num;
+            }
+        }
+        for (vector<int> &query : queries) {
+            if (nums[query[1]] % 2 == 0) sum -= nums[query[1]];
+            nums[query[1]] += query[0];
+            if (nums[query[1]] % 2 == 0) sum += nums[query[1]];
+            res.push_back(sum);
+        }
+        return res;
+    }
+};
